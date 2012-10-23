9face917774ee6958a6da84d1a2c1e5a8cec5f9b
David Turner <dwt@ualberta.ca>
2012-10-23 09:59:12 -0600

    -Added the HQP Milestone Comments PDF
    Former-commit-id: 33c87b430db5d3370b8b831985091117a234e932

--- extensions/PDFGenerator/PDFParams.php
+++ extensions/PDFGenerator/PDFParams.php
@@ -18,6 +18,7 @@ define('RPTP_LEADER_COMMENTS', 8);
 define('RPTP_EXIT_HQP', 9);
 define('RPTP_HQP', 9); // Exit is not longer used, but they should be considered equivallent
 define('RPTP_NI_COMMENTS', 10);
+define('RPTP_HQP_COMMENTS', 11);
 
 define('RPTP_NI_ZIP', 100);
 define('RPTP_PROJ_ZIP', 101); 
