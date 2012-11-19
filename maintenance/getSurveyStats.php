f506266fb0f40f517a4103ec7db01eb5049c5df1
Denis Golovan <dgolovan@ualberta.ca>
2012-11-19 10:16:07 -0700

    script to get the Survey stats.
    Former-commit-id: 7180cebb978df6d3058f69db4afc86d7a278adf7

---
+++ maintenance/getSurveyStats.php
@@ -0,0 +1,57 @@
+<?php
+    require_once('commandLine.inc');
+    
+
+    $sql = "SELECT sr.* FROM survey_results sr";
+    $data = execSQLStatement($sql);
+    
+    echo "First Name,Last Name,Email,Role,Submitted\n";
+
+    foreach($data as $row){
+    	$user_id = $row['user_id'];
+    
+
+	    $person = Person::newFromId($user_id);
+
+	    $role = "Other";
+	    if($person->isHQP()){
+	    	$role="HQP";
+	    }
+	    else if($person->isCNI()){
+	    	$role="CNI";
+	    }
+	    else if($person->isPNI()){
+	    	$role="PNI";
+	    }
+
+	    $name = $person->splitName();
+	    $f_name = $name['first'];
+	    $l_name = $name['last'];
+	    $email = $person->getEmail();
+	    $submitted = ($row['submitted'] == 1)? "Yes" : "No";
+
+	    echo "$l_name,$f_name,$email,$role,$submitted\n";
+
+	}
+    
+    
+    function execSQLStatement($sql, $update=false){
+		if($update == false){
+			$dbr = wfGetDB(DB_SLAVE);
+		}
+		else {
+			$dbr = wfGetDB(DB_MASTER);
+			return $dbr->query($sql);
+		}
+		$result = $dbr->query($sql);
+		$rows = null;
+		if($update == false){
+			$rows = array();
+			while ($row = $dbr->fetchRow($result)) {
+				$rows[] = $row;
+			}
+		}
+		return $rows;
+	}
+
+?>
