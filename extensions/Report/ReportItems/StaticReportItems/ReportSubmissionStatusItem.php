907a1859ecce48ebbbaea7e7e641fa9e589907f3
David Turner <dwt@ualberta.ca>
2012-12-04 09:50:51 -0700

    -Added the submission status to the progress report
    Former-commit-id: 91377ba5c96fd6aaefd7476b031712e2bb4b01dc

---
+++ extensions/Report/ReportItems/StaticReportItems/ReportSubmissionStatusItem.php
@@ -0,0 +1,36 @@
+<?php
+
+class ReportSubmissionStatusItem extends StaticReportItem {
+
+	function render(){
+	    global $wgOut;
+        $details = $this->getHTML();
+        $item = "$details";
+        $item = $this->processCData($item);
+		$wgOut->addHTML($item);
+	}
+	
+	function renderForPDF(){
+	    global $wgOut;
+        $details = $this->getHTML();
+        $item = "$details";
+        $item = $this->processCData($item);
+		$wgOut->addHTML($item);
+	}
+	
+	function getHTML(){
+	    $reportType = $this->getAttr('reportType', 'HQPReport');
+        $person = Person::newFromId($this->personId);
+        $project = $this->getReport()->project;
+        $report = new DummyReport($reportType, $person, $project);
+        if($report->isSubmitted()){
+        	$details = "Submitted";
+        }
+        else{
+        	$details = "Not Submitted";
+        }
+        return $details;
+	}
+}
+
+?>
