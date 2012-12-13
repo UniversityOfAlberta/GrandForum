23d16c279966e9dbd6ae864a7f92b2214265a806
David Turner <dwt@ualberta.ca>
2012-12-13 15:32:56 -0700

    -Added comments to separate the different types of APIs
    Former-commit-id: 7b6a5a8abcb0805b5ae61e3e4e9483222753d42e

--- extensions/GrandObjects/GrandObjects.php
+++ extensions/GrandObjects/GrandObjects.php
@@ -10,15 +10,19 @@ autoload_register('GrandObjects/API');
 $wgHooks['OutputPageParserOutput'][] = 'createModels';
 
 global $apiRequest;
+// Person
 $apiRequest->addAction('Hidden','person', new PersonAPI());
 $apiRequest->addAction('Hidden','person/:id', new PersonAPI());
 $apiRequest->addAction('Hidden','person/:id/projects', new PersonProjectsAPI());
 $apiRequest->addAction('Hidden','person/:id/roles', new PersonRolesAPI());
 $apiRequest->addAction('Hidden','person/:id/products', new PersonProductAPI());
+// Role
 $apiRequest->addAction('Hidden','role', new RoleAPI());
 $apiRequest->addAction('Hidden','role/:id', new RoleAPI());
+// Project
 $apiRequest->addAction('Hidden','project', new ProjectAPI());
 $apiRequest->addAction('Hidden','project/:id', new ProjectAPI());
+// Product
 $apiRequest->addAction('Hidden','product', new ProductAPI());
 $apiRequest->addAction('Hidden','product/:id/authors', new PersonProductAPI());
 
