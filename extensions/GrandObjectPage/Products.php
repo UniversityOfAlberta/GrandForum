1a5278e737e2b50bf52e2d1efb4deba7eda850fd
David Turner <dwt@ualberta.ca>
2012-12-19 09:22:13 -0700

    -Added a BackbonePage, which subclasses SpecialPage, making it easier to create Special Pages for Backbone
    Former-commit-id: 0f505d572a26aed1aa55389a8a6eabd251503e47

---
+++ extensions/GrandObjectPage/Products.php
@@ -0,0 +1,17 @@
+<?php
+
+BackbonePage::register('Products', 'Products', 'grand-tools');
+
+class Products extends BackbonePage {
+    
+    function getTemplates(){
+        return array();
+    }
+    
+    function getViews(){
+        return array();
+    }
+    
+}
+
+?>
