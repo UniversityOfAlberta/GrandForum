ac4d782290163d277b32ce9ea3502936cf7bfccf
David Turner <dwt@ualberta.ca>
2012-12-19 15:44:40 -0700

    -Added the routes for the products
    Former-commit-id: c15a4b1bc8f3c78b4005de3e1ab72b53d30fb76d

--- extensions/GrandObjectPage/Products/routes.js
+++ extensions/GrandObjectPage/Products/routes.js
@@ -1,13 +1,32 @@
 PageRouter = Backbone.Router.extend({
     routes: {
-        ":category/:id": "showProduct"
+        ":category": "showProducts",
+        ":category/new": "newProduct",
+        ":category/:id": "showProduct",
+        ":category/:id/edit": "editProduct"
     }
 });
 
 // Initiate the router
 var pageRouter = new PageRouter;
 
+pageRouter.on('route:showProducts', function(category){
+    // Get All Products
+    products = new Products();
+});
+
+pageRouter.on('route:newProduct', function(category){
+    // Create New Product
+    products = new Product();
+});
+
 pageRouter.on('route:showProduct', function (category, id) {
+    // Get A single product
+    product = new Product({'id': id});
+});
+
+pageRouter.on('route:editProduct', function (category, id) {
+    // Get A single product
     product = new Product({'id': id});
 });
 
