04fc77ada4c54569ac08199d9895a5930e52f3e4
David Turner <dwt@ualberta.ca>
2012-12-20 15:42:12 -0700

    -Added some more routes
    Former-commit-id: 2597e6955c8bacb943559a6ce1348842f30de2c0

--- extensions/GrandObjectPage/Products/routes.js
+++ extensions/GrandObjectPage/Products/routes.js
@@ -1,14 +1,16 @@
 PageRouter = Backbone.Router.extend({
-    
+        
     initialize: function(){
         this.bind('all', function(event){
             $("#currentView").html("<div id='currentViewSpinner'></div>");
             spin = spinner("currentViewSpinner", 40, 75, 12, 10, '#888');
         });
     },
-
+    
     routes: {
-        ":category": "showProducts",
+        ":category": "showGrandProducts",
+        ":category/grand": "showGrandProducts",
+        ":category/nonGrand": "showNonGrandProducts",
         ":category/new": "newProduct",
         ":category/:id": "showProduct",
         ":category/:id/edit": "editProduct"
@@ -18,11 +20,27 @@ PageRouter = Backbone.Router.extend({
 // Initiate the router
 var pageRouter = new PageRouter;
 
-pageRouter.on('route:showProducts', function(category){
+pageRouter.on('route:showGrandProducts', function(category){
     // Get All Products
     products = new Products();
     products.category = category;
     products.grand = 'grand';
+    if(category != 'Press'){
+        categoryTitle = category + 's';
+    }
+    main.set('title', 'GRAND ' + categoryTitle);
+    productListView = new ProductListView({el: $("#currentView"), model: products});
+});
+
+pageRouter.on('route:showNonGrandProducts', function(category){
+    // Get All Products
+    products = new Products();
+    products.category = category;
+    products.grand = 'nonGrand';
+    if(category != 'Press'){
+        categoryTitle = category + 's';
+    }
+    main.set('title', 'Non-GRAND ' + categoryTitle);
     productListView = new ProductListView({el: $("#currentView"), model: products});
 });
 
