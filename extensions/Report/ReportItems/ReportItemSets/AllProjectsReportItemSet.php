4b60c34700cd2c43c6e5f0bcc8f89c714e6a12c3
Denis Golovan <dgolovan@ualberta.ca>
2013-01-08 15:55:36 -0700

    skip AD-NODE
    Former-commit-id: c0baa0523939dadb46103ceee2f83017cf95adf2

--- extensions/Report/ReportItems/ReportItemSets/AllProjectsReportItemSet.php
+++ extensions/Report/ReportItems/ReportItemSets/AllProjectsReportItemSet.php
@@ -6,6 +6,9 @@ class AllProjectsReportItemSet extends ReportItemSet {
         $data = array();
         $projects = Project::getAllProjects();
         foreach($projects as $project){
+            if($project->getName() == "AD-NODE"){
+                continue;
+            }
             $tuple = self::createTuple();
             $tuple['project_id'] = $project->getId();
             $data[] = $tuple;
