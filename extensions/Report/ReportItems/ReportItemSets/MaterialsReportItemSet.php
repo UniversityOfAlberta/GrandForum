d4f11df0244168b27cc8a98c2fca882a711c45a0
David Turner <dwt@ualberta.ca>
2013-01-09 09:56:17 -0700

    -Combined PersonMaterialsReportItemSet with ProjectMaterialsReportItemSet
    Former-commit-id: 03bdd97fec00d77c81869255991d88ea1bcd29bb

---
+++ extensions/Report/ReportItems/ReportItemSets/MaterialsReportItemSet.php
@@ -0,0 +1,27 @@
+<?php
+
+class MaterialsReportItemSet extends ReportItemSet {
+
+    function getData(){
+        $data = array();
+        $person = Person::newFromId($this->personId);
+        $type = $this->getAttr('subType', 'PNI');
+        $subs = $person->getEvaluates($type);
+        if(is_array($subs)){
+            foreach($subs as $sub){
+                $tuple = self::createTuple();
+                if($type == "Project"){
+                    $tuple['project_id'] = $sub->getId();
+                }
+                else{
+                    $tuple['person_id'] = $sub->getId();
+                }
+                $data[] = $tuple;
+            }
+        }
+        return $data;
+    }
+
+}
+
+?>
