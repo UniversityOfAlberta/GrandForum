c1c15e9130006b4d2e531a4c133f40bcd3f16d12
Mike Huggett <michaelhuggett@gmail.com>
2013-01-13 20:05:26 -0700

    - Add URL and Peer Reviewed to Journal Abstract. - Rationalize fields for Poster and White Paper.
    Former-commit-id: dc1232e26017b24a59071bcc9a45cd9b28e6b498

--- extensions/API/Publications/Publications/WhitePaperAPI.php
+++ extensions/API/Publications/Publications/WhitePaperAPI.php
@@ -9,7 +9,6 @@ class WhitePaperAPI extends PaperAPI{
 	    $this->addPOST("status",false,"The status of the publication.  Can be either Submitted,Under Revision,Published,Rejected","Submitted");
 	    $this->addPOST("published_in",false,"The title of the journal that this publication was published in","Technical Series Special Issue");
 	    $this->addPOST("publisher",false,"The name of the publisher","My Publishing Company");
-	    $this->addPOST("url",false,"The url of this white paper","http://www.dh.gov.uk/en/Publicationsandstatistics/Publications/PublicationsPolicyAndGuidance/DH_117353");
 	    $this->addPOST("editor",false,"The editor of this white paper","Mr Editor");
       $this->addPOST("url",false,"Link to a copy of the publication","http://mySite.org/myPaper.pdf");
       $this->addPOST("peer_reviewed",false,"Whether or not the publication is peer reviewed","Yes");
