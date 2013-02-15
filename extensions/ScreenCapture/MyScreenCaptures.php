069419eac80d4a4135352b23b74d72802c199c3f
David Turner <dwt@ualberta.ca>
2013-02-15 16:43:21 -0700

    -Added edit dialogues for descriptions
    Former-commit-id: d8492cd2b0710798fe6dce3f7ae1a10a59a93a91

--- extensions/ScreenCapture/MyScreenCaptures.php
+++ extensions/ScreenCapture/MyScreenCaptures.php
@@ -139,8 +139,10 @@ EOF
 		}
 		
 		function addDesc(parent){
-		    var desc = $("<div class='window' style='background:#ffffff;border:1px solid #aaa;padding:3px;position:absolute;z-index:5000;-webkit-border-radius:5px;-moz-border-radius: 5px;border-radius:5px;cursor:pointer;'>Description</div>");
+		    var description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In magna ligula, pretium nec pharetra id, accumsan varius enim. Praesent mollis, dui eu convallis faucibus, sapien est vehicula dui, vitae iaculis augue nisi eget elit. Sed ultrices mauris euismod neque luctus condimentum. In hendrerit eros at justo fringilla in interdum dui convallis. Praesent eget libero ac sem ornare hendrerit a quis tellus. Donec placerat, sem at suscipit iaculis, purus quam fringilla augue, nec porta eros risus ac sapien. Praesent sagittis, tellus eu semper dapibus, velit mauris elementum ipsum, at semper quam metus a purus. Suspendisse id elit vel quam consequat tempor id id lacus.\\n\\nQuisque ac turpis lacus. Phasellus nibh quam, vestibulum sit amet feugiat ac, aliquam nec dolor. Sed lectus arcu, venenatis in eleifend id, ornare non leo. Nunc pharetra, ante quis fringilla fringilla, quam ante consectetur nisi, a sodales lorem metus vel mauris. Nunc pellentesque sapien vulputate nibh scelerisque ultrices aliquet leo volutpat. Quisque molestie vulputate magna, et volutpat sapien accumsan ac. Donec rutrum hendrerit tellus, sit amet consectetur quam viverra id. Curabitur egestas massa at nibh dictum et lacinia nisl aliquam. Nullam cursus nunc vitae metus suscipit et volutpat neque lacinia. Integer lacinia molestie interdum. Integer et diam justo. In vitae rhoncus enim. In porttitor gravida sapien sollicitudin condimentum. Cras elit nisl, lobortis nec varius nec, pellentesque consectetur massa. Vestibulum commodo porttitor mauris quis tristique. Donec varius nunc sed lectus consequat tempus.\\n\\nProin justo urna, lobortis non tincidunt commodo, aliquet quis nunc. In et purus nisi, et varius nisi. Pellentesque ultrices diam nec tellus tempus sagittis. Ut mattis lorem vel mauris commodo faucibus. Nunc sed sapien et urna sodales ullamcorper. Nulla ullamcorper sagittis metus, ac elementum ante mattis at. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin eu arcu ante, pharetra vulputate arcu. Ut molestie rutrum malesuada.\\n\\nSuspendisse potenti. Aenean ultricies lacus a nulla adipiscing ac pellentesque magna tempus. Mauris cursus, dolor in interdum tempor, dolor ipsum adipiscing orci, id gravida justo felis quis risus. Aliquam hendrerit libero et velit feugiat in gravida orci pharetra. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur bibendum mollis justo, vel eleifend lorem placerat at. Duis eu libero eget urna venenatis ullamcorper. Maecenas vel quam et leo porttitor aliquet. Curabitur non nunc eget elit ultricies lacinia id facilisis urna. Suspendisse ac quam a lorem cursus imperdiet vel sit amet sem. Duis luctus posuere neque et faucibus. Nunc suscipit, arcu interdum molestie feugiat, magna dolor blandit est, quis semper arcu est a enim. Duis lobortis purus eget nibh ultrices ullamcorper. Ut sit amet porta mauris. Pellentesque imperdiet nulla non est tempus vulputate.";
+		    var desc = $("<div class='window' style='background:#ffffff;border:1px solid #aaa;padding:3px;position:absolute;z-index:5000;-webkit-border-radius:5px;-moz-border-radius: 5px;border-radius:5px;cursor:pointer;'>" + description.substring(0,10) + "</div>");
 		    desc.fadeIn();
+		    desc.colorbox({title:'Description',width:'500px',height:'300px',html:description.replace(/\\n/g, '<br />')});
             $("#graph").append(desc);
             jsPlumb.draggable(desc, {containment:"parent"});
             var con = jsPlumb.connect({
@@ -159,6 +161,19 @@ EOF
 	            $("#descMenu > ul").menu();
 	            $("#descMenu > ul a[name=deleteDesc]").click(function(){
 	                deleteDesc(desc);
+	            });
+	            $("#descMenu > ul a[name=editDesc]").click(function(){
+	                var content = $("<textarea style='height:190px;width:100%;margin:0;'>" + description + "</textarea><button id='saveDesc'>Save</button><button id='cancelDesc'>Cancel</button>");
+	                $.colorbox({open:true,title:'Edit Description',width:'500px',height:'300px',html:content});
+	                $("#saveDesc").click(function(){
+	                    description = $("#colorbox textarea").val();
+	                    desc.html(description.substring(0,10));
+	                    desc.colorbox({title:'Description',width:'500px',height:'300px',html:description.replace(/\\n/g, '<br />')});
+	                    $.colorbox.close();
+	                });
+	                $("#cancelDesc").click(function(){
+	                    $.colorbox.close();
+	                });
 	            });
 	            $("#descMenu").css('left', $(this).position().left + $(this).width()).css('top', $(this).position().top);
 	            e.stopPropagation();
