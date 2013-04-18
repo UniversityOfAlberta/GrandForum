a7024a7f1a50aa560b2f4fb023d72658b2c58584
David Turner <dwt@ualberta.ca>
2013-04-18 13:57:17 -0600

    -Make the search bar go to the old search for now
    Former-commit-id: 12f648d8fb9d212dc42f0034f60471ad3a0cbdc1

--- extensions/GlobalSearch/Views/GlobalSearchView.js
+++ extensions/GlobalSearch/Views/GlobalSearchView.js
@@ -3,6 +3,14 @@ GlobalSearchView = Backbone.View.extend({
     initialize: function(){
         this.template = _.template($("#global_search_template").html());
     },
+    
+    events: {
+        "keyup #globalSearchInput": "search"
+    },
+    
+    search: function(e){
+        var value = this.$el.find("#globalSearchInput").val();
+    },
 
     render: function(){
         this.$el.html(this.template());
