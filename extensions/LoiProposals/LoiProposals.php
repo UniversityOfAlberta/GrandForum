60ee2d3fd13d191a876c68d0ea57d9f9893a4df9
Denis Golovan <dgolovan@ualberta.ca>
2013-06-24 16:10:59 -0600

    LOI Proposals: CV import and table
    Former-commit-id: 31277b479f47be4ccaf3c0be08132045c83af94f

--- extensions/LoiProposals/LoiProposals.php
+++ extensions/LoiProposals/LoiProposals.php
@@ -43,6 +43,27 @@ class LoiProposals extends SpecialPage {
 			    exit;
 			}	
 		}
+		else if (isset($_GET['getcvpdf'])) {
+			$filename = $_GET['getcvpdf'];
+			$filepath = "/local/data/www-root/grand_forum/data/loi_proposals/cv/{$filename}";
+			//echo $filepath;
+			if (file_exists($filepath)) {
+				$wgOut->disable();
+            	ob_clean();
+            	//flush();
+			    header("Pragma: public");
+	            header("Expires: 0");
+	            header("Cache-Control: must-revalidate, post-check=0, pre-check=0"); 
+	            header("Content-Type: application/force-download");
+	            header("Content-Type: application/octet-stream");
+	            header("Content-Type: application/download");
+			    header('Content-Disposition: attachment; filename='.$filename);
+			    header('Content-Transfer-Encoding: binary');
+			   
+			    readfile($filepath);
+			    exit;
+			}	
+		}
 
 	    $wgOut->setPageTitle("LOI Proposals");
 	    $wgOut->addHTML("");
@@ -50,24 +71,53 @@ class LoiProposals extends SpecialPage {
 	    <div id='ackTabs'>
         <ul>
             <li><a href='#lois'>LOI Proposals</a></li>
-            <!--<li><a href='#ni'>CV</a></li>-->
+            <li><a href='#cv'>CV</a></li>
         </ul>
-	    <div id="lois" style='width: 1100px;overflow: scroll;'>
-	    <table class='indexTable' style='background:#ffffff; width: 1500px; table-layout: auto;' cellspacing='1' cellpadding='3' frame='box' rules='all'>
+EOF;
+		$html .= "<div id='lois' style='width: 100%; position:relative; overflow: scroll;'>";
+		$html .= LoiProposals::loiTable();
+		$html .= "</div>";
+
+		$html .= "<div id='cv' style='width: 100%; overflow: scroll;'>";
+		$html .= LoiProposals::cvTable();
+		$html .= "</div>";
+
+		//$html .= "</div";
+
+		$html .=<<<EOF
+		<script type='text/javascript'>
+            $(document).ready(function(){
+                $('.loiindexTable').dataTable({
+                	"bAutoWidth": false,
+                	"iDisplayLength": 25
+    			});
+				$('.cvindexTable').dataTable({
+                	"bAutoWidth": false,
+                	"iDisplayLength": 100
+    			});
+                $('#ackTabs').tabs();
+            });
+        </script>
+EOF;
+		$wgOut->addHTML($html);
+	}
+
+	static function loiTable(){
+		global $wgOut, $wgUser, $wgServer, $wgScriptPath, $wgMessage;
+
+		$html =<<<EOF
+	    <table class='loiindexTable' style='background:#ffffff; width: 100%; table-layout: auto;' cellspacing='1' cellpadding='3' frame='box' rules='all'>
             <thead>
                 <tr bgcolor='#F2F2F2'>
                     <th>Name</th>
-                    <th>Full Name</th>
                     <th>Type</th>
-                    <th>Related LOI</th>
-                    <th style="width: 200px;">Description</th>
+                    <th>Description</th>
                     <th>Lead</th>
                     <th>Co-Lead</th>
                     <th>Champion</th>
-                    <th>Primary Challenge</th>
-                    <th>Secondary Challenge</th>
-                    <th>LOI File</th>
-                    <th>Supplemental</th>
+                    <th>Challenges</th>
+                    <!--<th>Secondary Challenge</th>-->
+                    <th>LOI Files</th>
                 </tr>
             </thead>
             <tbody>
@@ -102,36 +152,86 @@ EOF;
 
 			$html .=<<<EOF
 				<tr>
-				<td>{$name}</td>
-				<td>{$full_name}</td>
+				<td width="13%">
+				<b>{$name}:</b><br /><br />
+				{$full_name}<br />
+				<b>Related LOI: </b>{$related_loi}
+				</td>
+				
 				<td>{$type}</td>
-				<td>{$related_loi}</td>
 				<td>{$description}</td>
 				<td>{$lead}</td>
 				<td>{$colead}</td>
 				<td>{$champion}</td>
-				<td>{$primary_challenge}</td>
-				<td>{$secondary_challenge}</td>
-				<td>{$loi_pdf}</td>
-				<td>{$supplemental_pdf}</td>
+				<td>
+				<p>
+				<b>Primary:</b><br />
+				{$primary_challenge}
+				</p>
+				<p>
+				<b>Secondary:</b><br />
+				{$secondary_challenge}
+				</p>
+				</td>
+				<!--<td>{$secondary_challenge}</td>-->
+				<td>
+				<b>LOI: {$loi_pdf}</b><br /><br />
+				<b>Supplemental: {$supplemental_pdf}</b>
+				</td>
 				</tr>
 EOF;
 		}
-
+		
 		$html .=<<<EOF
-		</tbody>
-		</table>
-		</div>
-		<script type='text/javascript'>
-            $(document).ready(function(){
-                $('.indexTable').dataTable({
-                	"bAutoWidth": false
-    			});
-                $('#ackTabs').tabs();
-            });
-        </script>
+			</tbody>
+			</table>
 EOF;
-		$wgOut->addHTML($html);
+
+		return $html;
+
+	}
+
+	static function cvTable(){
+		global $wgOut, $wgUser, $wgServer, $wgScriptPath, $wgMessage;
+
+		$html =<<<EOF
+	    <table class='cvindexTable' style='background:#ffffff; table-layout: auto;' cellspacing='1' cellpadding='3' frame='box' rules='all'>
+            <thead>
+                <tr bgcolor='#F2F2F2'>
+                    <th>Researcher Name</th>
+                    <th>Download CV</th>
+                </tr>
+            </thead>
+            <tbody>
+EOF;
+
+		$query = "SELECT * FROM grand_researcher_cv WHERE year=2013";
+		$data = DBFunctions::execSQL($query);
+		foreach($data as $row){
+			$researchername 	= $row['researcher_name'];
+			$cv_pdf = $row['filename'];
+			
+			if(!empty($cv_pdf)){
+				$cv_pdf = "<a target='_blank' href='{$wgServer}{$wgScriptPath}/index.php/Special:LoiProposals?getcvpdf={$cv_pdf}'>{$cv_pdf}</a>";
+			}else{
+				$cv_pdf = "N/A";
+			}
+
+			$html .=<<<EOF
+				<tr>
+				<td>{$researchername}</td>
+				<td>{$cv_pdf}</td>
+				</tr>
+EOF;
+		}
+		
+		$html .=<<<EOF
+			</tbody>
+			</table>
+EOF;
+
+		return $html;
+
 	}
 
 	// static function createTab(){
