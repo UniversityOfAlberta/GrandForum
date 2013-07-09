905ade838a7d697d5dfd83a7f253cae7c896b4b8
Denis Golovan <dgolovan@ualberta.ca>
2013-07-09 13:45:19 -0600

    LOI Eval PDF mods: page breaks, etc
    Former-commit-id: a45978fd325596b0d5baec0ca982c75323d15097

--- extensions/Report/ReportItems/StaticReportItems/LOIHeaderReportItem.php
+++ extensions/Report/ReportItems/StaticReportItems/LOIHeaderReportItem.php
@@ -8,8 +8,32 @@ class LOIHeaderReportItem extends StaticReportItem {
         $loi = null;
         $loi = LOI::newFromId($this->projectId);
         $loi_name = $loi->getName();
+        $full_name = $loi->getFullName();
+        $description = $loi->getDescription();
+        $type = $loi->getType();
+        //$related_loi = $loi->getRelatedLOI();
+        $lead = str_replace('<br />', ', ', $loi->getLead());
+        $colead = str_replace('<br />', ', ', $loi->getCoLead());
+        $champ = str_replace('<br />', ', ', $loi->getChampion());
+        $primary_challenge = str_replace('<br />', ', ', $loi->getPrimaryChallenge());
+        $secondary_challenge = str_replace('<br />', ', ', $loi->getSecondaryChallenge());
 
-	    $wgOut->addHTML("<br /><h2>{$loi_name}</h2>");
+        $html =<<<EOF
+        	<h2>{$loi_name}</h2>
+        	<table style="text-align: left; border: 1px solid #CCCCCC;">
+        	<tr><th>Full Name</th><td>{$full_name}</td></tr>
+            <tr><th>Type:</th><td>{$type}</td></tr>
+            <tr><th>Lead:</th><td>{$lead}</td></tr>
+            <tr><th>Co-Lead:</th><td>{$colead}</td></tr>
+            <tr><th>Champion:</th><td>{$champ}</td></tr>
+           	<tr><th>Primary Challenge:</th><td>{$primary_challenge}</td></tr>
+            <tr><th>Secondary Challenge:</th><td>{$secondary_challenge}</td></tr>
+            <tr><th>Description:</th><td></td></tr>
+            <tr><td colspan="2">{$description}</td></tr>
+            </table>
+EOF;
+
+	    $wgOut->addHTML($html);
 	}
 	
 	function renderForPDF(){
