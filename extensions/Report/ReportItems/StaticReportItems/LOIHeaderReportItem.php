3a47f2317aa06be0a7e6dcad55a43e716c8c5ff7
Denis Golovan <dgolovan@ualberta.ca>
2013-07-09 13:50:58 -0600

    LOI Eval PDF mods: page breaks, etc
    Former-commit-id: 657ba3c30416539f406a52a7c28e18995f54e3a6

--- extensions/Report/ReportItems/StaticReportItems/LOIHeaderReportItem.php
+++ extensions/Report/ReportItems/StaticReportItems/LOIHeaderReportItem.php
@@ -20,17 +20,18 @@ class LOIHeaderReportItem extends StaticReportItem {
 
         $html =<<<EOF
         	<h2>{$loi_name}</h2>
-        	<table style="text-align: left; border: 1px solid #CCCCCC;">
-        	<tr><th>Full Name</th><td>{$full_name}</td></tr>
-            <tr><th>Type:</th><td>{$type}</td></tr>
-            <tr><th>Lead:</th><td>{$lead}</td></tr>
-            <tr><th>Co-Lead:</th><td>{$colead}</td></tr>
-            <tr><th>Champion:</th><td>{$champ}</td></tr>
-           	<tr><th>Primary Challenge:</th><td>{$primary_challenge}</td></tr>
-            <tr><th>Secondary Challenge:</th><td>{$secondary_challenge}</td></tr>
-            <tr><th>Description:</th><td></td></tr>
-            <tr><td colspan="2">{$description}</td></tr>
+        	<table style="border: 1px solid #CCCCCC;">
+        	<tr><th align="left">Full Name</th><td>{$full_name}</td></tr>
+            <tr><th align="left">Type:</th><td>{$type}</td></tr>
+            <tr><th align="left">Lead:</th><td>{$lead}</td></tr>
+            <tr><th align="left">Co-Lead:</th><td>{$colead}</td></tr>
+            <tr><th align="left">Champion:</th><td>{$champ}</td></tr>
+           	<tr><th align="left">Primary Challenge:</th><td>{$primary_challenge}</td></tr>
+            <tr><th align="left">Secondary Challenge:</th><td>{$secondary_challenge}</td></tr>
+            <tr><th align="left">Description:</th><td></td></tr>
+            <tr><td align="left" colspan="2">{$description}</td></tr>
             </table>
+            <br />
 EOF;
 
 	    $wgOut->addHTML($html);
