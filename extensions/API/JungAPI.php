b947afd2078ff3bc707f3d9dd86d0a54985328ef
Finalfantasykid <finalfantasykid13@gmail.com>
2013-10-02 14:39:19 -0600

    -Changed JungAPI to have yearRegistered instead of nDaysInGRAND
    Former-commit-id: f2c53323af836c1d21ac8f86bec20a1ced788ccb

--- extensions/API/JungAPI.php
+++ extensions/API/JungAPI.php
@@ -330,7 +330,7 @@ class JungAPI extends API{
             $tuple['Title'] = (string)$this->personUniversities[$person->getName()]['position'];
             $tuple['Gender'] = (string)$person->getGender();
             $tuple['Nationality'] = (string)$person->getNationality();
-            $tuple['nDaysInGRAND'] = (string)(ceil((strtotime("December 31 {$this->year}") - strtotime($person->getRegistration()))/86400));
+            $tuple['yearRegistered'] = (string)substr($person->getRegistration(), 0, 4);
             
             // Extra
             $tuple['Projects'] = array();
