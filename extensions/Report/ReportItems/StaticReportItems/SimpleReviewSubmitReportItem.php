d04dcc86c4b16cb6f73d17616dda34c3e49d450e
Finalfantasykid <finalfantasykid13@gmail.com>
2013-11-15 10:48:39 -0700

    -Changed the wording for the pdf generation of the MTG -Fixed the permissions for the MTG
    Former-commit-id: 47d353a879bacc0a8816fa1ef51946ac1422ad9c

---
+++ extensions/Report/ReportItems/StaticReportItems/SimpleReviewSubmitReportItem.php
@@ -0,0 +1,188 @@
+<?php
+
+class SimpleReviewSubmitReportItem extends ReviewSubmitReportItem {
+
+	function render(){
+		global $wgOut, $wgUser, $wgServer, $wgScriptPath, $wgImpersonating;
+		$reportname = $this->getReport()->name;
+		$person = Person::newFromId($wgUser->getId());
+		$projectGet = "";
+		if($this->getReport()->project != null){
+		    $projectGet = "&project={$this->getReport()->project->getName()}";
+		}
+		$year = "";
+        if(isset($_GET['reportingYear']) && isset($_GET['ticket'])){
+            $year = "&reportingYear={$_GET['reportingYear']}&ticket={$_GET['ticket']}";
+        }
+		if(!$wgImpersonating || checkSupervisesImpersonee()){
+		    $wgOut->addHTML("<script type='text/javascript'>
+		        $(document).ready(function(){
+		        
+		            $('#generateButton').click(function(){
+		                $('#generateButton').prop('disabled', true);
+		                $('#generate_success').html('');
+                        $('#generate_success').css('display', 'none');
+                        $('#generate_error').html('');
+                        $('#generate_error').css('display', 'none');
+                        $('#generate_throbber').css('display', 'inline-block');
+		                $.ajax({
+		                        url : '$wgServer$wgScriptPath/index.php/Special:Report?report={$this->getReport()->xmlName}{$projectGet}{$year}&generatePDF', 
+		                        success : function(data){
+		                                        //var data = jQuery.parseJSON(response);
+		                                        for(index in data){
+		                                            val = data[index];
+		                                            if(typeof val.tok != 'undefined'){
+		                                                var tok = val.tok;
+		                                                var time = val.time;
+		                                                var len = val.len;
+		                                                var name = val.name;
+		                                                
+		                                                $('#ex_token_' + index).html(tok);
+                                                        $('#ex_time_' + index).html(time);
+                                                        $('#generate_button_' + index).attr('value', tok);
+                                                        $('#download_button_' + index).removeAttr('disabled');
+                                                        
+                                                        $('#generate_success').html('PDF Generated Successfully.');
+                                                        $('#generate_success').css('display', 'block');
+                                                        $('#download_button_' + index).attr('name', tok);
+                                                        $('#download_button_' + index).text(name + ' PDF');
+                                                        $('.submit_status_cell').css('background', 'red');
+		                                                $('.submit_status_cell').html('<b>No</b>');
+                                                    }
+                                                    else{
+                                                        $('#generate_error').html('There was an error generating the PDF.  Please try again, and if it still fails, contact <a href=\"mailto:support@forum.grand-nce.ca\">support@forum.grand-nce.ca</a>');
+                                                        $('#generate_error').css('display', 'block');
+                                                    }
+                                                }
+		                                        $('#generate_throbber').css('display', 'none');
+		                                        $('#generateButton').removeAttr('disabled');
+		                                        $('#submitCheck').removeAttr('checked');
+		                                        $('#submitCheck').removeAttr('disabled');
+		                                  },
+		                        error : function(response){
+                                              // Error
+                                              $('#generate_error').html('There was an error generating the PDF.  Please try again, and if it still fails, contact <a href=\"mailto:support@forum.grand-nce.ca\">support@forum.grand-nce.ca</a>');
+                                              $('#generate_error').css('display', 'block');
+		                                      $('#generateButton').removeAttr('disabled');
+		                                      $('#generate_throbber').css('display', 'none');
+		                                      //$('#submitCheck').removeAttr('disabled');
+		                                  }
+		                });
+		            });
+		            
+		            $('#submitButton').click(function(){
+		                var that = this;
+		                if(errors > 0){
+		                    $('#reportErrors').dialog('destroy');
+		                    $('#reportErrors').dialog({resizable: false,
+		                                               modal: true,
+		                                               buttons: {
+                                                            'Submit': function() {
+                                                                submitReport(that);
+                                                                $( this ).dialog('close');
+                                                            },
+                                                            'Cancel': function() {
+                                                                $( this ).dialog('close');
+                                                            }
+                                                       }});
+                            $('.ui-dialog-buttonset button').removeClass('ui-widget').removeClass('ui-state-default').removeClass('ui-corner-all').removeClass('ui-button-text-only').removeClass('ui-state-hover');
+		                }
+		                else{
+		                    submitReport(that);
+		                }
+		            });
+		            
+		            function submitReport(button){
+		                $('#submitButton').prop('disabled', true);
+		                $('#submit_throbber').css('display', 'inline-block');
+		                $.get('$wgServer$wgScriptPath/index.php/Special:Report?report={$this->getReport()->xmlName}{$projectGet}{$year}&submitReport&tok=' + $(button).val() ,function(data){
+		                    $('.submit_status_cell').css('background', '#008800');
+		                    $('.submit_status_cell').html('<b>Yes</b>');
+		                    $('#submitButton').removeAttr('disabled');
+		                    $('#submit_throbber').css('display', 'none');
+		                });
+		            }
+		        });
+		    </script>");
+		}
+		$wgOut->addHTML("<script type='text/javascript'>
+		    function clickButton(button){
+                $('#pdf_download_frame').attr('src',  '{$wgServer}{$wgScriptPath}/index.php/Special:ReportArchive?getpdf=' + button.name);
+            }
+		</script>");
+		$disabled = "";
+		if($wgImpersonating && !checkSupervisesImpersonee()){
+		    $disabled = "disabled='true'";
+		}
+		$wgOut->addHTML("<h3>1. Generate a new PDF</h3>");
+		$wgOut->addHTML("<p>Generate a PDF with the data submitted: <button id='generateButton' $disabled>Generate PDF</button><img id='generate_throbber' style='display:none;vertical-align:-20%;' src='../skins/Throbber.gif' /><br />
+		                    <div style='display:none;' class='error' id='generate_error'></div><div style='display:none;' class='success' id='generate_success'></div></p>");
+
+		$wgOut->addHTML("<h3>2. Download the PDF</h3>");
+		
+		$gmt_date = date('P');
+		$temp_html =<<<EOF
+		<p><table cellspacing='5'>
+        <tr>
+        	<th align='left'><span style="font-size:8pt; font-family:monospace;">Identifier</span><br />Generated (GMT {$gmt_date})</th>
+        </tr>
+EOF;
+
+		$wgOut->addHTML($temp_html);
+		$pdfcount = 1;
+		$wgOut->addHTML("<iframe id='pdf_download_frame' style='position:absolute;top:-1000px;left:-1000px;width:1px;height:1px;'></iframe>");
+        foreach($this->getReport()->pdfFiles as $file){
+            $tok = false;
+            $tst = '';
+            $sub = 0;
+            $sto = new ReportStorage($person);
+            $project = Project::newFromId($this->projectId);
+            $report = new DummyReport($file, $person, $project);
+        	$check = $report->getPDF();
+        	if (count($check) > 0) {
+        		$tok = $check[0]['token']; 	
+        		$tst = $check[0]['timestamp'];
+        		$sub = $check[0]['submitted'];
+        	}
+        	
+        	// Present some data on available reports.
+        	$style1 = "";
+        	if ($tok === false) {
+        		// No reports available.
+        		$style1 = "disabled='disabled'";
+        	}
+
+		    if($tok === false){
+		    	$show_pdf = "No PDF has been generated yet";
+		    }else{
+		    	$show_pdf = "<br />".$tst;
+		    }
+
+
+		    $subm_table_row =<<<EOF
+		    <tr>
+		    <td>
+		    	<span style="font-size:8pt; font-family: monospace;" id='ex_token_{$file}'>{$tok}</span>
+		    	<span id='ex_time_{$file}'>{$show_pdf}</span></td>
+            <td>
+            	<button id='download_button_{$file}' name='{$tok}' onClick='clickButton(this)' {$style1}>{$report->name} PDF</button>
+            </td>
+EOF;
+
+            $subm_table_row .= "</tr>";
+
+            $wgOut->addHTML($subm_table_row);
+            $pdfcount++;
+        }
+
+        
+        $wgOut->addHTML("</table></p>");
+	}
+	
+	function renderForPDF(){
+	    global $wgOut;
+	    $wgOut->addHTML($this->processCData(""));
+	}
+}
+
+?>
