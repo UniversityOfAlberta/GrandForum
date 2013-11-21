f192d81f57cf7f613411b8bd0cc0907e0f34dd2d
Finalfantasykid <finalfantasykid13@gmail.com>
2013-11-21 12:13:30 -0700

    -Fixed duplicate entries in the NIReportItemSet, and sort them by last name -Added the roles to the rollup table
    Former-commit-id: f4b3416f79af4e9f798b09b49f7d62e624fb94e9

--- extensions/Report/ReportItems/ReportItemSets/NIReportItemSet.php
+++ extensions/Report/ReportItems/ReportItemSets/NIReportItemSet.php
@@ -6,7 +6,12 @@ class NIReportItemSet extends ReportItemSet {
         $data = array();
         $people = array_merge(Person::getAllPeopleDuring('PNI', REPORTING_CYCLE_START, REPORTING_CYCLE_END),
                               Person::getAllPeopleDuring('CNI', REPORTING_CYCLE_START, REPORTING_CYCLE_END));
+        $finalPeople = array();
         foreach($people as $person){
+            $finalPeople[$person->getName()] = $person;
+        }
+        ksort($finalPeople);
+        foreach($finalPeople as $person){
             $tuple = self::createTuple();
             $tuple['person_id'] = $person->getId();
             $data[] = $tuple;
