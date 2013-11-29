cd0e367c8f863e3a2ee8dc4b389f150e99cfb266
Finalfantasykid <finalfantasykid13@gmail.com>
2013-11-29 09:27:09 -0700

    -Allow the PersonProduct autocomplete to use hqp products
    Former-commit-id: 6a7365ecea6769594d363904d7c5b1e7c70b8db6

--- extensions/Report/ReportItems/ReportItemSets/PersonProductsReportItemSet.php
+++ extensions/Report/ReportItems/ReportItemSets/PersonProductsReportItemSet.php
@@ -5,7 +5,7 @@ class PersonProductsReportItemSet extends ReportItemSet {
     function getData(){
         $data = array();
         $person = Person::newFromId($this->personId);
-        $products = $person->getPapersAuthored('all', REPORTING_CYCLE_START, REPORTING_CYCLE_END);
+        $products = $person->getPapersAuthored('all', REPORTING_CYCLE_START, REPORTING_CYCLE_END, true);
         if(is_array($products)){
             foreach($products as $prod){
                 $tuple = self::createTuple();
