22885ef7a808a98493255417e7486ac52203c2f1
Finalfantasykid <finalfantasykid13@gmail.com>
2013-12-13 17:35:18 -0700

    -Fixed the date of the maintenance
    Former-commit-id: 6c1424636f9d284e6fd35d2cde8f7ed9165001cc

--- skins/cavendish.php
+++ skins/cavendish.php
@@ -409,7 +409,7 @@ class cavendishTemplate extends QuickTemplate {
 	                        DEVELOPMENT ($branchFull, $revIdFull)</div>";
 	            }
 	        }
-	            $wgMessage->addPurpleInfo("Important: The GRAND Forum will be down for maintenance from 1-2AM (MST), Sun, Dec. 14.");
+	            $wgMessage->addPurpleInfo("Important: The GRAND Forum will be down for maintenance from 1-2AM (MST), Sun, Dec. 15.");
 	        ?>
 		    <a name="top" id="contentTop"></a>
     <ul class="top-nav">
