00a4cdc5be6998571acfe3d307952151e61b589a
Finalfantasykid <finalfantasykid13@gmail.com>
2014-01-02 11:22:31 -0700

    -Allow easier extension of Reporting ranges
    Former-commit-id: 0ce0e8f7c0e6230e9f85fbae6b1ba04467b9edc2

--- extensions/Report/ReportItems/ReportItemSets/PersonProductsReportItemSet.php
+++ extensions/Report/ReportItems/ReportItemSets/PersonProductsReportItemSet.php
@@ -5,7 +5,7 @@ class PersonProductsReportItemSet extends ReportItemSet {
     function getData(){
         $data = array();
         $person = Person::newFromId($this->personId);
-        $products = $person->getPapersAuthored('all', REPORTING_CYCLE_START, REPORTING_CYCLE_END, true);
+        $products = $person->getPapersAuthored('all', REPORTING_CYCLE_START, REPORTING_CYCLE_END_ACTUAL, true);
         if(is_array($products)){
             foreach($products as $prod){
                 $tuple = self::createTuple();
