9346066351a6a490ed6b5ad2af89e09757e19756
Finalfantasykid <finalfantasykid13@gmail.com>
2014-01-06 11:16:34 -0700

    -Fixed a bug with who shows in the project dashboard
    Former-commit-id: e7330ec9f10723a40257913265288577b5dcd3f1

--- extensions/QueryableTable/DashboardTable/Arrays/ProjectPeopleNoLeadersArray.php
+++ extensions/QueryableTable/DashboardTable/Arrays/ProjectPeopleNoLeadersArray.php
@@ -2,18 +2,26 @@
 
     class ProjectPeopleNoLeadersArray extends GroupByArray {
         
-        function ProjectPeopleNoLeadersArray($table){
+        function ProjectPeopleNoLeadersArray($table, $params){
             $project = $table->obj;
-            $people = $project->getAllPeople();
+            if(isset($params[1]) && isset($params[2])){
+                $start = $params[1];
+                $end = $params[2];
+                $people = $project->getAllPeopleDuring(null, $start, $end);
+            }
+            else{
+                $people = $project->getAllPeople();
+            }
             foreach($people as $person){
-                if($person->isRole(PNI) || $person->isRole(CNI) || $person->isRole(AR)){
+                if((isset($params[1]) && isset($params[2]) && $person->isRoleDuring(PNI, $start, $end) || $person->isRole(CNI, $start, $end) || $person->isRole(AR, $start, $end)) || 
+                   ($person->isRole(PNI) || $person->isRole(CNI) || $person->isRole(AR))){
                     if(!$person->leadershipOf($project->getName())){
                         $this->array[] = $person->getName();
                     }
                 }
             }
         }
-        
+
     }
 
 ?>
--- extensions/QueryableTable/GroupByArray.php
+++ extensions/QueryableTable/GroupByArray.php
@@ -1,13 +1,13 @@
 <?php
     
-    abstract class GroupByArray {
-        
-        var $array = array();
-        
-        function getArray(){
-            return $this->array;
-        }
-        
+abstract class GroupByArray {
+    
+    var $array = array();
+    
+    function getArray(){
+        return $this->array;
     }
+    
+}
     
 ?>
