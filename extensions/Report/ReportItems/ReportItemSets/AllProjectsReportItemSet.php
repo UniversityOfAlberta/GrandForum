1fc2470db88f3ff19eecd3708bd52b0e14bca4c9
Finalfantasykid <finalfantasykid13@gmail.com>
2014-01-07 12:13:14 -0700

    -Fixed a bug with DashboardTable -Categorized Phase1/2 project pdfs
    Former-commit-id: 086a878c7be7648a6f7dc57f5893238dc3548af5

--- extensions/Report/ReportItems/ReportItemSets/AllProjectsReportItemSet.php
+++ extensions/Report/ReportItems/ReportItemSets/AllProjectsReportItemSet.php
@@ -4,9 +4,14 @@ class AllProjectsReportItemSet extends ReportItemSet {
     
     function getData(){
         $data = array();
+        $phase = ($this->getAttr("phase") != "") ? $this->getAttr("phase") : 0;
         $projects = Project::getAllProjects();
         foreach($projects as $project){
+            if($phase != 0 && $project->getPhase() != $phase){
+                continue;
+            } 
             if($project->getName() == "AD-NODE"){
+                // Special Project
                 continue;
             }
             $tuple = self::createTuple();
