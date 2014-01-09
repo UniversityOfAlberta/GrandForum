aa6bdf40cecbf55fcc86736658973f2dc668a081
Finalfantasykid <finalfantasykid13@gmail.com>
2014-01-09 11:05:53 -0700

    -Added EvalOptReport
    Former-commit-id: 8e5c2dec6ad8236dfa2a4493fa4cb1f834c3b333

--- extensions/Report/ReportItems/ReportItemSets/AllPeopleReportItemSet.php
+++ extensions/Report/ReportItems/ReportItemSets/AllPeopleReportItemSet.php
@@ -4,11 +4,23 @@ class AllPeopleReportItemSet extends ReportItemSet {
     
     function getData(){
         $data = array();
+        $roles = explode(",",$this->getAttr("roles", ""));
+        $start = $this->getAttr("start", REPORTING_CYCLE_START);
+        $end = $this->getAttr("end", REPORTING_CYCLE_END);
         $allPeople = Person::getAllPeople();
         foreach($allPeople as $person){
-            $tuple = self::createTuple();
-            $tuple['person_id'] = $person->getId();
-            $data[] = $tuple;
+            $found = true;
+            foreach($roles as $role){
+                if($role != "" && !$person->isRoleDuring($role, $start, $end)){
+                    $found = false;
+                    break;
+                }
+            }
+            if($found){
+                $tuple = self::createTuple();
+                $tuple['person_id'] = $person->getId();
+                $data[] = $tuple;
+            }
         }
         return $data;
     }
