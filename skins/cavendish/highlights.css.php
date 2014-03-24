ad22153f6df921ecfcfe5947f3dcaa2ed4fb1c5e
Finalfantasykid <finalfantasykid13@gmail.com>
2014-03-24 15:08:30 -0600

    -Forgot to include the highlights stylesheet
    Former-commit-id: 4ede989921f370e479222efcf7c9c56f2bd8cda9

---
+++ skins/cavendish/highlights.css.php
@@ -0,0 +1,92 @@
+<?php
+
+header('Content-Type: text/css');
+
+require_once("../../config/Config.php");
+
+$hl = $config->getValue("highlightColor");
+
+echo <<<EOF
+
+/* General */
+
+.highlights-text {
+    color: $hl !important;
+}
+
+.highlights-text-hover:hover {
+    color: $hl !important;
+}
+
+.highlights-background {
+    background: $hl !important;
+    font-color: #FFFFFF !important;
+}
+
+.highlights-background-hover:hover {
+    background: $hl !important;
+    font-color: #FFFFFF !important;
+}
+
+/* Input */
+
+.selected .highlights-tab, .highlights-tab:hover {
+    border-width: 3px 0 0 0;
+    border-style: solid;
+    border-color: $hl;
+    color: $hl !important;
+}
+
+input:focus, textarea:focus {
+    outline: none;
+    border: 1px solid $hl !important;
+	box-shadow: inset 0 0 2px $hl;
+    -moz-box-shadow: inset 0 0 2px $hl;
+    -webkit-box-shadow: inset 0 0 2px $hl;
+}
+
+input[type=button]:active, input[type=submit]:active, .button:active, .ui-button:active, .ui-state button:not(#cboxPrevious):not(#cboxNext):not(#cboxSlideshow):not(#cboxClose):not([disabled]):active {
+    color: $hl !important;
+}
+
+input[type=button]:hover, input[type=submit]:hover, .button:hover, .ui-button:hover, button:not(#cboxPrevious):not(#cboxNext):not(#cboxSlideshow):not(#cboxClose):not([disabled]):hover {
+    color: $hl !important;
+}
+
+/* JQuery UI */
+
+.ui-tabs .ui-tabs-nav li.ui-tabs-selected a{
+	color: $hl;
+}
+
+.ui-datepicker {
+    border: 1px solid $hl !important;
+}
+
+.ui-progressbar .ui-progressbar-value {
+    background: $hl url(../smoothness/images/pbar-done.gif);
+}
+
+.ui-tabs .ui-tabs-nav li:hover a{
+    color: $hl;
+}
+
+/* Other */
+
+.purpleInfo a, .purpleInfo a:visited, .inlinePurpleInfo a, .inlinePurpleInfo a:visited {
+    color: $hl;
+}
+
+.small_card:hover, .small_card_hover {
+    background: $hl !important;
+    color: #ffffff !important;
+}
+
+.qtip-light .qtip-content a:hover {
+    color: $hl;
+    text-decoration: none;
+}
+
+EOF;
+
+?>
