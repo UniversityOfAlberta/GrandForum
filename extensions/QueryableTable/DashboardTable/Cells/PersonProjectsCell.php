29d4e0c4483281dbb14f1aa072b549f23760d239
Finalfantasykid <finalfantasykid13@gmail.com>
2014-04-02 15:34:53 -0600

    -Changed (Deleted) to (Completed) on the dashboard cell
    Former-commit-id: a8789e3fe819a3bde44475912601749d92af49d9

--- extensions/QueryableTable/DashboardTable/Cells/PersonProjectsCell.php
+++ extensions/QueryableTable/DashboardTable/Cells/PersonProjectsCell.php
@@ -22,7 +22,7 @@ class PersonProjectsCell extends Cell{
     function render(){
         global $wgServer, $wgScriptPath;
         $project = Project::newFromName($this->value);
-        $deleted = ($project->isDeleted()) ? " (Deleted)" : "";
+        $deleted = ($project->isDeleted()) ? " (Completed)" : "";
         return "<a href='{$project->getUrl()}' target = '_blank'><b>{$project->getName()}{$deleted}</b></a>";
     }
 }
