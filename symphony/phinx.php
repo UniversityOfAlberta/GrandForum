aa7c51572e3a1cd230a6f8e688b051dd838d7548
Finalfantasykid <finalfantasykid13@gmail.com>
2014-04-04 14:27:31 -0600

    -Forgot to add phinx.php
    Former-commit-id: a2b31cbec7a4d0a250d6ef121b23c0d9e714116c

---
+++ symphony/phinx.php
@@ -0,0 +1,21 @@
+<?php
+require_once('../config/Config.php');
+
+return array(
+    "paths" => array(
+        "migrations" => "./migrations"
+    ),
+    "environments" => array(
+        "default_migration_table" => "phinxlog",
+        "default_database" => "dev",
+        "dev" => array(
+            "adapter" => $config->getValue('dbType'),
+            "host" => $config->getValue('dbServer'),
+            "name" => $config->getValue('dbName'),
+            "user" => $config->getValue('dbUser'),
+            "pass" => $config->getValue('dbPassword'),
+            "port" => 3306
+        )
+    )
+);
+?>
