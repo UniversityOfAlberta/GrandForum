66f998640dcf9bfadca66a79a53f464e0cf43297
Finalfantasykid <finalfantasykid13@gmail.com>
2014-04-25 15:36:21 -0600

    -Disable the maintenance message
    Former-commit-id: 35accf27ff98f9a1a9044aa7c9e15cd18e33e572

--- skins/cavendish.php
+++ skins/cavendish.php
@@ -409,7 +409,7 @@ class cavendishTemplate extends QuickTemplate {
 	                        DEVELOPMENT ($branchFull, $revIdFull)</div>";
 	            }
 	        }
-	            $wgMessage->addPurpleInfo("Important: The GRAND Forum will be down for maintenance from 1-2PM (MDT), Fri, Apr. 25.");
+	            //$wgMessage->addPurpleInfo("Important: The GRAND Forum will be down for maintenance from 1-2PM (MDT), Fri, Apr. 25.");
 	        ?>
 		    <a name="top" id="contentTop"></a>
     <ul class="top-nav">
