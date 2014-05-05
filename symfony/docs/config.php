b737928d6d19c490917a1d018a33ad035935c26a
Finalfantasykid <finalfantasykid13@gmail.com>
2014-05-05 11:49:54 -0600

    -Docs changes
    Former-commit-id: c0e6dee530112fd52af9c88144a74c140f9af0f9

---
+++ symfony/docs/config.php
@@ -0,0 +1,21 @@
+<?php
+
+use Sami\Sami;
+use Symfony\Component\Finder\Finder;
+
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->exclude('AnnokiControl')
+    ->exclude('AccessControls')
+    ->in('../extensions/')
+;
+
+return new Sami($iterator, array(
+    'title'                => 'The Forum',
+    'build_dir'            => '/local/data/www-root/html/docs',
+    'cache_dir'            => __DIR__.'/cache',
+    'default_opened_level' => 2
+));
+
+?>
