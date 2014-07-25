156e3d1906aa39de412a72a414f57dcd484faeff
Finalfantasykid <finalfantasykid13@gmail.com>
2014-07-25 12:27:35 -0600

    -Updated the import 2mm
    Former-commit-id: 8d9865eff0d107905abdc7807e5038461ef4c15c

--- maintenance/conference_materials/import_2mm.php
+++ maintenance/conference_materials/import_2mm.php
@@ -2,11 +2,11 @@
 require_once( "../commandLine.inc" );
 global $wgServer, $wgScriptPath;
 
-$current_year = "2013";
+$current_year = "2014";
 $target = "2mm";
 $type = "2MM";
-$date = "2013-05-14";
-$status = "Not Invited";
+$date = "2014-05-14";
+$status = "Published";
 
 $string = file_get_contents("csv/{$current_year}/{$target}.csv");
 $count = 0;
@@ -21,13 +21,9 @@ foreach(explode("\n", $string) as $line){
 
     $file = $split[0];
     $title = mysql_escape_string($split[1]);
-    $authors_arr = explode(',', $split[2]);
     $authors = array();
-    foreach ($authors_arr as $a){
-        $authors[] = trim($a);
-    }
 
-    $projects_arr = explode(',', $split[3]);
+    $projects_arr = explode(',', $split[2]);
     $projects = array();
     foreach($projects_arr as $p){
         $projects[] = strtoupper(trim($p));
@@ -42,23 +38,29 @@ foreach(explode("\n", $string) as $line){
     }
 
     $data = array(
-            "organizing_body" => "GRAND NCE",
+            "publisher" => "GRAND NCE",
             "url"=>$file,
-            "event_title"=>"GRAND Annual Conference 2013",
-            "event_location"=> "Toronto, Canada",
+            "event_title"=>"GRAND Annual Conference 2014",
+            "event_location"=> "Ottawa, Canada",
             );
 
-    $sql = "INSERT INTO grand_products (`description`,`category`,`projects`,`type`,`title`,`date`,`venue`,`status`,`authors`,`data`)
-           VALUES ('','Presentation','".serialize($projects)."','{$type}','{$title}','{$date}','','{$status}','".serialize($authors)."','".serialize($data)."')";
+    $sql = "INSERT INTO grand_products (`description`,`category`,`type`,`title`,`date`,`venue`,`status`,`authors`,`data`)
+           VALUES ('','Presentation','{$type}','{$title}','{$date}','','{$status}','".serialize($authors)."','".serialize($data)."')";
 
     $result = DBFunctions::execSQL($sql, true);
     //echo mysql_insert_id(DBFunctions::$dbw);
-   /// var_dump(DBFunctions::$dbw->mConn);
+    /// var_dump(DBFunctions::$dbw->mConn);
     $last_id = DBFunctions::$dbw->insertId();
-
     
     if($result != 1){
         echo "ERROR: {$sql}\n";
+    }
+    else{
+        foreach($projects as $pname){
+            $project = Project::newFromName($pname);
+            $sql = "INSERT INTO grand_product_projects (`product_id`,`project_id`) VALUES ('{$last_id}','{$project->getId()}')";
+            DBFunctions::execSQL($sql, true);
+        }
     }
 
     echo "**[[Presentation:{$last_id} | {$title}]]\n";
