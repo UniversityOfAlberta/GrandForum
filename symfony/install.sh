a7d68722b34f7d0c5bdd5fe2bee8e17d63dcf96b
Finalfantasykid <finalfantasykid13@gmail.com>
2014-08-26 15:14:30 -0600

    -Update version of selenium
    Former-commit-id: a0f829d1ab72e250f45623588cc796a71c49a807

--- symfony/install.sh
+++ symfony/install.sh
@@ -8,4 +8,4 @@ curl http://getcomposer.org/installer | php
 echo "This might take a while..."
 php composer.phar install --prefer-source
 echo "Downloading Selenium..."
-curl http://selenium.googlecode.com/files/selenium-server-standalone-2.38.0.jar > bin/selenium.jar
+curl http://selenium-release.storage.googleapis.com/2.42/selenium-server-standalone-2.42.2.jar > bin/selenium.jar
