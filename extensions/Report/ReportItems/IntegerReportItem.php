1a12a67f5fba0bdd03ac9ebe1a085aa39f4a54b1
Finalfantasykid <finalfantasykid13@gmail.com>
2014-08-27 11:15:14 -0600

    -Made the GRAND Time fields numeric
    Former-commit-id: 65a19d814872e1ec527e6a617bfe1b882ef2afad

---
+++ extensions/Report/ReportItems/IntegerReportItem.php
@@ -0,0 +1,20 @@
+<?php
+
+class IntegerReportItem extends TextReportItem {
+	
+	function render(){
+		global $wgOut;
+		$min = $this->getAttr('min');
+		$max = $this->getAttr('max');
+		$value = $this->getBlobValue();
+		$width = (isset($this->attributes['width'])) ? $this->attributes['width'] : "150px";
+		$item = "<input type='text' name='{$this->getPostId()}' style='width:{$width};' value='{$value}' />";
+		$item = $this->processCData($item);
+		$wgOut->addHTML("$item");
+		$wgOut->addHTML("<script type='text/javascript'>
+		    $('input[name={$this->getPostId()}]').forceNumeric({min: $min, max: $max});
+		</script>");
+	}
+}
+
+?>
