2e360215a22e6a19a562c31dce99a8fc09c61ec2
Finalfantasykid <finalfantasykid13@gmail.com>
2014-09-15 11:14:09 -0600

    -Updated the Project Report PDF -Fixed a css issue with the report
    Former-commit-id: 7d2aba7180143646196314b64cda1b114f27e2ad

--- Classes/dompdf/dompdf_config.custom.inc.php
+++ Classes/dompdf/dompdf_config.custom.inc.php
@@ -8,7 +8,7 @@
 //define("DOMPDF_DEFAULT_FONT", "serif");
 //define("DOMPDF_DPI", 72);
 //define("DOMPDF_ENABLE_PHP", true);
-//define("DOMPDF_ENABLE_REMOTE", true);
+define("DOMPDF_ENABLE_REMOTE", true);
 //define("DOMPDF_ENABLE_CSS_FLOAT", true);
 //define("DOMPDF_ENABLE_JAVASCRIPT", false);
 //define("DEBUGPNG", true);
