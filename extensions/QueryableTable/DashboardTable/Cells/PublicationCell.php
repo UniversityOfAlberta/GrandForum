8c11926a8aca68be8a39801be1d8cfea7e481d78
Finalfantasykid <finalfantasykid13@gmail.com>
2014-09-17 14:27:10 -0600

    -Publication Details Projects row should only go up to 50% width -Fix line break issue with PDF
    Former-commit-id: aab48a1818da8f431c1eb3585a8447bf940bb015

--- extensions/QueryableTable/DashboardTable/Cells/PublicationCell.php
+++ extensions/QueryableTable/DashboardTable/Cells/PublicationCell.php
@@ -164,7 +164,7 @@ abstract class PublicationCell extends DashboardCell {
                 $stat = "{$status} / ";
             }
         }
-        $details = "<td style='white-space:nowrap;text-align:left;' class='pdfnodisplay'>{$paper->getDate()}</td><td style='text-align:left;' class='pdfnodisplay'>".implode(", ", $projs)."</td><td class='pdfnodisplay' style='text-align:left;'>{$first_author}{$hqpAuthored}</td><td style='width:50%;text-align:left;'>{$citation}<div class='pdfOnly' style='width:100%;text-align:right;'><i>{$stat}".implode(", ", $projs)."</i></div></td>\n";
+        $details = "<td style='white-space:nowrap;text-align:left;' class='pdfnodisplay'>{$paper->getDate()}</td><td style='text-align:left;' class='pdfnodisplay'>".implode(", ", $projs)."</td><td class='pdfnodisplay' style='text-align:left;'>{$first_author}{$hqpAuthored}</td><td style='width:50%;text-align:left;'>{$citation}<div class='pdfOnly' style='width:50%;margin-left:50%;text-align:right;'><i>{$stat}".implode(", ", $projs)."</i></div></td>\n";
         return $details;
     }
     
