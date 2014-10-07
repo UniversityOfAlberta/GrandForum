8cf6fa9ca9077bc417dae951d8ed9f237ba0f1cb
Finalfantasykid <finalfantasykid13@gmail.com>
2014-10-07 13:45:54 -0600

    -Added a migration to change the enum of milestones
    Former-commit-id: 7b02b8357a472faa2ea4dfd4cc1dc5bbbca8e7f1

---
+++ symfony/migrations/20141007134147_milestones_completed.php
@@ -0,0 +1,36 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class MilestonesCompleted extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+    */
+    
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $sql = "ALTER TABLE `grand_milestones` CHANGE `status` `status` ENUM( 'New', 'Revised', 'Continuing', 'Current', 'Closed', 'Completed', 'Abandoned') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL";
+        $this->execute($sql);
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+
+    }
+}
