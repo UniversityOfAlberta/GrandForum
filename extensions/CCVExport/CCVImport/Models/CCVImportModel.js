5442fd49ac0ba4d87bc4700a5058ee2139a358d5
Finalfantasykid <finalfantasykid13@gmail.com>
2014-12-15 09:42:53 -0700

    -Forgot to add the CCVImport directory
    Former-commit-id: 65ca773c178f3fd2c1955a7e817901ad076b3cd8

---
+++ extensions/CCVExport/CCVImport/Models/CCVImportModel.js
@@ -0,0 +1,11 @@
+CCVImportModel = Backbone.Model.extend({
+
+    initialize: function(){
+        
+    },
+
+    defaults: {
+        person: null
+    }
+    
+})
---
+++ extensions/CCVExport/CCVImport/Views/CCVImportView.js
@@ -0,0 +1,57 @@
+CCVImportView = Backbone.View.extend({
+
+    template: _.template($("#ccv_import_template").html()),
+
+    initialize: function(options){
+        this.people = new People();
+        this.listenTo(this.people, "sync", $.proxy(function(){
+            this.render();
+        }, this));
+        this.people.fetch();
+        this.model.on("change:person", this.render);
+    },
+    
+    changeSelection: function(){
+        var person = this.people.findWhere({id: this.$("#member-select").val()});;
+        this.model.set('person', person);
+    },
+    
+    upload: function(){
+        var button = $("#upload");
+        button.prop("disabled", true);
+        ccvUploaded = $.proxy(function(response, error){
+            // Purposefully global so that iframe can access
+            if(error == undefined || error == ""){
+                clearAllMessages();
+                var nCreated = response.created.length;
+                var nError = response.error.length;
+                if(nCreated > 0){
+                    addSuccess("<b>" + nCreated + "</b> products were created");
+                }
+                if(nError > 0){
+                    addInfo("<b>" + nError + "</b> products were ignored (probably duplicates)");
+                }
+                button.prop("disabled", false);
+            }
+            else{
+                button.prop("disabled", false);
+                clearAllMessages();
+                addError(error);
+            }
+        }, this);
+        var form = this.$("form");
+        form.submit();
+    },
+    
+    events: {
+        "change #member-select": "changeSelection",
+        "click #upload": "upload"
+    },
+    
+    render: function(){
+        this.$el.html(this.template(this.model.toJSON()));
+        this.$("#member-select").chosen();
+        return this.$el;
+    }
+
+});
