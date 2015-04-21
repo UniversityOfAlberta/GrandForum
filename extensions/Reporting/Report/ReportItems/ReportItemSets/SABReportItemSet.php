2aee840d94147490f92392db11b47c8acc31ffbb
Finalfantasykid <finalfantasykid13@gmail.com>
2015-04-21 10:45:36 -0600

    -Added a date report item set which changes when certain sections of the report are visible
    Former-commit-id: 1786ed0b68ed011ea6fef3e4326cabe9e0f17519

--- extensions/Reporting/Report/ReportItems/ReportItemSets/SABReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/SABReportItemSet.php
@@ -3,15 +3,18 @@
 class SABReportItemSet extends ReportItemSet {
     
     function getData(){
+        $me = Person::newFromWgUser();
         // Returns the array of SAB for that year
         $data = array();
         $proj = Project::newFromId($this->projectId);
         if($proj != null){
             $evaluators = $proj->getEvaluators($this->getReport()->year, 'SAB');
             foreach($evaluators as $e){
-                $tuple = self::createTuple();
-                $tuple['person_id'] = $e->getId();
-                $data[$e->getReversedName()] = $tuple;
+                if($e->getId() != $me->getId()){
+                    $tuple = self::createTuple();
+                    $tuple['person_id'] = $e->getId();
+                    $data[$e->getReversedName()] = $tuple;
+                }
             }
             ksort($data);
         }
