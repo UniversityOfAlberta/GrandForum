8a5c677c24aec0a6a2acdeb2c275b1be2c047e65
Finalfantasykid <finalfantasykid13@gmail.com>
2015-04-23 13:09:18 -0600

    -Added some more common words
    Former-commit-id: eaf4c89922a4cd610f4b9f305dd93a943c14359e

--- Classes/removeCommonWords/removeCommonWords.php
+++ Classes/removeCommonWords/removeCommonWords.php
@@ -67,7 +67,7 @@ class CommonWords {
 	                     'without','wonder','won\'t','would','wouldn\'t','x','y','yes','yet','you','you\'d','you\'ll',
 	                     'your','you\'re','yours','yourself','yourselves','you\'ve','z','zero','university','professor',
 	                     'undergraduate','masters','phd','group','research','role','fellow','fellowship','postdoctoral',
-	                     'postdoc','subsequently');
+	                     'postdoc','subsequently','alberta','department','tenure','including','associate','received','position');
 
     static function removeCommonWords($input){
 		$input = preg_replace('/\b('.implode('|',self::$commonWords).')\b/','',$input);
