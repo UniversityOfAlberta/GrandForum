215314a9a69c1038cc408f7285c0a596c934b1d8
Finalfantasykid <finalfantasykid13@gmail.com>
2015-04-27 10:36:28 -0600

    -Changed the terminology of themes/admin projects
    Former-commit-id: 906b80be1e0056e1b0529754e3f84ca8c83385ea

--- config/default_config.php
+++ config/default_config.php
@@ -64,6 +64,12 @@
     // Global Message (ie. maintenance message)
     $config->setValue("globalMessage", "");
     
+    // The terminology for project themes 
+    $config->setValue("projectThemes", "Theme");
+    
+    // The terminology for administrative projects 
+    $config->setValue("adminProjects", "Administrative");
+    
     // Whether or not to allow bigBetProjects
     $config->setValue("bigBetProjects", false);
     
