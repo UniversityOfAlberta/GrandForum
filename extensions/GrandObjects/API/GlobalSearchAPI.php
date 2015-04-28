52b820384f1ff8133908dd93817e1339629bfb07
Finalfantasykid <finalfantasykid13@gmail.com>
2015-04-28 09:55:08 -0600

    -Don't include Admin in search results
    Former-commit-id: 765494537f44ffe2e241909e36813a9d4fa7acf0

--- extensions/GrandObjects/API/GlobalSearchAPI.php
+++ extensions/GrandObjects/API/GlobalSearchAPI.php
@@ -46,6 +46,10 @@ class GlobalSearchAPI extends RESTAPI {
                 $people = Person::getByIds($dataCollection->pluck('user_id'));
                 foreach($people as $person){
                     $continue = false;
+                    if($person->getName() == "Admin"){
+                        // Don't include Admin
+                        $continue = true; 
+                    }
                     if(!$me->isLoggedIn() && !$person->isRoleAtLeast(CNI)){
                         $continue = true;
                     }
