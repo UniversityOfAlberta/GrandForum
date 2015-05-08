6aaca63fdca078d1b8977ab01e3991e3fcae2e5a
Finalfantasykid <finalfantasykid13@gmail.com>
2015-05-08 14:20:59 -0600

    -Don't show 'Manage Products' on the dashboard table -Improved the appearance of the citations
    Former-commit-id: 3101e7350022c5110e4bfacb13bbb0dcfd7c44be

--- extensions/GrandObjects/Paper.php
+++ extensions/GrandObjects/Paper.php
@@ -1068,7 +1068,7 @@ class Paper extends BackboneModel{
         $data = $this->getData();
         $type = $this->getType();
         $title = $this->getTitle();
-        $status = ($showStatus) ? "&nbsp;".$this->getStatus() : "";
+        $status = ($showStatus) ? $this->getStatus() : "";
         $category = $this->getCategory();
         $au = array();
         foreach($this->getAuthors() as $a){
@@ -1131,10 +1131,10 @@ class Paper extends BackboneModel{
         $peer_rev = "";
         if($showPeerReviewed && $category == "Publication"){
             if(isset($data['peer_reviewed']) && $data['peer_reviewed'] == "Yes"){
-                $peer_rev = ",&nbsp;Peer Reviewed";
+                $peer_rev = "&nbsp;/&nbsp;Peer Reviewed";
             }
             else if(isset($data['peer_reviewed']) && $data['peer_reviewed'] == "No"){
-                $peer_rev = ",&nbsp;Not Peer Reviewed";
+                $peer_rev = "&nbsp;/&nbsp;Not Peer Reviewed";
             }
         }
 
@@ -1146,26 +1146,19 @@ class Paper extends BackboneModel{
         }
         $date = date("Y M", strtotime($this->getDate()));
         $type = str_replace("Misc: ", "", $type);
-        if(in_array($type, array('Book', 'Book Chapter', 'Collections Paper', 'Proceedings Paper', 'Journal Paper'))){
-            if($pg != "" || $pb != ""){
-                $vn .= ",";
-            }
+        if( in_array($type, array('Book', 'Book Chapter', 'Collections Paper', 'Proceedings Paper', 'Journal Paper'))){
             if($vn != "" || $pg != "" || $pb != ""){
                 $vn = ":&nbsp;$vn";
             }
-            if(($pg != "" || $pb != "") && ($status != "" || $peer_rev != "")){
-                $pb .= "<span class='pdfnodisplay'>,</span>";
-            }
-       		$citation = "{$au}&nbsp;({$date}).&nbsp;<i>{$text}.</i>&nbsp;{$type}{$vn}&nbsp;{$pg}&nbsp;{$pb}&nbsp;<span class='pdfnodisplay'>{$status}{$peer_rev}</span>";
+       		$citation = "{$au}&nbsp;({$date}).&nbsp;<i>{$text}.</i>&nbsp;{$type}{$vn},&nbsp;{$pg}&nbsp;{$pb}
+       		             <div class='pdfnodisplay' style='width:85%;margin-left:15%;text-align:right;'>{$status}{$peer_rev}</div>";
     	}
     	else{
     	    if($vn != ""){
     	        $vn = ":&nbsp;$vn";
             }
-            if($status != "" || $peer_rev != ""){
-                $vn .= "<span class='pdfnodisplay'>,</span>";
-            }
-        	$citation = "{$au}&nbsp;({$date}).&nbsp;<i>{$text}.</i>&nbsp;{$type}{$vn}<span class='pdfnodisplay'>{$status}{$peer_rev}</span>";
+        	$citation = "{$au}&nbsp;({$date}).&nbsp;<i>{$text}.</i>&nbsp;{$type}{$vn}
+        	             <div class='pdfnodisplay' style='width:85%;margin-left:15%;text-align:right;'>{$status}{$peer_rev}</div>";
         }
         return trim($citation);
     }
