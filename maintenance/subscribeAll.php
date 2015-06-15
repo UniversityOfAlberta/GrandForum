19b33397b6c6369b788ac47db1e757de8a074adf
Finalfantasykid <finalfantasykid13@gmail.com>
2015-06-15 12:44:10 -0600

    -Updated the subscribeAll command
    Former-commit-id: 3bf2867d9ffd270dffa244e0521c497f5c2e17e0

--- maintenance/subscribeAll.php
+++ maintenance/subscribeAll.php
@@ -1,20 +1,10 @@
 <?php
 	require_once( 'commandLine.inc' );
-	
+	global $wgUser;
+	$wgUser = User::newFromId(1);
 	$people = Person::getAllPeople();
 	
 	foreach($people as $person){
-	    $projects = $person->getProjects();
-	    foreach($projects as $project){
-	        if($project != null){
-	            $status = MailingList::subscribe($project, $person);
-	            if($status == true){
-	                echo "{$person->getName()} added to {$project->getName()} mailing list\n";
-	            }
-	            else if($status == false){
-	                echo "{$person->getName()} not added to {$project->getName()} mailing list\n";
-	            }
-	        }
-	    }
+	    MailingList::subscribeAll($person);
 	}
 ?>
