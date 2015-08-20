fe0e0f6a5f193b5c12acf9026d399c2ff1c647e9
Finalfantasykid <finalfantasykid13@gmail.com>
2015-08-20 11:09:28 -0600

    -Fixed an extra quote
    Former-commit-id: a913c0366b8a7cc04410db7ac262b5f3c76d959a

--- extensions/Reporting/Report/ReportItems/UploadReportItem.php
+++ extensions/Reporting/Report/ReportItems/UploadReportItem.php
@@ -28,7 +28,7 @@ class UploadReportItem extends AbstractReportItem {
                             </script>";
         $html .= "<div>";
         
-        $html .= "<div id='budgetDiv'><iframe id='fileFrame{$this->getPostId()}' frameborder='0' style='border-width:0;height:65px;width:100%;min-height:65px;'' scrolling='none' src='../index.php/Special:Report?report={$report->xmlName}&section=".urlencode($section->name)."&fileUploadForm={$this->getPostId()}{$projectGet}{$year}'></iframe></div>";
+        $html .= "<div id='budgetDiv'><iframe id='fileFrame{$this->getPostId()}' frameborder='0' style='border-width:0;height:65px;width:100%;min-height:65px;' scrolling='none' src='../index.php/Special:Report?report={$report->xmlName}&section=".urlencode($section->name)."&fileUploadForm={$this->getPostId()}{$projectGet}{$year}'></iframe></div>";
         $html .= "</div>";
         
         $item = $this->processCData($html);
