fba62b8baad76f654bb60ce96d887251de949a22
Finalfantasykid <finalfantasykid13@gmail.com>
2015-09-15 13:35:51 -0600

    -Fixed more characters in PDF
    Former-commit-id: 21f45fcbccc2062f11e5816ec7db5851f54d09c5

--- extensions/Reporting/PDFGenerator/PDFGenerator.php
+++ extensions/Reporting/PDFGenerator/PDFGenerator.php
@@ -167,6 +167,7 @@ abstract class PDFGenerator {
         $str = str_replace("&#8209;", "-", $str);
         $str = str_replace("&#61485;", "~", $str);
         $str = str_replace("&#8208;", "-", $str);
+        $str = str_replace("‐", "-", $str);
         /*preg_match_all("/<strong>(.*?)<\/strong>/", $str, $matches);
         foreach($matches[1] as $match){
             $match1 = str_replace(" ", "</strong> &nbsp;<strong>", $match);
