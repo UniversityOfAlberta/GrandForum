52d7944dec1422c4746a5ce76fb46b5e07106ed2
Finalfantasykid <finalfantasykid13@gmail.com>
2015-09-15 10:38:51 -0600

    -Changed the wording of the HQP Profile Tab
    Former-commit-id: 7e03fe8a77eb0ec9e925d88bd74e69c9adf1d7cd

--- extensions/GrandObjectPage/PersonPage/HQPProfileTab.php
+++ extensions/GrandObjectPage/PersonPage/HQPProfileTab.php
@@ -66,19 +66,17 @@ class HQPProfileTab extends AbstractEditableTab {
         $bio      = $this->getBlobValue(HQP_APPLICATION_BIO);
         $align    = $this->getBlobValue(HQP_APPLICATION_ALIGN);
         $boundary = $this->getBlobValue(HQP_APPLICATION_BOUNDARY);
-        
-        $this->html .= "<h2>Core Research Program Highly Qualified Personnel Application</h2>";
-        $this->html .= "<h3>Statement of Research Focus</h3>
+
+        $this->html .= "<h3>Statement of Research Focus (for HQP completing a research program) (½ page)</h3>
 <small>
-    <p>AGE-WELL is inviting trainees and other highly qualified personnel in the broad area of technology and aging. The research project may be linked to one of AGE-WELL’s eight Research Workpackages, but does not need to limited to these.  Research should address at least one of our key research questions: </p>
+    <p>Your research project may be linked to one of AGE-WELL’s eight Research Workpackages, but does not need to limited to these. Research should address at least one of our key research questions:</p>
     <ol>
         <li>What are the needs of older adults and caregivers?</li>
         <li>What technology-based systems and services should be used to meet those needs?</li>
         <li>How can we foster innovation in the technology and aging sector?</li>
     </ol>
     <p>
-    The project may also support one of AGE-WELL’s four Crosscutting activities- Knowledge Mobilization, Commercialization, Transdisciplinarity and HQP Training. 
-    </p>
+    The project may also support one of AGE-WELL’s four Crosscutting activities - Knowledge Mobilization, Commercialization, Transdisciplinarity and HQP Training.</p>
     <p>In this section, applicants should describe:</p>
     <ol>
         <li>How does your research connect to research foci of the AGE-WELL NCE?</li>
@@ -88,10 +86,8 @@ class HQPProfileTab extends AbstractEditableTab {
     </ol>
 </small>";
         $this->html .= "<textarea name='research' style='height:200px;'>{$research}</textarea>";
-        $this->html .= "<h3>Statement of Training Focus</h3>
+        $this->html .= "<h3>Statement of Training Focus (for research associates and trainees in professional programs) (½ page)</h3>
 <small>
-    <p>AGE-WELL is inviting trainees and other highly qualified personnel in the broad area of technology and aging, including students in professional programs (i.e. non-thesis bases programs) such as gerontology and allied health. The training program and the work being undertaken may be linked to one of AGE-WELL’s eight Research Workpackages, but does not need to limited to these.</p>
-
     <p>In this section, applicants should describe:</p>
     <ol>
         <li>How does your training program and/or background relate to the AGE-WELL NCE?</li>
@@ -99,7 +95,7 @@ class HQPProfileTab extends AbstractEditableTab {
     </ol>
 </small>";
         $this->html .= "<textarea name='train' style='height:200px;'>{$train}</textarea>";
-        $this->html .= "<h3>Biography and Career Goals</h3>
+        $this->html .= "<h3>Biography and Career Goals (½ page)</h3>
 <small>
     <p>In this section briefly describe:</p>
     <ol>
@@ -109,16 +105,16 @@ class HQPProfileTab extends AbstractEditableTab {
     </ol>
 </small>";
         $this->html .= "<textarea name='bio' style='height:200px;'>{$bio}</textarea>";
-        $this->html .= "<h3>Alignment of research, training, and/or career goals to the mission and goals of AGE-WELL</h3>
+        $this->html .= "<h3>Alignment of research, training, and/or career goals to the mission and goals of AGE-WELL (½ page)</h3>
 <small>
-    <p>Successful applications will need to be aligned with the key strategic goals of AGE-WELL. Applicants are strongly encouraged to familiarize themselves with the document called AGE-WELL Network Goals available on the <a target='_blank' href='{$config->getValue('networkSite')}'>AGE-WELL website</a></p>
+    <p>AGE-WELL HQP will need to be aligned with the key strategic goals of AGE-WELL and are strongly encouraged to familiarize themselves with the document called AGE-WELL Network Goals available on the <a target='_blank' href='{$config->getValue('networkSite')}'>AGE-WELL website</a></p>
 
-<p>Applicants will need to demonstrate that their research, training, and/or career goals has potential for real world impact.  In this section, describe how these goals fits with AGE-WELL’s vision and strategic goals.</p>
+<p>HQP will need to demonstrate that their research, training, and/or career goals has potential for real world impact. In this section, describe how these goals fit with AGE-WELL’s vision and strategic goals.</p>
 </small>";
         $this->html .= "<textarea name='align' style='height:200px;'>{$align}</textarea>";
-        $this->html .= "<h3>In what ways are you interested in going beyond conventional disciplinary boundaries?</h3>
+        $this->html .= "<h3>In what ways are you interested in going beyond conventional disciplinary boundaries (½ page)</h3>
 <small>
-    <p>Transdisciplinary working - that is working across and with other disciplines than your own – is an important aspect of AGE-WELL.  In this section please address the following:</p>
+    <p>Transdisciplinary working - that is working across and with other disciplines than your own – is an important aspect of AGE-WELL. In this section please address the following:</p>
     <ol>
         <li>Describe networking that may occur across disciplines and sites within AGE-WELL.</li>
         <li>How does your work and goals link with other projects/activities in the AGE-WELL Network?</li>
