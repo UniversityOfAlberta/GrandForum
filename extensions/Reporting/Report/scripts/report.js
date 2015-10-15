26f5dd9cb760dc2764845bb5abbb33d03cd71152
Finalfantasykid <finalfantasykid13@gmail.com>
2015-10-15 11:36:09 -0600

    -Don't mess up the input when a conflict is resolved
    Former-commit-id: c3b01ee9bf18ffbb13cb78e935409c269a295b75

--- extensions/Reporting/Report/scripts/report.js
+++ extensions/Reporting/Report/scripts/report.js
@@ -158,10 +158,10 @@ function showConflictError(data){
     alert("Some of the fields could not save.  A change was made to at least one of the fields after this section was loaded.  This may have been caused by multiple browser sessions being opened at the same time.  Please go and close any other browser or tabs that you have open to the " + networkName + " reporting page.\n\nAfter clicking 'Ok', you will be returned to the conflicting section and the conflicting fields will be highlighted.  Any field which was not conflicting, has been saved already.");
     for(index in data){
         var postId = data[index].postId;
-        var value = data[index].value;
-        var oldValue = data[index].oldValue;
-        var postValue = data[index].postValue;
-        var diff = data[index].diff;
+        var value = _.unescape(data[index].value);
+        var oldValue = _.unescape(data[index].oldValue);
+        var postValue = _.unescape(data[index].postValue);
+        var diff = _.unescape(data[index].diff);
         var obj = $("[name=" + postId + "]:last");
         var isRadio = false;
         
