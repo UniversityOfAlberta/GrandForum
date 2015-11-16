734a212f53c3f1d979b5955ec62a2a3d1bc0720b
Finalfantasykid <finalfantasykid13@gmail.com>
2015-11-16 11:10:29 -0700

    -Made a change to the HQP Epic Tab
    Former-commit-id: 9dc16fc35d81d02d6e0077eb4215f868732f1e0d

--- extensions/GrandObjectPage/PersonPage/HQPEpicTab.php
+++ extensions/GrandObjectPage/PersonPage/HQPEpicTab.php
@@ -55,15 +55,28 @@ class HQPEpicTab extends AbstractEditableTab {
     function generatePhD(){
         $this->html .= "<div style='font-size:1.2em;text-align:center;'><h3>AGE-WELL EPIC Program Requirements For Doctoral Students</h3>
                         <b>E</b>arly <b>P</b>rofessionals, <b>I</b>nspired <b>C</b>areers</div>
-                        <p>As an AGE-WELL HQP you have the opportunity to be truly EPIC.  You have unprecedented access to a wide variety of aging and technology resources and experts in a unique training environment based on scientific excellence and real-world applicability.</p>
-<p>By completing the EPIC Program and earning the AGE-WELL Innovators of Tomorrow certificate your CV/resume will speak to the full range of economic, social, and ethical implications of developing technologies for older adults through workshops, seminar series and other events and activities that transcend the typical training offered through academia.</p>
-<p>The core of our training program revolves around “Knowledge Partnerships” between higher education and industry, service, and public sectors working together towards mutually beneficial results and outcomes. To that end, training activities are available through AGE-WELL, industry and service partners, and through your institution. While a preliminary list of these opportunities is available on Forum, you are strongly encouraged to consider eligible activities available through your departments and institutions.</p>
+                        <p>As an AGE-WELL HQP you have the opportunity to be truly EPIC. You have unprecedented access to a wide variety of aging and technology resources and experts in a unique training environment based on scientific excellence and real-world applicability.</p>
+                        <p>By completing the EPIC Program and earning the AGE-WELL Innovators of Tomorrow certificate your CV/resume will speak to the full range of economic, social, and ethical implications of developing technologies for older adults through workshops, seminar series and other events and activities that transcend the typical training offered through academia.</p>
+                        <p>The core of our training program revolves around “Knowledge Partnerships” between higher education and industry, service, and public sectors working together towards mutually beneficial results and outcomes. To that end, training activities are available through AGE-WELL, industry and service partners, and through your institution. While a preliminary list of these opportunities is available on Forum (<a href='https://forum.agewell-nce.ca/index.php/AGE-WELL_Seminars' target='_blank'>AGE-WELL Seminars</a> and <a href='https://forum.agewell-nce.ca/index.php/HQP_Wiki:HQP_Resources' target='_blank'>HQP Resources</a> tab), you are strongly encouraged to consider eligible activities available through your departments and institutions.  A collected list of these is available on Forum <a href='https://forum.agewell-nce.ca/AnnokiUploadAuth.php/e/eb/Non_AGE-WELL_HQP_activities.pdf' target='_blank'>here</a>.</p>
 <p><b>Be EPIC - Earning the AGE-WELL Innovators of Tomorrow Certificate</b></p>
-<p>HQP award holders must complete the AGE-WELL EPIC Program during their award tenure.  There are four major steps:</p>
+<p>HQP must complete all of their EPIC requirements in one calendar year to earn the Innovators of Tomorrow Certificate. There are four major steps as outlined below.</p>
+<p><b>INSTRUCTIONS</b></p>
 <ol>
     <li><b>AGE-WELL ORIENTATION SESSION:</b> Attend session at the AGM or view on Forum.</li>
-    <li><b>CREATE AND SUBMIT A TRAINING PLAN:</b> Where would you like to build capacity?  Take a look at the minimum requirements checklist attached and the opportunities listed on Forum, consider additional activities you know of or those in which you are already involved. Using the attached checklist as your training plan, submit a list of the activities you would like to complete to the Education and Training Administrator via Forum and await approval. This needs to be approved by your principal supervisor and submitted within 2 months of accepting AGE-WELL funding</li>
-    <li><b>COMPLETE TRAINING PLAN:</b> Once your plan has been approved, complete your planned activities and let us know about it on Forum! If a planned activity is cancelled, made unavailable, or if you have found a more appropriate activity, please choose another, edit your Forum training plan accordingly, and submit for approval.</li>
+    <li><b>CREATE AND SUBMIT A TRAINING PLAN:</b> Where would you like to build capacity? Take a look at the minimum requirements checklist on this page and the opportunities listed on Forum, consider additional activities you know of or those in which you are already involved.
+        <ul>
+            <li>Use this checklist on your Forum EPIC tab as your training plan. To submit a list of activities you are interested in completing, please scroll to the bottom of the page and click on the “edit EPIC” button on the lower left corner.  Your EPIC table is now editable and all changes are submitted automatically to the Education and Training Administrator for review. </li>
+            <li>Once you have entered planned activities, please show your plan to your principal supervisor for approval.  You then may check the “Approved (Supervisor)” box.</li>
+            <li>Once your completed plan has been reviewed by the Education and Training Administrator, a check mark will appear in the “Completed (NMO)” box.</li>
+            <li>Training plans should be completed and submitted within 2 months of accepting AGE-WELL funding.</li>
+        </ul>
+    </li>
+    <li><b>COMPLETE TRAINING PLAN:</b> Once your plan has been approved, complete your planned activities and let us know about it on Forum! If a planned activity is cancelled, made unavailable, or if you have found a more appropriate activity, please choose another, edit your Forum training plan accordingly.
+        <ul>
+            <li>Please check the appropriate “Completed (HQP)” box when you have completed an activity and retain evidence of completion/participation for the final annual report.</li>
+            <li>Evidence of activity completion or participation may include: a screenshot, an abstract, conference proceedings, emails, a letter from your supervisor to name a few.</li>
+        </ul>
+    </li>
     <li><b>COMPLETE AND SUBMIT ANNUAL REPORT:</b> After completing your activities, submit via Forum an annual report that includes your completed checklist, evidence of satisfactory completion of your planned activities, and a brief evaluation of the activities chosen in terms of learning outcomes.</li>
 </ol>";
         $this->html .= "<table class='wikitable'>
