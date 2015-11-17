3e7fcd6f2f2a1ef9422f308e750a466c4fc5d835
Finalfantasykid <finalfantasykid13@gmail.com>
2015-11-17 09:24:49 -0700

    -Changed the behavior of the Progress Report
    Former-commit-id: 20048b75dd86d29b659d1321eb6958015bcef68c

--- extensions/Reporting/Report/ReportItems/ReportItemSets/ProjectPeopleReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/ProjectPeopleReportItemSet.php
@@ -8,10 +8,12 @@ class ProjectPeopleReportItemSet extends ReportItemSet {
         $proj_id = $this->projectId;
         $role = $this->getAttr("role", NI);
         $roles = explode(",", $role);
+        $start = $this->getAttr("startDate", REPORTING_CYCLE_START);
+        $end = $this->getAttr("endDate", REPORTING_CYCLE_END_ACTUAL);
         if($proj != null){
             $members = array();
             foreach($roles as $role){
-                foreach($proj->getAllPeopleDuring($role, REPORTING_CYCLE_START, REPORTING_CYCLE_END_ACTUAL) as $person){
+                foreach($proj->getAllPeopleDuring($role, $start, $end) as $person){
                     $members[$person->getReversedName()] = $person;
                 }
             }
