7e0178a060a02a9c40a189dcc133dd9633a6d542
Finalfantasykid <finalfantasykid13@gmail.com>
2015-11-19 11:07:59 -0700

    -Added a message about checking junk mail
    Former-commit-id: a149be52f9d43d8ef9cb130a75861964d37a9c17

--- extensions/HQPRegister/HQPRegister.php
+++ extensions/HQPRegister/HQPRegister.php
@@ -94,7 +94,7 @@ class HQPRegister extends SpecialPage{
      function generateFormHTML($wgOut){
         global $wgUser, $wgServer, $wgScriptPath, $wgRoles, $config;
         $user = Person::newFromId($wgUser->getId());
-        $wgOut->addHTML("By registering with {$config->getValue('networkName')} you will be granted the role of HQP-Candidate.<br /><br />");
+        $wgOut->addHTML("By registering with {$config->getValue('networkName')} you will be granted the role of HQP-Candidate.  You may need to check your spam/junk mail for the registration email if it doesn't show up after a few minutes.  If you still don't get the email, please contact <a href='mailto:{$config->getValue('supportEmail')}'>{$config->getValue('supportEmail')}</a>.<br /><br />");
         $wgOut->addHTML("<form action='$wgScriptPath/index.php/Special:HQPRegister' method='post'>\n");
         $form = self::createForm();
         $wgOut->addHTML($form->render());
