e18a86010c57266b437d315eeabf64b0068a18d4
Finalfantasykid <finalfantasykid13@gmail.com>
2015-11-19 14:50:08 -0700

    -Added the Catalyst/Transformative review tables
    Former-commit-id: 5cc30182dbcea7757baa63f60a68425c214da319

--- extensions/Reporting/Report/ReportItems/ReportItemSets/AllMaterialsReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/AllMaterialsReportItemSet.php
@@ -7,10 +7,11 @@ class AllMaterialsReportItemSet extends ReportItemSet {
         //$person = Person::newFromId($this->personId);
         
         $type = $this->getAttr('subType', 'NI');
-        if($type != 'SAB' && $type != 'Project'){
+        $class = $this->getAttr('class', 'Person');
+        if($type != 'SAB' && $type != 'Project' && $class != "Project"){
             //$subs = Person::getAllPeople($type);
             $year = $this->getReport()->year;
-            $subs = Person::getAllEvaluates($type, $year);
+            $subs = Person::getAllEvaluates($type, $year, $class);
             $sorted = array();
             foreach ($subs as $s){
                 $rev_name = $s->getReversedName();
@@ -25,7 +26,7 @@ class AllMaterialsReportItemSet extends ReportItemSet {
         if(is_array($subs)){
             foreach($subs as $sub){
                 $tuple = self::createTuple();
-                if($type == "Project" || $type == "SAB"){
+                if($type == "Project" || $type == "SAB" || $class == "Project"){
                     $tuple['project_id'] = $sub->getId();
                 }
                 else{
