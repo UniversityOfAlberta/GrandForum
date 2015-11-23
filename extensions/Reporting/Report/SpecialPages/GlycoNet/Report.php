5962a8495a8c6c04ec2cb3a9c296dfc76b14da25
Finalfantasykid <finalfantasykid13@gmail.com>
2015-11-23 14:21:18 -0700

    -Added the HQPRegister table for GlycoNet
    Former-commit-id: f748b60263d68537b635aa39674d8197e956b184

--- extensions/Reporting/Report/SpecialPages/GlycoNet/Report.php
+++ extensions/Reporting/Report/SpecialPages/GlycoNet/Report.php
@@ -6,6 +6,7 @@ $wgExtensionMessagesFiles['Report'] = $dir . 'Report.i18n.php';
 $wgSpecialPageGroups['Report'] = 'reporting-tools';
 
 require_once("RFPApplicationTable.php");
+require_once("HQPRegisterTable.php");
 
 $wgHooks['TopLevelTabs'][] = 'Report::createTab';
 $wgHooks['SubLevelTabs'][] = 'Report::createSubTabs';
