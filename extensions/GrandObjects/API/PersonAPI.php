4d25a8cbd326afaac9d382632c3bd44bd6c0871a
Finalfantasykid <finalfantasykid13@gmail.com>
2016-02-22 14:01:44 -0700

    -Minor changes to PersonAPI
    Former-commit-id: 7d86bd52e7dfd0047ef2c9c145d6366329877f58

--- extensions/GrandObjects/API/PersonAPI.php
+++ extensions/GrandObjects/API/PersonAPI.php
@@ -238,7 +238,7 @@ class PersonProjectsAPI extends RESTAPI {
         Notification::addNotification($me, $person, "Project Membership Removed", "You have been removed from '{$project->getName()}'", "{$person->getUrl()}");
         $person->projects = null;
         MailingList::subscribeAll($person);
-        return false;
+        return $this->doGET();
     }
 }
 
@@ -427,7 +427,7 @@ class PersonUniversitiesAPI extends RESTAPI {
                             array('id' => $personUniversityId));
         $person->universityDuring = array();
         MailingList::subscribeAll($person);
-        return false;
+        return $this->doGET();
     }
 }
 
@@ -500,7 +500,7 @@ class PersonRelationsAPI extends RESTAPI {
             $this->throwError("This Relationship does not exist");
         }
         $relation->delete();
-        return false;
+        return $this->doGET();
     }
 }
 
