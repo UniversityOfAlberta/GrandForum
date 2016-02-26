1f1792b854c2dad62284b6de8956b85b43cbd226
Finalfantasykid <finalfantasykid13@gmail.com>
2016-02-26 15:32:11 -0700

    -Fixed an issue with the workpackage pdf
    Former-commit-id: d9c428a9dcd11d1f8ba8eed334eca289bcc0b9ca

--- extensions/Reporting/Report/ReportItems/StaticReportItems/SimpleReviewSubmitReportItem.php
+++ extensions/Reporting/Report/ReportItems/StaticReportItems/SimpleReviewSubmitReportItem.php
@@ -102,7 +102,13 @@ EOF;
             $tok = false;
             $tst = '';
             $sto = new ReportStorage($person);
-            $project = Project::newFromId($this->projectId);
+            $project = null;
+            if($this->getReport()->project instanceof Project){
+                $project = Project::newFromId($this->projectId);
+            }
+            else if($this->getReport()->project instanceof Theme){
+            $project = Theme::newFromId($this->projectId);
+            }
             $report = new DummyReport($file, $person, $project);
         	$check = $report->getPDF();
         	if (count($check) > 0) {
