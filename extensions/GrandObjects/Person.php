5504af2a3f8092126f89eff6fa4c914710037022
Finalfantasykid <finalfantasykid13@gmail.com>
2016-03-17 10:59:27 -0600

    -Fixed an issue with 12 hour format
    Former-commit-id: fe0633842d17d28ac306deb75e60976187ce1861

--- extensions/GrandObjects/Person.php
+++ extensions/GrandObjects/Person.php
@@ -2024,7 +2024,7 @@ class Person extends BackboneModel {
      */
     function getRoleOn($project, $year=null){
         if($year == null){
-            $year = date('Y-m-d h:i:s');
+            $year = date('Y-m-d H:i:s');
         }
         if($this->isRoleOn(AR, $year, $project) && !$this->leadershipOf($project)){
             return AR;
