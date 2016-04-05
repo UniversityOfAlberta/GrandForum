c43fc835c3c5bdce4f808e07c40bb099cbbda0ed
Finalfantasykid <finalfantasykid13@gmail.com>
2016-04-05 14:14:11 -0600

    -Renable SD permission change
    Former-commit-id: 6898e9458a645a58c5b5d73ae27bf8e787d68bb5

--- extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
@@ -19,7 +19,7 @@ class ApplicationsTable extends SpecialPage{
     
     function userCanExecute($user){
         $person = Person::newFromUser($user);
-        return ($person->isRoleAtLeast(STAFF));
+        return ($person->isRoleAtLeast(SD));
     }
 
     function execute($par){
