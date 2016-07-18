252c6e6a44b97812f94de4c380ddd5f1932c9966
Finalfantasykid <finalfantasykid13@gmail.com>
2016-07-18 12:58:23 -0600

    -Added a script to list the new members
    Former-commit-id: 41b043738014ed04a04b13549fa68209402c309b

---
+++ maintenance/listNewMembers.php
@@ -0,0 +1,25 @@
+<?php
+require_once('commandLine.inc');
+
+global $wgUser;
+$wgUser = User::newFromName("Admin");
+
+$newMembers = array();
+$oldMembers = @unserialize(file_get_contents("members.txt"));
+if($oldMembers == null){
+    $oldMembers = array();
+}
+
+$currentMembers = Person::getAllPeople('all');
+foreach($currentMembers as $member){
+    if(!$member->isCandidate()){
+        if(!isset($oldMembers[$member->getId()])){
+            echo "{$member->getName()}: {$member->getEmail()} ({$member->getRoleString()})\n";
+        }
+        $newMembers[$member->getId()] = $member->getId();
+    }
+}
+
+file_put_contents("members.txt", serialize($newMembers));
+
+?>
