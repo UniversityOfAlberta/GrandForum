2a75b5b49a2d0c9de51ecb7755749aaddc2b4031
Finalfantasykid <finalfantasykid13@gmail.com>
2016-08-10 13:02:46 -0600

    -Added some APIs
    Former-commit-id: 2cb2f42110a0a53d80512a17ebaca62d678823a6

--- extensions/GrandObjects/API/ProjectAPI.php
+++ extensions/GrandObjects/API/ProjectAPI.php
@@ -198,4 +198,83 @@ class ProjectProductAPI extends RESTAPI {
     
 }
 
+class ProjectContributionsAPI extends RESTAPI {
+
+    function doGET(){
+        if($this->getParam('id') != ""){
+            $me = Person::newFromWgUser();
+            $array = array();
+            $project = Project::newFromId($this->getParam('id'));
+            if($project == null || $project->getName() == ""){
+                $project = Project::newFromName($this->getParam('id'));
+                if($project == null || $project->getName() == ""){
+                    $this->throwError("This project does not exist");
+                }
+            }
+            if(!$me->isRoleAtLeast(MANAGER)){
+                $this->throwError("You are not allowed to access this API");
+            }
+            $contributions = $project->getContributions();
+            foreach($contributions as $contribution){
+                $array[] = $contribution;
+            }
+            $array = new Collection(array_values($array));
+            return $array->toJSON();
+        }
+    }
+    
+    function doPOST(){
+        return $this->doGET();
+    }
+    
+    function doPUT(){
+        return $this->doGET();
+    }
+    
+    function doDELETE(){
+        return $this->doGET();
+    }
+
+}
+
+class ProjectAllocationsAPI extends RESTAPI {
+    
+    function doGET(){
+        global $config;
+        if($this->getParam('id') != ""){
+            $me = Person::newFromWgUser();
+            $array = array();
+            $project = Project::newFromId($this->getParam('id'));
+            if($project == null || $project->getName() == ""){
+                $project = Project::newFromName($this->getParam('id'));
+                if($project == null || $project->getName() == ""){
+                    $this->throwError("This project does not exist");
+                }
+            }
+            if(!$me->isRoleAtLeast(MANAGER)){
+                $this->throwError("You are not allowed to access this API");
+            }
+            $startYear = date('Y');
+            $endYear = $config->getValue('projectPhaseDates');
+            $endYear = $endYear[1];
+            for($y = $startYear; $y >= $endYear; $y--){
+                $array[$y] = $project->getAllocatedAmount($y);
+            }
+            return json_encode($array);
+        }
+    }
+    
+    function doPOST(){
+        return $this->doGET();
+    }
+    
+    function doPUT(){
+        return $this->doGET();
+    }
+    
+    function doDELETE(){
+        return $this->doGET();
+    }
+}
+
 ?>
