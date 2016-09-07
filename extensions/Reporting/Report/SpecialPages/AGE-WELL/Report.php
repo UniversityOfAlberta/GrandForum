686ef7ccd8648f5be6d5a8b433d916b5c9382664
Finalfantasykid <finalfantasykid13@gmail.com>
2016-09-07 13:16:53 -0600

    -Added the next SIP Application
    Former-commit-id: 9fa6b09e323485d67580bdf65ccfc93ad9e13e2d

--- extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
@@ -63,8 +63,8 @@ class Report extends AbstractReport {
         }
         if($person->isRole(NI) || $person->isRole(NI.'-Candidate') ||
            $person->isRole(EXTERNAL) || $person->isRole(EXTERNAL.'-Candidate')){
-            $selected = @($wgTitle->getText() == "Report" && $_GET['report'] == "SIPApplication072016") ? "selected" : false;
-            $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("SIP Application", "{$url}SIPApplication072016", $selected);
+            $selected = @($wgTitle->getText() == "Report" && $_GET['report'] == "SIPApplication102016") ? "selected" : false;
+            $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("SIP Application", "{$url}SIPApplication102016", $selected);
             /*
             $selected = @($wgTitle->getText() == "Report" && $_GET['report'] == "CatalystApplication") ? "selected" : false;
             $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("Catalyst Application", "{$url}CatalystApplication", $selected);
