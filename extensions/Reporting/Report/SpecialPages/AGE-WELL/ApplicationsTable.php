7b5f067bc190f99e4e610874415fab762ebb4c4e
Finalfantasykid <finalfantasykid13@gmail.com>
2016-09-16 13:43:57 -0600

    -Added the Access Application
    Former-commit-id: 8a80acd462684082d1230ae49d0568ed3866d932

--- extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
@@ -13,6 +13,12 @@ function runApplicationsTable($par) {
 
 class ApplicationsTable extends SpecialPage{
 
+    var $nis;
+    var $fullHQPs;
+    var $hqps;
+    var $wps;
+    var $projects;
+
     function ApplicationsTable() {
         SpecialPage::__construct("ApplicationsTable", null, false, 'runApplicationsTable');
     }
@@ -27,52 +33,140 @@ class ApplicationsTable extends SpecialPage{
         ApplicationsTable::generateHTML($wgOut);
     }
     
+    function initArrays(){
+        $this->nis = array_merge(Person::getAllPeople(NI), 
+                                 Person::getAllCandidates(NI),
+                                 Person::getAllPeople(EXTERNAL),
+                                 Person::getAllCandidates(EXTERNAL));
+        
+        $this->fullHQPs = Person::getAllPeople(HQP);
+        
+        $this->hqps = array_merge($this->fullHQPs,
+                                  Person::getAllCandidates(HQP));
+                                  
+        
+        
+        $this->wps = Theme::getAllThemes();
+        
+        $this->projects = Project::getAllProjects();
+    }
+    
     function generateHTML($wgOut){
         global $wgUser, $wgServer, $wgScriptPath, $wgRoles, $config;
         
         $me = Person::newFromWgUser();
         
-        $nis = array_merge(Person::getAllPeople(NI), 
-                           Person::getAllCandidates(NI),
-                           Person::getAllPeople(EXTERNAL),
-                           Person::getAllCandidates(EXTERNAL));
+        $links = array();
         
-        $hqps = array_merge(Person::getAllPeople(HQP), 
-                            Person::getAllCandidates(HQP));
-        
-        $wps = Theme::getAllThemes();
-        
-        $projects = Project::getAllProjects();
-        
-        $tabbedPage = new TabbedPage("person");
-
         if($me->isRoleAtLeast(SD)){
-            $tabbedPage->addTab(new ApplicationTab('RP_SIP', $nis, 2015, "SIP 01-2016"));
-            $tabbedPage->addTab(new ApplicationTab('RP_SIP_04_2016', $nis, 2015, "SIP 04-2016"));
-            $tabbedPage->addTab(new ApplicationTab('RP_SIP_07_2016', $nis, 2015, "SIP 07-2016"));
-            $tabbedPage->addTab(new ApplicationTab('RP_SIP_10_2016', $nis, 2015, "SIP 10-2016"));
-            $tabbedPage->addTab(new ApplicationTab('RP_CAT', $nis, 2015, "Catalyst"));
-            $tabbedPage->addTab(new ApplicationTab('RP_CIP', $nis, 2015, "CIP"));
+            $links[] = "<a href='$wgServer$wgScriptPath/index.php/Special:ApplicationsTable?program=sip'>SIP</a>";
+            $links[] = "<a href='$wgServer$wgScriptPath/index.php/Special:ApplicationsTable?program=cip'>CIP</a>";
+            $links[] = "<a href='$wgServer$wgScriptPath/index.php/Special:ApplicationsTable?program=access'>ACCESS</a>";
+            $links[] = "<a href='$wgServer$wgScriptPath/index.php/Special:ApplicationsTable?program=catalyst'>Catalyst</a>";
+            $links[] = "<a href='$wgServer$wgScriptPath/index.php/Special:ApplicationsTable?program=wp'>WP</a>";
+            $links[] = "<a href='$wgServer$wgScriptPath/index.php/Special:ApplicationsTable?program=project'>Project Evaluation</a>";
         }
         if($me->isRoleAtLeast(SD) || count($me->getEvaluates('RP_SUMMER', 2015, "Person")) > 0){
-            $summerHQPs = array();
-            if($me->isRoleAtLeast(SD)){
-                $summerHQPs = $hqps;
-            }
-            else{
-                foreach($hqps as $hqp){
-                    if($me->isEvaluatorOf($hqp, 'RP_SUMMER', 2015, "Person")){
-                        $summerHQPs[] = $hqp;
-                    }
+            $links[] = "<a href='$wgServer$wgScriptPath/index.php/Special:ApplicationsTable?program=summer'>Summer Institute</a>";
+        }
+        
+        $wgOut->addHTML("<h1>Report Tables:&nbsp;".implode("&nbsp;|&nbsp;", $links)."</h1><br />");
+        if(!isset($_GET['program'])){
+            return;
+        }
+        $program = $_GET['program'];
+        
+        $this->initArrays();
+        
+        if($program == "sip" && $me->isRoleAtLeast(SD)){
+            $this->generateSIP();
+        }
+        else if($program == "cip" && $me->isRoleAtLeast(SD)){
+            $this->generateCIP();
+        }
+        if($program == "access" && $me->isRoleAtLeast(SD)){
+            $this->generateAccess();
+        }
+        else if($program == "catalyst" && $me->isRoleAtLeast(SD)){
+            $this->generateCatalyst();
+        }
+        else if($program == "summer" && ($me->isRoleAtLeast(SD) || count($me->getEvaluates('RP_SUMMER', 2015, "Person")) > 0)){
+            $this->generateSummer();
+        }
+        else if($program == "wp" && $me->isRoleAtLeast(SD)){
+            $this->generateWP();
+        }
+        else if($program == "project" && $me->isRoleAtLeast(SD)){
+            $this->generateProject();
+        }
+        return;
+    }
+    
+    function generateSIP(){
+        global $wgOut;
+        $tabbedPage = new InnerTabbedPage("reports");
+        $tabbedPage->addTab(new ApplicationTab('RP_SIP', $this->nis, 2015, "01-2016"));
+        $tabbedPage->addTab(new ApplicationTab('RP_SIP_04_2016', $this->nis, 2015, "04-2016"));
+        $tabbedPage->addTab(new ApplicationTab('RP_SIP_07_2016', $this->nis, 2015, "07-2016"));
+        $tabbedPage->addTab(new ApplicationTab('RP_SIP_10_2016', $this->nis, 2015, "10-2016"));
+        $wgOut->addHTML($tabbedPage->showPage());
+    }
+    
+    function generateCIP(){
+        global $wgOut;
+        $tabbedPage = new InnerTabbedPage("reports");
+        $tabbedPage->addTab(new ApplicationTab('RP_CIP', $this->nis, 2015, "2015"));
+        $wgOut->addHTML($tabbedPage->showPage());
+    }
+    
+    function generateCatalyst(){
+        global $wgOut;
+        $tabbedPage = new TabbedPage("reports");
+        $tabbedPage->addTab(new ApplicationTab('RP_CAT', $this->nis, 2015, "Catalyst"));
+        $wgOut->addHTML($tabbedPage->showPage());
+    }
+    
+    function generateAccess(){
+        global $wgOut;
+        $tabbedPage = new InnerTabbedPage("reports");
+        $tabbedPage->addTab(new ApplicationTab('RP_ACCESS_01_2017', $this->fullHQPs, 2015, "01-2017"));
+        $tabbedPage->addTab(new ApplicationTab('RP_ACCESS_04_2017', $this->fullHQPs, 2015, "04-2017"));
+        $tabbedPage->addTab(new ApplicationTab('RP_ACCESS_07_2017', $this->fullHQPs, 2015, "07-2017"));
+        $tabbedPage->addTab(new ApplicationTab('RP_ACCESS_10_2017', $this->fullHQPs, 2015, "10-2017"));
+        $wgOut->addHTML($tabbedPage->showPage());
+    }
+    
+    function generateSummer(){
+        global $wgOut;
+        $me = Person::newFromWgUser();
+        $summerHQPs = array();
+        if($me->isRoleAtLeast(SD)){
+            $summerHQPs = $this->hqps;
+        }
+        else{
+            foreach($hthis->qps as $hqp){
+                if($me->isEvaluatorOf($hqp, 'RP_SUMMER', 2015, "Person")){
+                    $summerHQPs[] = $hqp;
                 }
             }
-            $tabbedPage->addTab(new ApplicationTab('RP_SUMMER', $summerHQPs, 2015, "Summer Institute 2016"));
         }
-        if($me->isRoleAtLeast(SD)){
-            $tabbedPage->addTab(new ApplicationTab('RP_WP_REPORT', $wps, 2015, "WP Report"));
-        }
-        $tabbedPage->addTab(new ApplicationTab('RP_PROJ_EVALUATION', $projects, 2015, "Project Evaluation"));
-        $tabbedPage->showPage();
+        $tabbedPage = new TabbedPage("reports");
+        $tabbedPage->addTab(new ApplicationTab('RP_SUMMER', $summerHQPs, 2015, "2016"));
+        $wgOut->addHTML($tabbedPage->showPage());
+    }
+    
+    function generateWP(){
+        global $wgOut;
+        $tabbedPage = new TabbedPage("reports");
+        $tabbedPage->addTab(new ApplicationTab('RP_WP_REPORT', $this->wps, 2015, "2015"));
+        $wgOut->addHTML($tabbedPage->showPage());
+    }
+    
+    function generateProject(){
+        global $wgOut;
+        $tabbedPage = new TabbedPage("reports");
+        $tabbedPage->addTab(new ApplicationTab('RP_PROJ_EVALUATION', $this->projects, 2015, "2016"));
+        $wgOut->addHTML($tabbedPage->showPage());
     }
     
     static function createSubTabs(&$tabs){
