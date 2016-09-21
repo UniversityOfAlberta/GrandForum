377dc8146e4bf0c2fd95d94f670fccd7cd935ddf
Finalfantasykid <finalfantasykid13@gmail.com>
2016-09-21 15:05:56 -0600

    -Fixed an issue with the Message Board APIs
    Former-commit-id: faf6d271fe9132a205b4dd8a1ff648d7a867e4d1

---
+++ extensions/GrandObjects/API/MessageBoard/PostsAPI.php
@@ -0,0 +1,23 @@
+<?php
+
+class PostsAPI extends RESTAPI {
+
+    function doGET(){
+        return false;
+    }
+
+    function doPOST(){
+        return false;
+    }
+
+    function doPUT(){
+        return false;
+    }
+
+    function doDELETE(){
+        return false;
+    }
+
+}
+
+?>
