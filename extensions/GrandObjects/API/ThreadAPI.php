9c6bc453646254f90b527dfbc52b2391a8f7d23f
Finalfantasykid <finalfantasykid13@gmail.com>
2016-10-05 15:50:24 -0600

    -Made changes to the Threads
    Former-commit-id: 574a3bb36da4a30c47492e5a86deec0d15edd14b

--- extensions/GrandObjects/API/ThreadAPI.php
+++ extensions/GrandObjects/API/ThreadAPI.php
@@ -77,9 +77,9 @@ class ThreadsAPI extends RESTAPI {
                                           FROM grand_posts p, grand_threads t, mw_user u
                                           WHERE p.thread_id = t.id
                                           AND u.user_id = t.user_id
-                                          AND (MATCH(p.message) AGAINST ('{$search}') OR 
-                                               LOWER(t.title)   LIKE '%{$search}%' OR
-                                               UPPER(CONVERT(u.user_real_name USING latin1))  LIKE '%{$search}%')
+                                          AND (MATCH(p.search) AGAINST ('{$search}') OR 
+                                               LOWER(t.title) LIKE '%{$search}%' OR
+                                               UPPER(CONVERT(u.user_real_name USING latin1)) LIKE '%{$search}%')
                                           ");
             foreach($data as $row){
                 $thread = Thread::newFromId($row['id']);
