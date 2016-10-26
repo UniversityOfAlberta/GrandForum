0b2041b7c42c2e89b28e4b40b10b77c109cace7c
Finalfantasykid <finalfantasykid13@gmail.com>
2016-10-26 14:18:55 -0600

    -Fix the default date for the datepicker
    Former-commit-id: 2e812b2c2da0513f09383cd42132f04be440e2ef

--- skins/cavendish/cavendish.php
+++ skins/cavendish/cavendish.php
@@ -151,7 +151,7 @@ class CavendishTemplate extends QuickTemplate {
                 $.each(this.$el.find('input[type=datepicker]'), function(index, val){
                     $(val).datepicker({
                         'dateFormat': $(val).attr('format'),
-                        'defaultDate': $(val).attr('value'),
+                        'defaultDate': $(val).attr('value').substr(0, 10),
                         'changeMonth': true,
                         'changeYear': true,
                         'showOn': "both",
