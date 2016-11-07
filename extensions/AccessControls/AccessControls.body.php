4410c376da0c9d75c5218072ad6b93887f8a0b7f
Finalfantasykid <finalfantasykid13@gmail.com>
2016-11-07 11:23:46 -0700

    -Fixed an issue with the guest sections
    Former-commit-id: 2066ac9a4bc6382e4ca6934c652bbb66ab50fc35

--- extensions/AccessControls/AccessControls.body.php
+++ extensions/AccessControls/AccessControls.body.php
@@ -146,7 +146,7 @@ function parsePublicSections($title, $text){
 		
 			$text = $buffer;
 		
-			$text = preg_replace("/\[private\].*\[\/private\]/", "", $text);
+			$text = preg_replace("/\[private\].*\[\/private\]/s", "", $text);
 		}
 	}
 	$text = str_ireplace("[public]", "<public>", $text);
@@ -157,9 +157,9 @@ function parsePublicSections($title, $text){
 }
 
 function parseGuestSections($title, $text){
-	global $wgUser, $wgScriptPath, $wgOut, $publicPresent;
+	global $wgUser, $wgScriptPath, $wgOut;
 	if(!is_null($title) && !$wgOut->isDisabled() && $wgUser->isLoggedIn()){
-		$text = preg_replace("/\[guest\].*\[\/guest\]/", "", $text);
+		$text = preg_replace("/\[guest\].*\[\/guest\]/s", "", $text);
 	}
 	$text = str_ireplace("[guest]", "<guest>", $text);
 	$text = str_ireplace("[/guest]", "</guest>", $text);
