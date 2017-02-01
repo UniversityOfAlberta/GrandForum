f319715496d29bfff3a72c935f6de82fa6f50d10
Finalfantasykid <finalfantasykid13@gmail.com>
2017-02-01 13:36:05 -0700

    Changed the Access application to the 04-2017
    Former-commit-id: e2aade79f27db3d496b9833543b17f55c688b69d

--- extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
@@ -152,6 +152,7 @@ class ApplicationsTable extends SpecialPage{
     function generateAccess(){
         global $wgOut;
         $tabbedPage = new InnerTabbedPage("reports");
+        $tabbedPage->addTab(new ApplicationTab('RP_ACCESS_04_2017', $this->fullHQPs, 2017, "04-2017"));
         $tabbedPage->addTab(new ApplicationTab('RP_ACCESS_01_2017', $this->fullHQPs, 2017, "01-2017"));
         $tabbedPage->addTab(new ApplicationTab('RP_ACCESS_10_2016', $this->fullHQPs, 2016, "10-2016"));
         $wgOut->addHTML($tabbedPage->showPage());
--- extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
@@ -54,8 +54,8 @@ class Report extends AbstractReport {
             $tabs["Reports"]['subtabs'][] = TabUtils::createSubTab("Annual Report - EPIC Survey", "{$url}EPICReport", $selected);
         }
         if($person->isRole(HQP)){
-            $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "AccessApplication012017")) ? "selected" : false;
-            $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("ACCESS Application", "{$url}AccessApplication012017", $selected);
+            $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "AccessApplication042017")) ? "selected" : false;
+            $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("ACCESS Application", "{$url}AccessApplication042017", $selected);
         }
         /*if($person->isRole(HQP) || $person->isRole(HQP.'-Candidate')){
             $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "SummerApplication")) ? "selected" : false;
