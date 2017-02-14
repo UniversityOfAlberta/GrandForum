01a8472d704db69b4fec250c0ea30ce1bb269191
David Turner <dwt@ssrg5.cs.ualberta.ca>
2017-02-14 14:01:42 -0700

    -Added some files
    Former-commit-id: f80a37073cea62be5e3ddedcf19f4698afd9dc8e

---
+++ extensions/Reporting/Report/SpecialPages/GARS/ReportStatusTable.i18n.php
@@ -0,0 +1,9 @@
+<?php
+
+$messages = array();
+$messages['en'] = array( 
+			   'ReportStatusTable' => 'Report Status Table',
+			   'Reportstatustable' => 'Report Status Table',
+			   'reportstatustable' => 'Report Status Table',
+			   );
+?>
