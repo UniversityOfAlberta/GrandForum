24edc44ceec1fb8f13ca8911fb889f60a8d0a7a3
Finalfantasykid <finalfantasykid13@gmail.com>
2017-02-16 09:50:40 -0700

    -Minor changes to SIP Accelerator
    Former-commit-id: e01fed1fb69503e319d068a1239eb120a5977ac3

--- extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
@@ -71,8 +71,8 @@ class Report extends AbstractReport {
         }
         if($person->isRole(NI) || $person->isRole(NI.'-Candidate') ||
            $person->isRole(EXTERNAL) || $person->isRole(EXTERNAL.'-Candidate')){
-            $selected = @($wgTitle->getText() == "Report" && $_GET['report'] == "SIPApplication012017") ? "selected" : false;
-            $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("SIP Application", "{$url}SIPApplication012017", $selected);
+            //$selected = @($wgTitle->getText() == "Report" && $_GET['report'] == "SIPApplication012017") ? "selected" : false;
+            //$tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("SIP Application", "{$url}SIPApplication012017", $selected);
             
             $selected = @($wgTitle->getText() == "Report" && $_GET['report'] == "CatalystApplication") ? "selected" : false;
             $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("Catalyst Application", "{$url}CatalystApplication", $selected);
