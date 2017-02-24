27f53ad57e983c42c7f222276da9918954c49185
Finalfantasykid <finalfantasykid13@gmail.com>
2017-02-24 14:01:18 -0700

    -Fixed an issue with the year
    Former-commit-id: 88adfa00ae29e12dd074d73bae0198f242e97cc8

--- extensions/Reporting/Report/SpecialPages/GlycoNet/ApplicationsTable.php
+++ extensions/Reporting/Report/SpecialPages/GlycoNet/ApplicationsTable.php
@@ -173,6 +173,7 @@ class ApplicationsTable extends SpecialPage{
         $reviewers->setAttr("class", "wikitable");
         $reviewers->setAttr("orientation", "list");
         $reviewers->setId("reviewers");
+        $tabbedPage->addTab(new ApplicationTab(array('RP_COLLAB'), $this->allNis, 2017, "04-2017"));
         $tabbedPage->addTab(new ApplicationTab(array('RP_COLLAB'), $this->allNis, 2016, "2016"));
         $wgOut->addHTML($tabbedPage->showPage());
     }
