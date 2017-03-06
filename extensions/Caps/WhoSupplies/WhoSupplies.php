c377b5a4929bf1f4555f4d95dfe143abc51b2062
Finalfantasykid <finalfantasykid13@gmail.com>
2017-03-06 15:10:54 -0700

    -Made some changes to the WhoSupplies
    Former-commit-id: 0ff1b69ae90226edb53073f47268b5c0467f1ccb

--- extensions/Caps/WhoSupplies/WhoSupplies.php
+++ extensions/Caps/WhoSupplies/WhoSupplies.php
@@ -25,7 +25,7 @@ class WhoSupplies extends SpecialPage{
             <h2>National Regulations</h2>
             <div>
                 <ul>
-                    <li>Health Canada’s Consent Form & Patient Information Guide (2 PDFs attached)</li>
+                    <li>Health Canada’s <a href='https://www.caps-cpca.ubc.ca/AnnokiUploadAuth.php/5/5e/Patients_mifegymisopatientconsentforme11_29_16.pdf' target='_blank'>Consent Form</a> & <a href='https://www.caps-cpca.ubc.ca/AnnokiUploadAuth.php/7/7a/Patients_Mifegymiso_Eng_guide_01_19_17.pdf' target='_blank'>Patient Information Guide</a></li>
                     <li>Awaiting the <a href='https://www.cadth.ca/about-cadth/what-we-do/products-services/cdr' target='_blank'>Canadian Agency for Drugs and Technologies in Health</a> (CADTH) Common Drug Review of Mifegymiso®, scheduled for March 2017. Approval may impact insurance coverage and provincial and territorial formularies (i.e list publically-funded drugs for individuals who qualify and/or do not have private insurance, for example, individuals on social assistance).</li>
                 </ul>
             </div>
@@ -33,49 +33,49 @@ class WhoSupplies extends SpecialPage{
             <h2 id='AB'>Alberta</h2>
             <div>
                 <ul>
-                    <li>Mifegymiso®, ~ $300/dose (Oct. 7th, 2016) <a href='http://edmontonjournal.com/news/politics/paula-simons-access-to-abortion-drug-one-step-closer-for-alberta-women' target='_blank'>read more</a></li>
-                    <li>Celopharma has shipped mifepristone to clinics in Calgary (Feb. 24th, 2017)</li>
+                    <li>As reported in the media, Mifegymiso®, ~ $300/dose (Oct. 7th, 2016) <a href='http://edmontonjournal.com/news/politics/paula-simons-access-to-abortion-drug-one-step-closer-for-alberta-women' target='_blank'>read more</a></li>
+                    <li>As of Feb. 24, 2017, Celopharma has shipped mifepristone to clinics in Calgary (Feb. 24th, 2017)</li>
                 </ul>
             </div>
             
             <h2 id='BC'>British Columbia</h2>
             <div>
                 <ul>
-                    <li>Both the College of Physicians and Surgeons of BC and the College of Pharmacists of BC say, treat Mifegymiso like any other medication <a href='https://www.cpsbc.ca/for-physicians/college-connector/2016-V04-06/01' target='_blank'>read more</a></li>
-                    <li>In private clinics, individuals must pay out of pocket, $300-750 per package <a href='http://www.willowclinic.ca/?page_id=15' target='_blank'>read more</a></li>
+                    <li>Both the College of Physicians and Surgeons of BC and the College of Pharmacists of BC  have provided guidance to their members on dispensing mifepristone</li>
+                    <li>In private clinics, individuals must pay out of pocket, ~$300 per package </li>
                     <li>Celopharma advised women to bring signed consent to pharmacy, with prescription- Not applicable in BC! <a href='http://www.bcpharmacists.org/mifegymiso' target='_blank'>read more</a></li>
                     <li>Mifegymiso® is being considered for possible coverage under the <a href='http://www2.gov.bc.ca/assets/gov/health/health-drug-coverage/pharmacare/mifepristone-misoprostol-3509-info.pdf' target='_blank'>B.C. PharmaCare Program</a> (Feb. 14th, 2017)</li>
-                    <li>Celopharma has shipped mifepristone to clinics in Vancouver (Feb. 24th, 2017)</li>
+                    <li>As of Feb. 24, 2017, Celopharma has shipped mifepristone to clinics in Vancouver</li>
                 </ul>
             </div>
             
             <h2 id='MB'>Manitoba</h2>
             <div>
                 <ul>
-                    <li>Mifegymiso® will not be coming to Manitoba for a few more months (Feb. 9th, 2017) <a href='http://www.cbc.ca/news/canada/manitoba/mifegymiso-abortion-pill-not-in-manitoba-1.3967489' target='_blank'>read more</a></li>
-                    <li>Celopharma has shipped mifepristone to clinics in Winnipeg (Feb. 24th, 2017)</li>
+                    <li>As reported in the media, Mifegymiso® will not be coming to Manitoba for a few more months <a href='http://www.cbc.ca/news/canada/manitoba/mifegymiso-abortion-pill-not-in-manitoba-1.3967489' target='_blank'>read more</a></li>
+                    <li>As of Feb. 24, 2017, Celopharma has shipped mifepristone to clinics in Winnipeg</li>
                 </ul>
             </div>
             
             <h2 id='NB'>New Brunswick</h2>
             <div>
                 <ul>
-                    <li>Student Health Plans covering: St Thomas University, $5 co-pay (Feb. 13th, 2017) <a href='https://www.lifesitenews.com/news/for-just-a-5-co-pay-abortion-drug-will-be-available-to-students-at-st.-thom' target='_blank'>read more</a></li>
-                    <li>Mifepristone is not making inroads into New Brunswick; only 14 physicians have registered for the medical abortion training (Feb. 27th, 2017) <a href='http://www.cbc.ca/news/canada/new-brunswick/mifegymiso-canada-health-doctors-1.4000643' target='_blank'>read more</a></li>
+                    <li>As reported in the media, Student Health Plans covering: St Thomas University, $5 co-pay (Feb. 13th, 2017) <a href='https://www.lifesitenews.com/news/for-just-a-5-co-pay-abortion-drug-will-be-available-to-students-at-st.-thom' target='_blank'>read more</a></li>
+                    <li>As reported in the media, Mifepristone is not making inroads into New Brunswick; only 14 physicians have registered for the medical abortion training (Feb. 27th, 2017) <a href='http://www.cbc.ca/news/canada/new-brunswick/mifegymiso-canada-health-doctors-1.4000643' target='_blank'>read more</a></li>
                 </ul>
             </div>
             
             <h2 id='NL'>Newfoundland and Labrador</h2>
             <div>
                 <ul>
-                    <li>Canadian distributor Celopharma as not reported shipments here to date (Jan. 28th, 2017) <a href='http://www.cbc.ca/news/canada/newfoundland-labrador/abortion-drug-not-available-1.2662032' target='_blank'>read more</a></li>
+                    <li>As reported in the media, Canadian distributor Celopharma as not reported shipments here to date (Jan. 28th, 2017) <a href='http://www.cbc.ca/news/canada/newfoundland-labrador/abortion-drug-not-available-1.2662032' target='_blank'>read more</a></li>
                 </ul>
             </div>
             
             <h2 id='NT'>Northwest Territories</h2>
             <div>
                 <ul>
-                    <li>Canadian distributor Celopharma as not reported shipments here to date (Jan. 28th, 2017)</li>
+                    <li>As of Jan. 28, 2017, Canadian distributor Celopharma as not reported shipments here to date</li>
                 </ul>
             </div>
             
@@ -89,7 +89,7 @@ class WhoSupplies extends SpecialPage{
             <h2 id='NU'>Nunavut</h2>
             <div>
                 <ul>
-                    <li>Canadian distributor Celopharma as not reported shipments here to date (Jan. 28th, 2017)</li>
+                    <li>As of Jan. 28, 2017, Canadian distributor Celopharma as not reported shipments here to date</li>
                 </ul>
             </div>
             
@@ -97,14 +97,14 @@ class WhoSupplies extends SpecialPage{
             <div>
                 <ul>
                     <li>Distributor in Ontario via Celopharma or wholesaler, McKesson <a href='https://www.mckesson.ca/mckesson-pharmaceutical' target='_blank'>read more</a></li>
-                    <li>Celopharma has shipped mifepristone to clinics in Toronto and Ottawa (Feb. 24th, 2017)</li>
+                    <li>As of Feb. 24, 2017, Celopharma has shipped mifepristone to clinics in Toronto and Ottawa</li>
                 </ul>
             </div>
             
             <h2 id='PE'>Prince Edward Island</h2>
             <div>
                 <ul>
-                    <li>Student Health Plans covering: UPEI (90% covered) and Holland College (80% covered) (Feb. 10th, 2017) <a href='http://www.cbc.ca/news/canada/prince-edward-island/abortion-drug-pill-mifegymiso-pei-doctors-pharmacists-1.3976558' target='_blank'>read more info</a></li>
+                    <li>As reported in the media, Student Health Plans covering: UPEI (90% covered) and Holland College (80% covered) (Feb. 10th, 2017) <a href='http://www.cbc.ca/news/canada/prince-edward-island/abortion-drug-pill-mifegymiso-pei-doctors-pharmacists-1.3976558' target='_blank'>read more info</a></li>
                 </ul>
             </div>
             
@@ -118,14 +118,14 @@ class WhoSupplies extends SpecialPage{
             <h2 id='SK'>Saskatchewan</h2>
             <div>
                 <ul>
-                    <li>Canadian distributor Celopharma as not reported shipments here to date (Jan. 28th, 2017)</li>
+                    <li>As of Jan. 28, 2017, Canadian distributor Celopharma as not reported shipments here to date</li>
                 </ul>
             </div>
             
             <h2 id='YT'>Yukon</h2>
             <div>
                 <ul>
-                    <li>Celopharma has shipped mifepristone to clinics in Whitehorse (Feb. 24th, 2017)</li>
+                    <li>As of Feb. 24, 2017, Celopharma has shipped mifepristone to clinics in Whitehorse</li>
                 </ul>
             </div>
         </div>
