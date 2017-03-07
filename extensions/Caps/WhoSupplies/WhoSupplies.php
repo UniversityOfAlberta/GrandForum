058e8dda9a39c265308131b0db15683c53b19bd3
Finalfantasykid <finalfantasykid13@gmail.com>
2017-03-07 11:35:22 -0700

    -Added some additional lines to the WhoSupplies
    Former-commit-id: 2b057c6619a0724533aa8f69d322fdc078814d20

--- extensions/Caps/WhoSupplies/WhoSupplies.php
+++ extensions/Caps/WhoSupplies/WhoSupplies.php
@@ -27,6 +27,17 @@ class WhoSupplies extends SpecialPage{
                 <ul>
                     <li>Health Canada’s <a href='https://www.caps-cpca.ubc.ca/AnnokiUploadAuth.php/5/5e/Patients_mifegymisopatientconsentforme11_29_16.pdf' target='_blank'>Consent Form</a> & <a href='https://www.caps-cpca.ubc.ca/AnnokiUploadAuth.php/7/7a/Patients_Mifegymiso_Eng_guide_01_19_17.pdf' target='_blank'>Patient Information Guide</a></li>
                     <li>Awaiting the <a href='https://www.cadth.ca/about-cadth/what-we-do/products-services/cdr' target='_blank'>Canadian Agency for Drugs and Technologies in Health</a> (CADTH) Common Drug Review of Mifegymiso®, scheduled for March 2017. Approval may impact insurance coverage and provincial and territorial formularies (i.e list publically-funded drugs for individuals who qualify and/or do not have private insurance, for example, individuals on social assistance).</li>
+                    <li>As of March 4th, 2017, private insurance providers intent on Mifegymiso® coverage are:
+                        <ol>
+                            <li>Great West Life: Open coverage</li>
+                            <li>Medavie Blue Cross: Open coverage</li>
+                            <li>Manulife: Open Coverage</li>
+                            <li>Pacific Blue Cross: In review; a response anticipated in 1 month</li>
+                            <li>Alberta Blue Cross: In review; a response anticipated in 1 month.</li>
+                            <li>TELUS: still in review</li>
+                            <li>Greenshield: in review</li>
+                        </ol>
+                    </li>
                 </ul>
             </div>
 
@@ -35,6 +46,7 @@ class WhoSupplies extends SpecialPage{
                 <ul>
                     <li>As reported in the media, Mifegymiso®, ~ $300/dose (Oct. 7th, 2016) <a href='http://edmontonjournal.com/news/politics/paula-simons-access-to-abortion-drug-one-step-closer-for-alberta-women' target='_blank'>read more</a></li>
                     <li>As of Feb. 24, 2017, Celopharma has shipped mifepristone to clinics in Calgary (Feb. 24th, 2017)</li>
+                    <li>As reported in the media, Calgary’s Kensington Clinic is providing Mifegymiso® for free, <a href='https://www.thestar.com/opinion/commentary/2017/03/06/abortion-pill-rollout-deeply-flawed-mallick.html' target='_blank'>read more</a></li>
                 </ul>
             </div>
             
