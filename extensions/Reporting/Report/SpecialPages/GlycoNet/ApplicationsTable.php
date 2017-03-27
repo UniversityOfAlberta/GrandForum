b968d68f7f8fd9436d46a98a1e2c43747820c05f
Finalfantasykid <finalfantasykid13@gmail.com>
2017-03-27 10:32:28 -0600

    -Added reviewers to collab
    Former-commit-id: d03766280742924cadb69203b23ee0519567e8e2

--- extensions/Reporting/Report/SpecialPages/GlycoNet/ApplicationsTable.php
+++ extensions/Reporting/Report/SpecialPages/GlycoNet/ApplicationsTable.php
@@ -174,7 +174,7 @@ class ApplicationsTable extends SpecialPage{
         $reviewers->setAttr("class", "wikitable");
         $reviewers->setAttr("orientation", "list");
         $reviewers->setId("reviewers");
-        $tabbedPage->addTab(new ApplicationTab(array('RP_COLLAB_04_2017'), $this->allNis, 2017, "04-2017"));
+        $tabbedPage->addTab(new ApplicationTab(array('RP_COLLAB_04_2017'), $this->allNis, 2017, "04-2017", array($reviewers)));
         $tabbedPage->addTab(new ApplicationTab(array('RP_COLLAB'), $this->allNis, 2016, "2016"));
         $wgOut->addHTML($tabbedPage->showPage());
     }
