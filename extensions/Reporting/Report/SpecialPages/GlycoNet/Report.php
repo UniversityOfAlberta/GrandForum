f1fa413a1866d70a61f263f82facd678fa671c5a
Finalfantasykid <finalfantasykid13@gmail.com>
2017-03-28 11:30:39 -0600

    -Changed the conditions for collaborative review
    Former-commit-id: 43ecd4df763b8270689d0b0fb11f7bb6b0dbbe61

--- extensions/Reporting/Report/SpecialPages/GlycoNet/Report.php
+++ extensions/Reporting/Report/SpecialPages/GlycoNet/Report.php
@@ -91,7 +91,7 @@ class Report extends AbstractReport{
             $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "SABCatalystReview")) ? "selected" : false;
             $tabs["Reviews"]['subtabs'][] = TabUtils::createSubTab("Catalyst Review", "{$url}SABCatalystReview", $selected);
         }
-        if(count($person->getEvaluates("SAB-Collaborative")) > 0){
+        if(count($person->getEvaluates("SAB-Collaborative", 2017)) > 0){
             $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "SABCollaborativeReview")) ? "selected" : false;
             $tabs["Reviews"]['subtabs'][] = TabUtils::createSubTab("Collab Review", "{$url}SABCollaborativeReview", $selected);
         }
