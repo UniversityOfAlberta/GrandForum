d5c27aaf264dea796dcf2344545661886c25ae98
Finalfantasykid <finalfantasykid13@gmail.com>
2017-03-28 09:20:42 -0600

    -Disabled the Collaborative Application
    Former-commit-id: 9e0fbb7d40308e04e2661f40723f1f9cc7d75b7c

--- extensions/Reporting/Report/SpecialPages/GlycoNet/Report.php
+++ extensions/Reporting/Report/SpecialPages/GlycoNet/Report.php
@@ -69,8 +69,8 @@ class Report extends AbstractReport{
         }*/
         if($person->isRole(NI) || $person->isRole(NI.'-Candidate') || 
            $person->isRole(EXTERNAL) || $person->isRole(EXTERNAL.'-Candidate')){
-            $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "CollaborativeReport042017")) ? "selected" : false;
-            $tabs["Proposals"]['subtabs'][] = TabUtils::createSubTab("Collaborative", "{$url}CollaborativeReport042017", $selected);
+            //$selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "CollaborativeReport042017")) ? "selected" : false;
+            //$tabs["Proposals"]['subtabs'][] = TabUtils::createSubTab("Collaborative", "{$url}CollaborativeReport042017", $selected);
             
             $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "TranslationalReport")) ? "selected" : false;
             $tabs["Proposals"]['subtabs'][] = TabUtils::createSubTab("Translational", "{$url}TranslationalReport", $selected);
