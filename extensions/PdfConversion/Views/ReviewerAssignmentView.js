1b676f48f29f07cfede7cfb9dae5256a3a645f12
David Turner <dwt@ssrg5.cs.ualberta.ca>
2017-04-04 14:20:50 -0600

    -Added missing files
    Former-commit-id: 971d58980603fa740febdc7881855b8151dd67c7

---
+++ extensions/PdfConversion/Views/ReviewerAssignmentView.js
@@ -0,0 +1,51 @@
+ReviewerAssignmentView = Backbone.View.extend({
+
+    template: _.template($("#reviewer_assignment_template").html()),
+
+    initialize: function(options){
+        this.people = new People();
+        this.people.roles = [NI];
+        this.listenTo(this.people, "sync", $.proxy(function(){
+            this.render();
+        }, this));
+        this.people.fetch();
+        this.model.on("change:person", this.render);
+    },
+    
+    changeSelection: function(){
+        var person = this.people.findWhere({id: this.$("#member-select").val()});;
+        this.model.set('person', person);
+    },
+    
+    upload: function(){
+        var button = $("#upload");
+        button.prop("disabled", true);
+        this.$(".throbber").show();
+        ccvUploaded = $.proxy(function(success, errors){
+            // Purposefully global so that iframe can access
+                clearAllMessages();
+                if(success != ""){
+                    addSuccess(success);
+                }
+                if(errors != ""){
+                    addError(errors);
+                }
+                button.prop("disabled", false);
+            this.$(".throbber").hide();
+        }, this);
+        var form = this.$("form");
+        form.submit();
+    },
+    
+    events: {
+        "change #member-select": "changeSelection",
+        "click #upload": "upload"
+    },
+    
+    render: function(){
+        this.$el.html(this.template(this.model.toJSON()));
+        this.$("#member-select").chosen();
+        return this.$el;
+    }
+
+});
