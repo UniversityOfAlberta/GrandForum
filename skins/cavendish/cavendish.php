6a6b2ca729e424472a96fb5dece9d95ee23d4cdb
Finalfantasykid <finalfantasykid13@gmail.com>
2017-04-07 10:31:48 -0600

    -Added the Latest News link to the sidebar
    Former-commit-id: 4a23f34bf304ad2573360039a170633d467b14b6

--- skins/cavendish/cavendish.php
+++ skins/cavendish/cavendish.php
@@ -1100,10 +1100,10 @@ $(function(){
 
             $latestnews_tab = array(TabUtils::createToolboxLink($title, "$wgServer$wgScriptPath/index.php/Special:LatestNews"));
 
+            array_splice($GLOBALS['toolbox']['Other']['links'],2,0,$latestnews_tab);
             array_splice($GLOBALS['toolbox']['Other']['links'],2,0,$whosupplies_tab);
             array_splice($GLOBALS['toolbox']['Other']['links'],0,0,$poll_tab);
             array_splice($GLOBALS['toolbox']['Other']['links'],0,0,$resources_tab);
-            //array_splice($GLOBALS['toolbox']['Other']['links'],0,0,$latestnews_tab);
 	        foreach($toolbox as $key => $header){
 	            if(count($header['links']) > 0){
 	                $hr = ($i > 0) ? "" : "";
