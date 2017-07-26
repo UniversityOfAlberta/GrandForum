41bb32e92e0d4c94022de2e3c1b76e934fcb63c6
Finalfantasykid <finalfantasykid13@gmail.com>
2017-07-26 14:24:38 -0600

    -Added a tab for the KT Application

--- extensions/Reporting/Report/SpecialPages/CFN/Report.php
+++ extensions/Reporting/Report/SpecialPages/CFN/Report.php
@@ -50,6 +50,10 @@ class Report extends AbstractReport{
                 }
             }
         }
+        if(($person->isRole(NI) || $person->isRole(NI."-Candidate")) && $person->isSubRole("KT2017Applicant")){
+            $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "KTApplication")) ? "selected" : false;
+            $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("KT Application", "{$url}KTApplication", $selected);
+        }
         if(($person->isRole(HQP) || $person->isRole(HQP."-Candidate")) && $person->isSubRole("IFP2017Applicant")){
             $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "IFPApplication")) ? "selected" : false;
             $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("IFP Application", "{$url}IFPApplication", $selected);
