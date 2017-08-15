a6e9d47a7467bb0510ce45f37152ceb2debbbfb2
Finalfantasykid <finalfantasykid13@gmail.com>
2017-08-15 10:23:49 -0600

    -Cleaned up and optimized some really old code

--- extensions/EditMember/EditMember.php
+++ extensions/EditMember/EditMember.php
@@ -232,10 +232,7 @@ class EditMember extends SpecialPage{
     function generateMain(){
         global $wgOut, $wgUser, $wgServer, $wgScriptPath, $wgTitle;
         $me = Person::newFromWgUser();
-        $wgOut->addScript('<script type="text/javascript">
-                            var sort = "first";
-                            var allPeople = new Array(');
-        $allPeople = array_merge(Person::getAllPeople('all'), Person::getAllCandidates('all'));
+        $allPeople = Person::getAllCandidates('all');
         $i = 0;
         $names = array();
         foreach($allPeople as $person){
@@ -250,164 +247,40 @@ class EditMember extends SpecialPage{
                 $names[] = $person->getName();
             }
         }
-        $wgOut->addScript('\''.implode("','", $names).'\');
-        var oldOptions = Array();
-
-        function filterResults(value){
-            $.each($("#names").children().not("#no"), function(index, value){
-                var valSelect = value.id;
-                oldOptions[valSelect] = $(value).detach();
-            });
-            
-            if(value == ""){
-                var no = $("#no").detach();
-                if(no.length > 0){
-                    oldOptions["no"] = no;
-                }
-            }
-            var n = 0;
-            var tmpDom = $("<select>");
-            for(i = 0; i < allPeople.length; i++){
-                var val = allPeople[i];
-                var valSelect = "";
-                if(sort == "last" && val.indexOf(".") != -1){
-                    if(val.split(".").length == 2){
-                        var firstName = val.split(".")[0];
-                        var lastName = val.split(".")[1];
-                    }
-                    else{
-                        var firstName = val.substring(0, val.lastIndexOf("."));
-                        var lastName = val.split(".")[val.split(".").length - 1];
-                    }
-                    valSelect = lastName + firstName;
-                    valSelect = valSelect.replace(/\./g, "");
-                }
-                else{
-                    valSelect = val.replace(/\./g, "");
-                }
-                if(unaccentChars(val.replace(/\./g, " ")).regexIndexOf(unaccentChars(value)) != -1 || (typeof oldOptions[valSelect] != "undefined" && unaccentChars(oldOptions[valSelect].attr("class")).regexIndexOf(unaccentChars(value)) != -1)){
-                    if(typeof oldOptions[valSelect] != "undefined"){
-                        oldOptions[valSelect].appendTo(tmpDom);
-                    }
-                    n++;
-                }
-            }
-            $("#names").html(tmpDom.html());
-            if(n == 0){
-                if(typeof oldOptions["no"] != "undefined"){
-                    oldOptions["no"].appendTo($("#names"));
-                }
-            }
-            else{
-                var no = $("#no").detach();
-                if(no.length > 0){
-                    oldOptions["no"] = no;
-                }
-            }
-        }
         
-        function sortBy(type){
-            var newAllPeople = Array();
-            for(i = 0; i < allPeople.length; i++){
-                var fullName = allPeople[i];
-                var firstName = "";
-                var lastName = "";
-                if(fullName.indexOf(".") != -1){
-                    if(type == "last" && sort == "first" || type == "first" && sort == "last"){
-                        firstName = fullName.split(".")[0];
-                        if(fullName.split(".").length == 2){
-                            lastName = fullName.split(".")[1];
-                        }
-                        else{
-                            lastName = fullName.substring(fullName.indexOf(".") + 1);
-                        }
-                    }
-                    else{
-                        if(fullName.split(".").length == 2){
-                            firstName = fullName.split(".")[1];
-                        }
-                        else{
-                            firstName = fullName.substring(fullName.indexOf(".") + 1);
-                        }
-                        lastName = fullName.split(".")[0];
-                    }
-                    newAllPeople[i] = lastName + "." + firstName;
-                }
-                else{
-                    newAllPeople[i] = fullName;
-                }
-            }
-            sort = type;
-            allPeople = newAllPeople;
-            allPeople.sort();
-            filterResults($("#search").val());
-        }
-        
-        $(document).ready(function(){
-            var no = $("#no").detach();
-            if(no.length > 0){
-                oldOptions["no"] = no;
-            }
-            
-            $("#search").keypress(function(event) {
-                if(event.keyCode == 40){        //DOWN
-                    $.each($("#names").children(":selected").not("#no"), function(index, value){
-                        if($(value).next().length > 0){
-                            $(value).attr("selected", false);
-                            $(value).next().attr("selected", true);
-                        }
-                    });
-                }
-                else if(event.keyCode == 38){   //UP
-                    $.each($("#names").children(":selected").not("#no"), function(index, value){
-                        if($(value).prev().length > 0){
-                            $(value).attr("selected", false);
-                            $(value).prev().attr("selected", true);
-                        }
-                    });
-                }
-            });
-            
-            $("#search").keyup(function(event) {
-                if(event.keyCode != 40 && event.keyCode != 38){
-                    filterResults(this.value);
-                }
-            });
-            sortBy("first");
-        });
-        </script>');
         if($me->isRoleAtLeast(STAFF)){
             foreach(Person::getAllStaff() as $person){
                 $allPeople[] = $person;
             }
         }
         $wgOut->addHTML("This page can be used to edit the roles and projects of members on the forum.<br />
-                         Select a user from the list below, and then click the 'Next' button.  You can filter out the selection box by searching a name or project below.<table>
-                            <tr><td>
-                                <a href='javascript:sortBy(\"first\");'>Sort by First Name</a> | <a href='javascript:sortBy(\"last\");'>Sort by Last Name</a><br />
-                                <b>Search:</b> <input style='width:100%;' id='search' type='text' />
-                            </td></tr>
+                         Select a user from the list below, and then click the 'Next' button.<table>
                             <tr><td>
                             <form action='$wgServer$wgScriptPath/index.php/Special:EditMember' method='post'>
-                                <select id='names' name='name' size='10' style='width:100%'>
-                                    <option id='no' disabled>Search did not match anyone</option>\n");
+                                <select data-placeholder='Choose a Person...' id='names' name='name' size='10' style='width:100%'>");
         foreach($allPeople as $person){
-            $projects = $person->getProjects(false, true);
-            $projs = array();
-            foreach($projects as $project){
-                $projs[] = $project->getName();
-            }
             if(!$me->isAllowedToEdit($person)){
                 // User does not have permission for this person
                 continue;           
             }
-            $wgOut->addHTML("<option class='".implode(" ", $projs)."' id='".str_replace(".", "", $person->getName())."' value=\"".str_replace(".", " ", $person->getName())."\">{$person->getNameForForms()}</option>\n");
+            $wgOut->addHTML("<option value=\"{$person->getName()}\">{$person->getNameForForms()}</option>\n");
         }
         $wgOut->addHTML("</select>
                 </td></tr>
                 <tr><td>
-            <input type='submit' name='next' value='Next' />
-        </form></td></tr></table>");
+            <input id='button' type='submit' name='next' value='Next' disabled='disabled' />
+        </form></td></tr></table>
+        <script type='text/javascript'>
+	        $('#names').chosen();
+	        $(document).ready(function(){
+	            $('#names').change(function(){
+	                var page = $('#names').val();
+	                if(page != ''){
+	                    $('#button').prop('disabled', false);
+	                }
+	            });
+	        });
+	    </script>");
     }
     
     function generateEditMemberFormHTML($wgOut){
