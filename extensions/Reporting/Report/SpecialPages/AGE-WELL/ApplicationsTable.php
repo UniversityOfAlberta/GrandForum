b8687774cfeb43029f37387816bbf64ccad93e0a
Finalfantasykid <finalfantasykid13@gmail.com>
2017-09-18 12:14:02 -0600

    -Changed the order of the evaluation years

--- extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
@@ -215,8 +215,8 @@ class ApplicationsTable extends SpecialPage{
     function generateProject(){
         global $wgOut;
         $tabbedPage = new InnerTabbedPage("reports");
-        $tabbedPage->addTab(new ApplicationTab('RP_PROJ_EVALUATION', $this->projects, 2015, "2016"));
         $tabbedPage->addTab(new ApplicationTab('RP_PROJ_EVALUATION', $this->projects, 2016, "2017"));
+        $tabbedPage->addTab(new ApplicationTab('RP_PROJ_EVALUATION', $this->projects, 2015, "2016"));
         $wgOut->addHTML($tabbedPage->showPage());
     }
     
