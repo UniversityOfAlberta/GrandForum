d3a482149b1ac8087c5b814be3faa0b058c686f7
Ruby De Jesus <rdejesus@ualberta.ca>
2017-10-27 12:12:58 -0600

    fixed filter mine

--- extensions/Reporting/Report/SpecialPages/CSGARS/Sops/Views/SopsView.js
+++ extensions/Reporting/Report/SpecialPages/CSGARS/Sops/Views/SopsView.js
@@ -171,7 +171,7 @@ SopsView = Backbone.View.extend({
     filterMineOnly: function(settings,data,dataIndex){
         var input = me.get('fullName').toUpperCase();
         if($('#filterMeOnly').is(':checked')){
-            var name = data[18];
+            var name = data[17];
             if(name.toUpperCase().indexOf(input) > -1){
                 return true;
             }
