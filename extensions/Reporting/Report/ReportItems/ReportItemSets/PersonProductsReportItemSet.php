5453ae7baf884b602a9e9917b28f957f34aeb079
David Turner <finalfantasykid13@gmail.com>
2017-11-07 11:05:52 -0700

    -Don't show header if there is no content

--- extensions/Reporting/Report/ReportItems/ReportItemSets/PersonProductsReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/PersonProductsReportItemSet.php
@@ -18,14 +18,14 @@ class PersonProductsReportItemSet extends ReportItemSet {
         foreach($categories as $cat){
             $products = array_merge($products, $person->getPapersAuthored($cat, $start_date, $end_date, $includeHQP));
         }
-
+        
         usort($products, function($a, $b){
             return (str_replace("0000-00-00", "9999-99-99", $a->getDate()) < str_replace("0000-00-00", "9999-99-99", $b->getDate())) ? 1 : -1;
         });
-        //$products = $person->getPapers($category, false, 'both', true, "Public");
+        
         if(is_array($products)){
             foreach($products as $prod){
-                if((count($productType) == 0 || in_array($prod->getType(), $productType)) &&
+                if((implode("", $productType) == "" || in_array($prod->getType(), $productType)) &&
                    ($peerReviewed == "" || ($prod->getData('peer_reviewed') == $peerReviewed)) &&
                    ($status == "" || ($prod->getStatus() == $status))){
                     $tuple = self::createTuple();
