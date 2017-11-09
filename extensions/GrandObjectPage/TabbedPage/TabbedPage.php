8821a34e267d66aa97ccee80b626b58064a86412
David Turner <finalfantasykid13@gmail.com>
2017-11-09 10:23:20 -0700

    -Fixed TabbedPage

--- extensions/GrandObjectPage/TabbedPage/TabbedPage.php
+++ extensions/GrandObjectPage/TabbedPage/TabbedPage.php
@@ -31,6 +31,117 @@ class TabbedPage {
     function addTab($tab){
         $this->tabs[] = $tab;
     }
+    
+    // Writes all of the html
+    function showPage($init_tab = 0){
+        global $wgOut, $wgServer, $wgScriptPath, $wgTitle, $wgMessage;
+        $active_tab = $init_tab;
+        $activeTabIndex = "";
+        $i = 0;
+
+        foreach($this->tabs as $tab){
+            if($tab instanceof AbstractEditableTab && $tab->canEdit()){
+                if(isset($_POST['submit']) && 
+                   ($_POST['submit'] == "Save {$tab->name}" || ($tab instanceof AbstractInlineEditableTab && $_POST['submit'] == "{$tab->name}"))){
+                    $activeTabIndex = $tab->id;
+                    $active_tab = $i;
+                    $errors = $tab->handleEdit();
+                    if($errors != null && $errors != ""){
+                        $wgMessage->addError("$errors");
+                        $_POST['submit'] = "Edit {$tab->name}";
+                        $_POST['edit'] = true;
+                    }
+                    else{
+                        $wgMessage->addSuccess("'{$tab->name}' updated successfully.");
+                    }
+                }
+                if(isset($_POST['submit']) && ($_POST['submit'] == "Edit {$tab->name}" || $_POST['submit'] == "{$tab->name}" || $_POST['submit'] == "Cancel" ) ) {
+                    $activeTabIndex = $tab->id;
+                    $active_tab = $i;
+                }
+            }
+            if(isset($_GET['tab']) && $_GET['tab'] == $tab->id){
+                $activeTabIndex = $tab->id;
+                $active_tab = $i;
+            }
+            $i++;
+        }
+        
+        $wgOut->addHTML("<form action='$wgServer$wgScriptPath/index.php/{$wgTitle->getNsText()}:{$wgTitle->getText()}' method='post' enctype='multipart/form-data'>
+            <div id='currentViewSpinner' style='text-align:center;margin-top:10%;'></div>
+            <script type='text/javascript'>
+                spin = spinner('currentViewSpinner', 40, 75, 12, 10, '#888');
+            </script>");
+        $wgOut->addHTML("<div style='display:none;' id='{$this->id}'>");
+        $wgOut->addHTML("<ul>");
+        foreach($this->tabs as $tab){
+            if($tab instanceof AbstractEditableTab){
+                if($tab->canEdit() && isset($_POST['edit'])){
+                    $tab->generateEditBody();
+                    $tab->showSaveButton();
+                    $tab->showCancelButton();
+                }
+                else if($tab->canEdit() && !isset($_POST['edit'])){
+                    $tab->generateBody();
+                    $tab->showEditButton();
+                }
+                else{
+                    $tab->generateBody();
+                }
+            }
+            else{
+                $tab->generateBody();
+            }
+            if($tab->html != ""){
+                $wgOut->addHTML("<li title='".str_replace("'", "&#39;", $tab->tooltip)."' class='tooltip'><a href='#{$tab->id}'>{$tab->name}</a></li>");
+            }
+        }
+        $wgOut->addHTML("</ul><h1 class='custom-title'>{$wgOut->getPageTitle()}</h1>");
+        $i = 0;
+        foreach($this->tabs as $tab){
+            if($tab->html != ""){
+                $wgOut->addHTML("<div id='{$tab->id}' style='overflow-x:auto;'>{$tab->html}</div>");
+                if($tab->id == $activeTabIndex){
+                    $active_tab = $i;
+                }
+                $i++;
+            }
+        }
+        $wgOut->addHTML("</div>\n</form>");
+        $wgOut->addHTML("<script type='text/javascript'>
+                var selectedTab = $('#{$this->id} > ul > .ui-tabs-selected');
+                if(selectedTab.length > 0){
+                    // If the tabs were created previously but removed from the dome, 
+                    // make sure to reselect the same tab as before
+                    var i = 0;
+                    $.each($('#{$this->id} li.ui-state-default'), function(index, val){
+                        if($(val).hasClass('ui-tabs-selected')){
+                            i = index;
+                        }
+                    });
+                    $('#{$this->id}').tabs({ selected: i });
+                }
+                else{
+                    $('#{$this->id}').tabs({ selected: {$active_tab} });
+                }
+                $('#currentViewSpinner').remove();
+                $('#{$this->id}').show();
+        </script>");
+        foreach($this->tabs as $key => $tab){
+            $wgOut->addHTML("<script type='text/javascript'>
+                $('#{$this->id}').bind('tabsselect', function(event, ui) {
+                    if(ui.panel.id == '{$tab->id}'){
+                        {$tab->tabSelect()}
+                    }
+                });
+            </script>");
+            if(isset($_POST['edit']) && $_POST['edit'] == true){
+                $wgOut->addHTML("<script type='text/javascript'>
+                    $('#{$this->id}').tabs('disable', {$key});
+                </script>");
+            }
+        }
+    }
 
 }
 
