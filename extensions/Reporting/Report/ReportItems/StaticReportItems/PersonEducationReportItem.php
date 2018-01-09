6b2685be51bac75a9175a7503c8638978d73bd4f
David Turner <finalfantasykid13@gmail.com>
2018-01-09 15:51:03 -0700

    -Fixed issue with education

--- extensions/Reporting/Report/ReportItems/StaticReportItems/PersonEducationReportItem.php
+++ extensions/Reporting/Report/ReportItems/StaticReportItems/PersonEducationReportItem.php
@@ -10,7 +10,7 @@ class PersonEducationReportItem extends StaticReportItem {
         $employment = array_reverse($person->getUniversities());
         $items = array();
         foreach($employment as $emp){
-            if(in_array($emp['position'], array("Undergraduate", "Graduate Student - Master's", "Graduate Student - Doctoral", "Post-Doctoral Fellow"))){
+            if(in_array($emp['position'], array("Undergraduate", "Graduate Student - Master's", "Graduate Student - Master's Thesis", "Graduate Student - Master's Course", "Graduate Student - Doctoral", "Post-Doctoral Fellow"))){
                 $startYear = substr($emp['start'], 0, 4)." - ";
                 $endYear = substr($emp['end'], 0, 4);
                 if($startYear == "0000 - "){
