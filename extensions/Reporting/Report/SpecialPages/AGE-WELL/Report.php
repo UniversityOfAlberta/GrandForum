ba0b091cdb63bf5584e1791bb0cb27458e319518
Finalfantasykid <finalfantasykid13@gmail.com>
2018-01-15 09:16:24 -0700

    -Changed over the access application

--- extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
@@ -63,8 +63,8 @@ class Report extends AbstractReport {
             $tabs["Reports"]['subtabs'][] = TabUtils::createSubTab("Annual Report - EPIC Survey", "{$url}EPICReport", $selected);
         }
         if($person->isRole(HQP)){
-            $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "AccessApplication012018")) ? "selected" : false;
-            $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("ACCESS Application", "{$url}AccessApplication012018", $selected);
+            $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "AccessApplication042018")) ? "selected" : false;
+            $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("ACCESS Application", "{$url}AccessApplication042018", $selected);
         }
         if($person->isRole(HQP) || $person->isRole(HQP.'-Candidate')){
             /*$selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "SummerApplication")) ? "selected" : false;
