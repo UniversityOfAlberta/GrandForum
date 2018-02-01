b18a11861c09438dc24513741c6376d7489de473
Finalfantasykid <finalfantasykid13@gmail.com>
2018-02-01 10:45:49 -0700

    -Fixed some duplicate names showing up on some pages

--- extensions/EditMember/EditMember.php
+++ extensions/EditMember/EditMember.php
@@ -242,17 +242,7 @@ class EditMember extends SpecialPage{
             }
             $names[] = $person->getName();
         }
-        if($me->isRoleAtLeast(STAFF)){
-            foreach(Person::getAllStaff() as $person){
-                $names[] = $person->getName();
-            }
-        }
         
-        if($me->isRoleAtLeast(STAFF)){
-            foreach(Person::getAllStaff() as $person){
-                $allPeople[] = $person;
-            }
-        }
         $wgOut->addHTML("This page can be used to edit the roles and projects of members on the forum.<br />
                          Select a user from the list below, and then click the 'Next' button.<table>
                             <tr><td>
