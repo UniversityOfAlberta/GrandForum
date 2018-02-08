afc532cf21f9976849e8686b660cdd31646f7944
Spencer Plant <splant.10@gmail.com>
2018-02-08 15:44:53 -0700

    additions

--- extensions/GrandObjects/BibTexExporter.php
+++ extensions/GrandObjects/BibTexExporter.php
@@ -21,36 +21,36 @@ class BibTexExporter {
 	* returns a String
 	*/
 	static function exportProduct($paper) {
+		print_r($paper);
+		print_r("\n");
 		$bibtex = "@";
 		$data = $paper->data;
-		$type = self::getType($paper->type);
-		$bibtex .= $type . "{" . $paper->bibtex_id . ",\n";
+		$something = array_flip(ImportBibTeXAPI::$bibtexHash);
+		$bibtex .= strtoupper($something[$paper->type]) . "{" . $paper->bibtex_id . ",\n";
 		$bibtex .= "author={";
 		$temp_arr = array();
 		foreach($paper->getAuthors() as $person) {
-			array_push($temp_arr, $person->getNameForProduct("{%Last}, {%F.}{ %M.}"));
+			array_push($temp_arr, $person->getNameForProduct("{%Last}, {%F.}{%M.}"));
 		}
 		$bibtex .= implode(" and ", $temp_arr);
 		$bibtex .= "},\n";
 		$bibtex .= "title={" . $paper->title . "},\n";
-		$bibtex .= "journal={" . $data['event_title'] . "},\n"; # this might need to change depending on type of product
+		// check type
+		$structure = Product::structure();
+		$data_structure = $structure['categories'][$paper->getCategory()]['types'][$paper->getType()]['data'];
+		foreach($data as $k=>$d) {
+			if (isset($data_structure[$k])) {
+				if ($data_structure[$k]['bibtex']) {
+					$bibtex .= "{$data_structure[$k]['bibtex']}={{$d}},\n";
+				}
+			}
+		}
+		//$bibtex .= "journal={" . @$data['event_title'] . "},\n"; # this might need to change depending on type of product
 		$bibtex .= "year={" . $paper->getYear() . "},\n";
+		$bibtex .= "}";
 		print_r($bibtex);
 		exit;
 	}
 
-	private static function getType($type) {
-		// switch different product type to get first thing in bibtex
-		switch ($type) {
-			case "Conference Paper":
-				return "CONFERENCE";
-			default:
-				return $type;
-		} 
-	}
-
-	private static function formatAuthors($authors) {
-
-	}
 }
 ?>
