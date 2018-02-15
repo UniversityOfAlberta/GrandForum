f9bf61b5fa24f50c7d267d8991621ce1800cc79a
Yuxuan Zhao <yzhao9@ualberta.ca>
2018-02-15 15:18:03 -0700

    finish the color encode

--- extensions/Caps/LatestNews/LatestNews.php
+++ extensions/Caps/LatestNews/LatestNews.php
@@ -41,10 +41,33 @@ class LatestNews extends SpecialPage{
                 $en = file_get_contents($_FILES['en']['tmp_name']);
                 $fr = file_get_contents($_FILES['fr']['tmp_name']);
                 $date = $_POST['date'];
+                $enTitle = $_POST['enTitle'];
+                $frTitle = $_POST['frTitle'];
+                $type = $_POST['type'];
+                if($type == 'CAPS Collaboration'){
+                    $color = "#000000";
+                }
+                else if($type == 'URGENT News'){
+                    $color = "#9DC3E3";
+                }
+                else if($type == 'Regulation/Policy'){
+                    $color = "#8FABD9";
+                }
+                else if($type == 'Coverage/Access'){
+                    $color = "#224F77";
+                }
+                else if($type == 'Resource'){
+                    $color = "#224F77";
+                }
                 DBFunctions::insert('grand_latest_news',
                                     array('date' => $date,
                                           'en' => $en,
-                                          'fr' => $fr));
+                                          'fr' => $fr,
+                                          'enTitle' => $enTitle,
+                                          'frTitle' => $frTitle,
+                                          'type' => $type,
+                                          'color'=>$color
+                                      ));
                 $wgMessage->addSuccess("Files Uploaded");
                 redirect("$wgServer$wgScriptPath/index.php/Special:LatestNews");
             }
@@ -59,6 +82,14 @@ class LatestNews extends SpecialPage{
                 <form action='$wgServer$wgScriptPath/index.php/Special:LatestNews' method='post' enctype='multipart/form-data'>
                     <table>
                         <tr>
+                            <td align='right'><b>Title(en):</b></td>
+                            <td><input type='text' name='enTitle'/></td>
+                        </tr>
+                        <tr>
+                            <td align='right'><b>Title(fr):</b></td>
+                            <td><input type='text' name='frTitle'/></td>
+                        </tr>
+                        <tr>
                             <td align='right'><b>English:</b></td>
                             <td><input type='file' name='en' accept='.pdf' /></td>
                         </tr>
@@ -69,6 +100,22 @@ class LatestNews extends SpecialPage{
                         <tr>
                             <td align='right'><b>Date:</b></td>
                             <td><input type='text' name='date' value='$defaultDate' /></td>
+                        </tr>
+
+                        <tr>
+                        <td align='right'><b>
+                        Type: </b>
+                        </td>
+                        <td>
+                        <select name='type'>
+                          <option value=''>Choose type...</option>
+                          <option value='CAPS Collaboration'>CAPS Collaboration</option>
+                          <option value='Regulation/Policy'>Regulation/Policy</option>
+                          <option value='Coverage/Access'>Coverage/Access</option>
+                          <option value='URGENT News'>URGENT News</option>
+                          <option value='Resource'>Resource</option>
+                        </select>
+                        </td>
                         </tr>
                         <tr>
                             <td></td>
@@ -86,12 +133,23 @@ class LatestNews extends SpecialPage{
             </div><br /><br />");
         }
         $data = DBFunctions::select(array('grand_latest_news'),
-                                    array('id', 'date'),
+                                    array('id', 'date','enTitle','frTitle','color','type'),
                                     array(),
-                                    array('date' => 'DESC', 'id' => 'DESC'));
+                                    array('date' => 'DESC', 'id' => 'DESC','enTitle' => 'DESC','frTitle' => 'DESC','type' => 'DESC','color' => 'DESC'));
         if(isset($data[0])){
             $pdfId = (isset($_GET['pdf'])) ? $_GET['pdf'] : $data[0]['id'];
             $wgOut->addHTML("<iframe src='https://docs.google.com/viewer?url=$wgServer$wgScriptPath/index.php?action=getPDF%26pdf={$pdfId}%26lang={$wgLang->getCode()}&embedded=true' width='800px' height='610px' frameborder='0'></iframe>");
+            $wgOut->addHTML("<div>
+                <fieldset>
+                <legend>Color Code</legend>
+                <div style='color:black'>CAPS Collaboration</div>
+                <div style='color:#8FABD9'>Regulation/Policy</div>
+                <div style='color:#224F77'>Coverage/Access</div>
+                <div style='color:#9DC3E3'>URGENT News</div>
+                <div style='color:#224F77'>Resource</div>
+
+                </fieldset>
+                </div>");
             if(count($data) > 1){
                 if($wgLang->getCode() == 'en'){
                     $header = "Previous News";
@@ -106,13 +164,14 @@ class LatestNews extends SpecialPage{
                         $date = explode("-",substr($row['date'], 0, 10));
                         if (!is_null($olddate)){
                             if ($olddate[1] != $date[1] || $olddate[0] != $date[0]){
+
                                 $wgOut->addHTML("</ul>");
                                 $monthNumber = explode("-",substr($row['date'], 0, 10))[1];
                                 $dateObj  = DateTime::createFromFormat('!m', $monthNumber);
                                 $monthName = $dateObj->format('F');
                                 $wgOut->addHTML("<h3>".$monthName." ".$date[0]."</h3>");
-
                             }
+
                         }
                         else{
                             $wgOut->addHTML("</ul>");
@@ -121,9 +180,9 @@ class LatestNews extends SpecialPage{
                             $monthName = $dateObj->format('F');
                             $wgOut->addHTML("<h3>".$monthName." ".$date[0]."</h3><ul>");
                         }
+                        $olddate = $date;
+                        $wgOut->addHTML("<li> (".substr($row['date'], 0, 10).") <a href='$wgServer$wgScriptPath/index.php/Special:LatestNews?pdf={$row['id']}' style='font-size: 1.25em; color:".$row['color'].";'>".$row['enTitle']."</a></li>");
 
-                        
-                        $wgOut->addHTML("<li><a href='$wgServer$wgScriptPath/index.php/Special:LatestNews?pdf={$row['id']}' style='font-size: 1.25em;'>".substr($row['date'], 0, 10)."</a></li>");
                     }
                 }
                 $wgOut->addHTML("</ul></div>");
