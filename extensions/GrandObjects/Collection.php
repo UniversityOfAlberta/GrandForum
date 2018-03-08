da0e4e45e4a027e749bddd5eff8ee9820fd1521d
David Turner <finalfantasykid13@gmail.com>
2018-03-08 12:00:17 -0700

    -Fixed php issue

--- extensions/GrandObjects/Collection.php
+++ extensions/GrandObjects/Collection.php
@@ -61,7 +61,8 @@ class Collection {
     function toSimpleArray(){
         $json = array();
         if(count($this->objects) > 0){
-            if(array_values($this->objects)[0] instanceof BackboneModel){
+            $values = array_values($this->objects);
+            if($values[0] instanceof BackboneModel){
                 foreach($this->objects as $object){
                     $json[] = $object->toSimpleArray();
                 }
