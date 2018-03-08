589f259bc55aba5f5f490e6ef301a6c872ff954e
Spencer Plant <splant.10@gmail.com>
2018-03-08 15:21:36 -0700

    signed in users should now be able to see their application status

--- extensions/GrandObjectPage/PersonPage/PersonSopTab.php
+++ extensions/GrandObjectPage/PersonPage/PersonSopTab.php
@@ -13,11 +13,48 @@ class PersonSopTab extends AbstractEditableTab {
     
     function generateBody(){
         global $wgOut, $wgUser, $wgTitle, $wgServer, $wgScriptPath;
-	$person = $this->person;
-	$url = $person->getSopPdfUrl();
-	if(!($url === false)){
-            $this->html .= "<iframe width='800' height='500' frameborder='0' src='$url'></iframe>";
-	} 
+    	$person = $this->person;
+        $gsms = GsmsData::newFromUserId($person->id);
+    	$url = $person->getSopPdfUrl();
+
+        $admissionStatus = $person->getSop()->getFinalAdmit();
+        $this->html .= "<div name='container' style='display:flex;'>";
+        $this->html .= "<div style='margin: 6px;padding: 0px 12px 15px 12px;'><h3>Application Decision</h3>$admissionStatus</div>";
+
+        if ($admissionStatus == "Admit") {
+
+            $supers = $gsms->getAssignedSupervisors()['q5'];
+            $this->html .= "<div style='margin: 6px;padding: 0px 12px 15px 12px;'><h3>Assigned Supervisor(s)</h3>";
+            if (count($supers) == 0) {
+                $this->html .= "<span style='color:#a5a5a5;'>Supervisors not yet assigned</span>";
+            } else {
+                $this->html .= "<ul>";
+                foreach($supers as $sup) {
+                    $this->html .= "<li>$sup</li>";
+                }
+                $this->html .= "</ul>";
+            }
+            $this->html .= "</div>";
+
+            $this->html .= "<div style='margin: 6px;padding: 0px 12px 15px 12px;'><h3>Funding Provided</h3>";
+            $funding = $gsms->getFunding();
+            if ($funding == "") {
+                $this->html .= "<span style='color:#a5a5a5;'>Decision not yet made</span>";
+            } else {
+                $this->html .= $funding;
+            }
+            $this->html .= "</div>";
+            
+        }
+        $this->html .= "</div>";
+
+    	if(!($url === false)){
+            $this->html .= "<div style='min-width:825px; max-width: 1000px; width:66%; height:500px;'><iframe style='width:100%; height:100%;' frameborder='0' src='$url'></iframe></div>";
+    	} 
+
+        
+
+
         return $this->html;
     }
 
@@ -25,10 +62,10 @@ class PersonSopTab extends AbstractEditableTab {
     }
 
     function handleEdit(){
-}
+    }
     
     function canEdit(){
-	return false;
+	   return false;
     }
     
 }
