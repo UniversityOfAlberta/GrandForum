7d22b388be2c89e323750415211ba6659a950428
Finalfantasykid <finalfantasykid13@gmail.com>
2018-03-20 15:49:23 -0600

    -More fixes

--- extensions/GrandObjectPage/Backbone/Views/ProjectSelectorView.js
+++ extensions/GrandObjectPage/Backbone/Views/ProjectSelectorView.js
@@ -62,8 +62,121 @@ ProjectSelectorView = Backbone.View.extend({
         return ret;
     },
     
-    events: {
+    select: function(projectId){
+        var projects = this.model.get('projects');
+        if(_.where(projects, {id: projectId}).length == 0){
+            projects.push({id: projectId});
+        }
+        // Only trigger an event if this is a parent
+        if(this.$("input[data-project=" + projectId + "]").attr('name') == 'project'){
+            this.model.trigger("change:projects");
+        }
+    },
     
+    unselect: function(projectId){
+        var project = _.findWhere(this.projects.models
+                                      .concat(this.otherProjects.models)
+                                      .concat(this.oldProjects.models), {id: projectId});
+        var projects = this.model.get('projects');
+
+        // Unselect all subprojects as well
+        if(project != undefined){
+            _.each(project.get('subprojects'), $.proxy(function(sub){
+                var index = _.indexOf(projects, _.findWhere(projects, {id: sub.id}));
+                if(index != -1){
+                    projects.splice(index, 1);
+                    this.$("input[data-project=" + sub.id + "]").prop('checked', false);
+                }
+            }, this));
+        }
+        projects.splice(_.indexOf(projects, _.findWhere(projects, {id: projectId})), 1);
+        // Only trigger an event if this is a parent
+        if(this.$("input[data-project=" + projectId + "]").attr('name') == 'project'){
+            this.model.trigger("change:projects");
+        }
+    },
+    
+    toggleSelect: function(e){
+        var target = $(e.currentTarget);
+        var projectId = target.attr('data-project');
+        if(target.is(":checked")){
+            // 'Check' Project
+            this.select(projectId);
+            if(target.attr('name') == "project"){
+                //this.$("div[data-project=" + projectId + "] div.subprojectPopup").slideDown();
+            }
+            else if(target.attr('name') == "subproject"){
+                var parentId = target.attr('data-parent');
+                this.$("div[data-project=" + parentId + "] div.subprojectPopup").show();
+            }
+            else if(target.attr('name') == "otherproject"){
+                $("div.otherSubProjects", target.parent()).slideDown();
+            }
+        }
+        else{
+            // 'Uncheck' Project
+            this.unselect(projectId);
+            if(target.attr('name') == "project"){
+                // Do nothing
+            }
+            else if(target.attr('name') == "subproject"){
+                var parentId = target.attr('data-parent');
+                this.$("div[data-project=" + parentId + "] div.subprojectPopup").show();
+            }
+            else if(target.attr('name') == "otherproject"){
+                $("div.otherSubProjects", target.parent()).slideUp();
+            }
+        }
+    },
+    
+    filterSearch: function(e){
+        var target = $(e.currentTarget);
+        var value = target.val();
+        var block = target.parent();
+        var options = $("div.popupMainProject", block);
+        options.each(function(i, el){
+            var text = $(el).text();
+            if(unaccentChars(text).indexOf(unaccentChars(value)) == -1){
+                $(el).slideUp(150);
+            }
+            else{
+                $(el).slideDown(150);
+            }
+        });
+    },
+    
+    showOther: function(e){
+        this.$("div.otherPopup").html(this.otherPopupTemplate(this.model.toJSON()));
+        var lastHeight = this.$el.prop("scrollHeight")
+        var interval = setInterval($.proxy(function(){
+            if(this.$el.prop("scrollHeight") > lastHeight){
+                this.$el.scrollTop(this.$el.scrollTop() + Math.abs(lastHeight - this.$el.prop("scrollHeight")));
+                lastHeight = this.$el.prop("scrollHeight");
+            }
+        }, this), 16);
+        this.$("div.otherPopup").slideDown($.proxy(function(){
+            clearInterval(interval);
+            if(this.$el.prop("scrollHeight") > lastHeight){
+                this.$el.scrollTop(this.$el.scrollTop() + Math.abs(lastHeight - this.$el.prop("scrollHeight")));
+                lastHeight = this.$el.prop("scrollHeight");
+            }
+        }, this));
+    },
+    
+    showSubprojects: function(e){
+        var target = $(e.currentTarget);
+        var projectId = target.attr('data-project');
+        var project = _.findWhere(this.projects.models, {id: projectId});
+        this.$("div[data-project=" + projectId + "] div.subprojectPopup").html(this.projectsPopupTemplate(_.extend(project.toJSON(), {projects: this.model.get('projects')})));
+        this.$("div[data-project=" + projectId + "] div.subprojectPopup").slideDown();
+    },
+    
+    events: {
+        "click div.showOther": "showOther",
+        "click div.showSubprojects": "showSubprojects",
+        "change input.popupBlockSearch": "filterSearch",
+        "keyup input.popupBlockSearch": "filterSearch",
+        "change div#productProjects input[type=checkbox]": "toggleSelect"
     },
     
     render: function(){
