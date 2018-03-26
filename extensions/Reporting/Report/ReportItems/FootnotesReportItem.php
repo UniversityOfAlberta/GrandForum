b7f08e2d9cd70e4bae66c1a655ae06f97c9ca855
David Turner <finalfantasykid13@gmail.com>
2018-03-26 09:53:16 -0600

    -Don't show footnote unless there was actually content for it

--- extensions/Reporting/Report/ReportItems/FootnotesReportItem.php
+++ extensions/Reporting/Report/ReportItems/FootnotesReportItem.php
@@ -1,24 +1,28 @@
 <?php
 
 class FootnotesReportItem extends AbstractReportItem {
-	function render(){
-            global $wgOut, $wgServer, $wgScriptPath, $config;
-            $value = $this->getBlobValue();
-            $width = (isset($this->attributes['width'])) ? $this->attributes['width'] : "100%";
-            $height = (isset($this->attributes['height'])) ? $this->attributes['height'] : "100%";
-            $value = str_replace("<", "<&lt;", $value);
-            $value = str_replace(">", "<&gt;", $value);
-            $isTopAnchor = (strtolower($this->getAttr('isTopAnchor', 'true')) == 'true');
-            $item = "
-              <a href='#' onclick='openDialog(\"{$this->getPostId()}\"); return false;' id='openfootnote{$this->getPostId()}'>
-                <img src='$wgServer$wgScriptPath/{$config->getValue('iconPath')}pen_16x16.png' />
-              </a>
-              <div id='topnote{$this->getPostId()}' class='footnote_dialog' title='Add Footnote' style='display:none'>
-                <textarea type='text' name='{$this->getPostId()}' style='width:{$width};height:{$height};'>{$value}</textarea>
-              </div>";
+    
+    static $top_anchor = 1;
+    static $bottom_anchor = 1;
 
-             $jscript =<<<EOF
-            <script type='text/javascript'>
+    function render(){
+        global $wgOut, $wgServer, $wgScriptPath, $config;
+        $value = $this->getBlobValue();
+        $width = (isset($this->attributes['width'])) ? $this->attributes['width'] : "100%";
+        $height = (isset($this->attributes['height'])) ? $this->attributes['height'] : "100%";
+        $value = str_replace("<", "<&lt;", $value);
+        $value = str_replace(">", "<&gt;", $value);
+        $isTopAnchor = (strtolower($this->getAttr('isTopAnchor', 'true')) == 'true');
+        $item = "
+          <a href='#' onclick='openDialog(\"{$this->getPostId()}\"); return false;' id='openfootnote{$this->getPostId()}'>
+            <img src='$wgServer$wgScriptPath/{$config->getValue('iconPath')}pen_16x16.png' />
+          </a>
+          <div id='topnote{$this->getPostId()}' class='footnote_dialog' title='Add Footnote' style='display:none'>
+            <textarea type='text' name='{$this->getPostId()}' style='width:{$width};height:{$height};'>{$value}</textarea>
+          </div>";
+
+         $jscript =<<<EOF
+             <script type='text/javascript'>
                 $('.footnote_dialog').dialog( "destroy" );
                 $('.footnote_dialog').dialog({
                     autoOpen: false, 
@@ -36,32 +40,32 @@ class FootnotesReportItem extends AbstractReportItem {
                 }
             </script>
 EOF;
-            $item .= $jscript;
-            $item = $this->processCData($item);
-            $wgOut->addHTML("$item");
-	}
-	
-	function renderForPDF(){
-            global $wgOut;
-            static $top_anchor = 1;
-            static $bottom_anchor = 1;
-            $value = $this->getBlobValue();
-            $value = str_replace("<", "<&lt;", $value);
-            $value = str_replace(">", "<&gt;", $value);
-            $value = nl2br($value);
+        $item .= $jscript;
+        $item = $this->processCData($item);
+        $wgOut->addHTML("$item");
+    }
+
+    function renderForPDF(){
+        global $wgOut;
+        $value = $this->getBlobValue();
+        $value = str_replace("<", "<&lt;", $value);
+        $value = str_replace(">", "<&gt;", $value);
+        $value = nl2br($value);
+        if(trim($value) != ""){
             $blob = $this->getMD5();
             $isTopAnchor = (strtolower($this->getAttr('isTopAnchor', 'true')) == 'true');
             if($isTopAnchor){ 
-                $item = "<a href='#footnote$blob' name='topnote$blob' class='anchor' id='goToFootnote$blob'>[$top_anchor]</a>";
-                $top_anchor+= 1;
+                $item = "<a href='#footnote$blob' name='topnote$blob' class='anchor' id='goToFootnote$blob'>[".self::$top_anchor."]</a>";
+                self::$top_anchor += 1;
             }
             else{
-                $item = "<a style='vertical-align:top;' href='#topnote$blob' name='footnote$blob' class='anchor' id='goToTopnote$blob'>[$bottom_anchor]</a> <div style='display:inline-block;vertical-align:top;'>{$value}</div>"; 
-                $bottom_anchor+=1;
+                $item = "<a style='vertical-align:top;' href='#topnote$blob' name='footnote$blob' class='anchor' id='goToTopnote$blob'>[".self::$bottom_anchor."]</a> <div style='display:inline-block;vertical-align:top;'>{$value}</div>"; 
+                self::$bottom_anchor += 1;
             }
-	    $item = $this->processCData($item);
+            $item = $this->processCData($item);
             $wgOut->addHTML("$item");
-	}
+        }
+    }
 }
 
 ?>
