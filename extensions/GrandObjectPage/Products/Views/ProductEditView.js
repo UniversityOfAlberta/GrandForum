48fa599d8cf991d268032fd0ddfe42de2d6a84ee
David Turner <finalfantasykid13@gmail.com>
2018-03-27 11:20:16 -0600

    -Changed the date range in products so that start/end must be in order

--- extensions/GrandObjectPage/Products/Views/ProductEditView.js
+++ extensions/GrandObjectPage/Products/Views/ProductEditView.js
@@ -1,10 +1,6 @@
 ProductEditView = Backbone.View.extend({
 
     isDialog: false,
-    projects: null,
-    allProjects: null,
-    otherProjects: null,
-    oldProjects: null,
     parent: null,
 
     initialize: function(options){
@@ -23,159 +19,49 @@ ProductEditView = Backbone.View.extend({
             this.isDialog = options.isDialog;
         }
         this.template = _.template($('#product_edit_template').html());
-        this.otherPopupTemplate = _.template($('#manage_products_other_popup_template').html());
-        this.projectsPopupTemplate = _.template($('#manage_products_projects_popup_template').html());
-        
-        this.allProjects = new Projects();
-        this.allProjects.fetch();
-        me.getProjects();
-        me.projects.ready().then($.proxy(function(){
-            this.projects = me.projects.getCurrent();
-            this.allProjects.ready().then($.proxy(function(){
-                this.otherProjects = this.allProjects.getCurrent();
-                this.oldProjects = this.allProjects.getOld();
-                this.otherProjects.remove(this.projects.models);
-                this.oldProjects.remove(this.projects.models);
-                if(!this.model.isNew() && !this.isDialog){
-                    this.model.fetch();
-                }
-                else{
-                    _.defer(this.render);
-                }
-            }, this));
-        }, this));
-        $(document).click($.proxy(function(e){
-            var popup = $("div.popupBox:visible").not(":animated").first();
-            if(popup.length > 0 && !$.contains(popup[0], e.target)){
-                this.model.trigger("change:projects");
-            }
-        }, this));
-    },
-    
-    select: function(projectId){
-        var projects = this.model.get('projects');
-        if(_.where(projects, {id: projectId}).length == 0){
-            projects.push({id: projectId});
-        }
-        // Only trigger an event if this is a parent
-        if(this.$("input[data-project=" + projectId + "]").attr('name') == 'project'){
-            this.model.trigger("change:projects");
-        }
-    },
-    
-    unselect: function(projectId){
-        var project = _.findWhere(this.parent.projects.models
-                                      .concat(this.parent.otherProjects.models)
-                                      .concat(this.parent.oldProjects.models), {id: projectId});
-        var projects = this.model.get('projects');
 
-        // Unselect all subprojects as well
-        if(project != undefined){
-            _.each(project.get('subprojects'), $.proxy(function(sub){
-                var index = _.indexOf(projects, _.findWhere(projects, {id: sub.id}));
-                if(index != -1){
-                    projects.splice(index, 1);
-                    this.$("input[data-project=" + sub.id + "]").prop('checked', false);
-                }
-            }, this));
-        }
-        projects.splice(_.indexOf(projects, _.findWhere(projects, {id: projectId})), 1);
-        // Only trigger an event if this is a parent
-        if(this.$("input[data-project=" + projectId + "]").attr('name') == 'project'){
-            this.model.trigger("change:projects");
-        }
-    },
-    
-    toggleSelect: function(e){
-        var target = $(e.currentTarget);
-        var projectId = target.attr('data-project');
-        if(target.is(":checked")){
-            // 'Check' Project
-            this.select(projectId);
-            if(target.attr('name') == "project"){
-                //this.$("div[data-project=" + projectId + "] div.subprojectPopup").slideDown();
-            }
-            else if(target.attr('name') == "subproject"){
-                var parentId = target.attr('data-parent');
-                this.$("div[data-project=" + parentId + "] div.subprojectPopup").show();
-            }
-            else if(target.attr('name') == "otherproject"){
-                $("div.otherSubProjects", target.parent()).slideDown();
-            }
+        if(!this.model.isNew() && !this.isDialog){
+            this.model.fetch();
         }
         else{
-            // 'Uncheck' Project
-            this.unselect(projectId);
-            if(target.attr('name') == "project"){
-                // Do nothing
-            }
-            else if(target.attr('name') == "subproject"){
-                var parentId = target.attr('data-parent');
-                this.$("div[data-project=" + parentId + "] div.subprojectPopup").show();
-            }
-            else if(target.attr('name') == "otherproject"){
-                $("div.otherSubProjects", target.parent()).slideUp();
-            }
+            _.defer(this.render);
         }
-    },
-    
-    filterSearch: function(e){
-        var target = $(e.currentTarget);
-        var value = target.val();
-        var block = target.parent();
-        var options = $("div.popupMainProject", block);
-        options.each(function(i, el){
-            var text = $(el).text();
-            if(unaccentChars(text).indexOf(unaccentChars(value)) == -1){
-                $(el).slideUp(150);
-            }
-            else{
-                $(el).slideDown(150);
-            }
-        });
-    },
-    
-    showOther: function(e){
-        this.$("div.otherPopup").html(this.otherPopupTemplate(this.model.toJSON()));
-        var lastHeight = this.$el.prop("scrollHeight")
-        var interval = setInterval($.proxy(function(){
-            if(this.$el.prop("scrollHeight") > lastHeight){
-                this.$el.scrollTop(this.$el.scrollTop() + Math.abs(lastHeight - this.$el.prop("scrollHeight")));
-                lastHeight = this.$el.prop("scrollHeight");
-            }
-        }, this), 16);
-        this.$("div.otherPopup").slideDown($.proxy(function(){
-            clearInterval(interval);
-            if(this.$el.prop("scrollHeight") > lastHeight){
-                this.$el.scrollTop(this.$el.scrollTop() + Math.abs(lastHeight - this.$el.prop("scrollHeight")));
-                lastHeight = this.$el.prop("scrollHeight");
-            }
-        }, this));
-    },
-    
-    showSubprojects: function(e){
-        var target = $(e.currentTarget);
-        var projectId = target.attr('data-project');
-        var project = _.findWhere(this.parent.projects.models, {id: projectId});
-        this.$("div[data-project=" + projectId + "] div.subprojectPopup").html(this.projectsPopupTemplate(_.extend(project.toJSON(), {projects: this.model.get('projects')})));
-        this.$("div[data-project=" + projectId + "] div.subprojectPopup").slideDown();
     },
     
     // Sets the end date to infinite (0000-00-00)
     setInfinite: function(){
-        this.$("input[name=date]").val('0000-00-00');
-        this.model.set('date', '0000-00-00');
+        this.$("input[name=date]").val('0000-00-00').change();
+        //this.model.set('date', '0000-00-00');
     },
     
     events: {
         "click #infinity": "setInfinite",
         "click #saveProduct": "saveProduct",
         "click #cancel": "cancel",
-        "click div.showOther": "showOther",
-        "click div.showSubprojects": "showSubprojects",
-        "change input.popupBlockSearch": "filterSearch",
-        "keyup input.popupBlockSearch": "filterSearch",
-        "change div#productProjects input[type=checkbox]": "toggleSelect"
+        "change #acceptance_date": "changeStart",
+        "change #date": "changeEnd"
+    },
+    
+    changeStart: function(){
+        var start_date = this.$("#acceptance_date").val();
+        var end_date = this.$("#date").val();
+        if(start_date != "" && start_date != "0000-00-00"){
+            if(end_date != "" && end_date != "0000-00-00"){
+                this.$("#date").datepicker("option", "minDate", start_date);
+            }
+            else{
+                this.$("#date").datepicker("option", "minDate", "");
+                this.$("#date").val(end_date).change();
+            }
+        }
+    },
+    
+    changeEnd: function(){
+        var start_date = this.$("#acceptance_date").val();
+        var end_date = this.$("#date").val();
+        if(end_date != "" && end_date != "0000-00-00"){
+            this.$("#acceptance_date").datepicker("option", "maxDate", end_date);
+        }
     },
     
     validate: function(){
@@ -345,6 +231,11 @@ ProductEditView = Backbone.View.extend({
         this.$("input[name=data_impact_factor]").prop('disabled', true);
         this.$("input[name=data_eigen_factor]").prop('disabled', true);
         this.$("input[name=data_eigen_factor]").after("<div>The IFs reported are based on the data available on July 1, " + (YEAR - 1) + "</div>");
+
+        _.defer($.proxy(function(){
+            this.$("#acceptance_date").change();
+            this.$("#date").change();
+        }, this));
         return this.$el;
     }
 
