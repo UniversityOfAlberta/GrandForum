11128db500ea156f6f4aaa8f28896ed26ae14165
David Turner <dwt@ssrg5.cs.ualberta.ca>
2018-04-03 15:23:54 -0600

    -Fixed compression issue

--- maintenance/compressSOProws.php
+++ maintenance/compressSOProws.php
@@ -3,10 +3,14 @@
     $wgUser=User::newFromId(1);
 
     $data = DBFunctions::select(array('grand_sop'),
-                                array('*'));
+                                array('id'));
     foreach($data as $row){
+        $data2 = DBFunctions::select(array('grand_sop'),
+                                     array('*'),
+                                     array('id' => $row['id']));
+        $row = $data2[0];
         if(strlen($row['pdf_contents']) > 0){
-            $deflated = gzinflate($row['pdf_contents']);
+            $deflated = gzdeflate($row['pdf_contents']);
             DBFunctions::update('grand_sop',
                                 array('pdf_contents' => $deflated),
                                 array('id' => $row['id']));
