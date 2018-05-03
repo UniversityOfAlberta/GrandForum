4de0489e199939f434c8cd22def744767a7619b7
Finalfantasykid <finalfantasykid13@gmail.com>
2018-05-03 11:44:50 -0600

    -Changes to the Report

--- extensions/Reporting/Report/ReportItems/ReportItemSets/AllProductReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/AllProductReportItemSet.php
@@ -5,26 +5,32 @@ class AllProductReportItemSet extends ReportItemSet {
     function getData(){
         $data = array();
         $category = $this->getAttr("category", "all");
+        $productType = explode("|", $this->getAttr("productType", ""));
         $start = $this->getAttr("start", REPORTING_CYCLE_START);
         $end = $this->getAttr("end", REPORTING_CYCLE_END);
         $uni = $this->getAttr("institution", "");
-	    $order = $this->getAttr("order", "date");
-	    $coll = $this->getAttr("coll", true);
+        $order = $this->getAttr("order", "date");
+        $coll = $this->getAttr("coll", true);
         $unis = array($uni);
-	if($coll == "false"){
-	    $coll = false;
-	}
-	if($uni == "All"){
-            $allPaper = Paper::getAllPapersDuring('all',$category,'all',$start,$end, false, true, $order, $coll);
-	}
-	else{
-            $allPaper = Paper::getAllPapersByInstitutionDuring($category,$start,$end,true, $unis,$order);
-	}
-        foreach($allPaper as $paper){
-            $tuple = self::createTuple();
-            $tuple['person_id'] = $paper->getId();
-            $data[] = $tuple;
+        if($coll == "false"){
+            $coll = false;
         }
+        if($uni == "All"){
+            $allPaper = Paper::getAllPapersDuring('all',$category,'all',$start,$end, false, true, $order, $coll);
+        }
+        else{
+            $allPaper = Paper::getAllPapersByInstitutionDuring($category,$start,$end,true, $unis,$order);
+        }
+
+        foreach($allPaper as $paper){
+            $type = explode(":", $paper->getType());
+            if((implode("", $productType) == "" || in_array($type[0], $productType))){
+                $tuple = self::createTuple();
+                $tuple['person_id'] = $paper->getId();
+                $data[] = $tuple;
+            }
+        }
+        
         return $data;
     }
 }
