185cbefa1c341fddee34f7192176e4b83d147754
Finalfantasykid <finalfantasykid13@gmail.com>
2018-05-31 10:51:15 -0600

    -Fixed the EOI application

--- extensions/Reporting/Report/ApplicationTabs/ApplicationTab.php
+++ extensions/Reporting/Report/ApplicationTabs/ApplicationTab.php
@@ -141,6 +141,7 @@ class ApplicationTab extends AbstractTab {
                 }
                 $first = $report;
             }
+
             if($first->hasStarted() || ($this->showAllWithPDFs && count($first->getPDF()) > 0)){
                 $pName = $person->getName();
                 if($person instanceof Theme){
