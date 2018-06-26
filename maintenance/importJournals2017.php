a928cc3757add0c053cf81139b6630a533aeb62c
David Turner <finalfantasykid13@gmail.com>
2018-06-26 14:30:36 -0600

    -Changed journal imports

--- maintenance/importJournals2017.php
+++ maintenance/importJournals2017.php
@@ -23,22 +23,20 @@
         $csv = str_getcsv($category);
         if(count($csv) <= 1) break;
         if(!isset($categories[trim($csv[0])])){
-            $categories[trim($csv[0])] = $csv;
-            $categories[trim($csv[0])]['count'] = 0;
-        }
-        else{
-            $categories[trim($csv[0])][2] += $csv[2];
+            $categories[trim($csv[0]).trim($csv[1])] = $csv;
+            $categories[trim($csv[0]).trim($csv[1])]['count'] = 0;
         }
     }
     
     foreach($connectionsCSV as $connection){
         $csv = str_getcsv($connection);
-        if(count($csv) <= 1) break;
+        if(count($csv) <= 1) continue;
         $issn = $csv[4];
         $connections[$issn][] = $csv;
     }
     
     $alreadyDone = array();
+    $toBeInserted = array();
     foreach($journalsCSV as $journal){
         $csv = str_getcsv($journal);
         if(count($csv) <= 1) break;
@@ -51,30 +49,51 @@
             $half_life = trim(ltrim($csv[4], '>'));
             $eigenfactor = $csv[5];
             foreach($connections[$issn] as $conn){
+                if(trim($conn[7]) == "Yes"){
+                    $type = "SCIE";
+                }
+                if(trim($conn[8]) == "Yes"){
+                    $type = "SSCI";
+                }
+                if(trim($conn[9]) == "Yes"){
+                    $type = "AHCI";
+                }
+                if(trim($conn[10]) == "Yes"){
+                    $type = "ESCI";
+                }
                 $description = strtoupper(trim($conn[12]));
-                if(isset($alreadyDone[$issn.$description])){
+                if(isset($alreadyDone[$issn.$description.$type]) || $type == ""){
                     continue;
                 }
                 $numer = 0;
                 $denom = 0;
-                if(isset($categories[$description])){
-                    $numer = trim(++$categories[$description]['count']);
-                    $denom = trim($categories[$description][2]);
+                if(isset($categories[$description.$type])){
+                    $numer = trim(++$categories[$description.$type]['count']);
                 }
-                DBFunctions::insert('grand_journals',
-                                array('year' => 2017,
-                                      'short_title' => $short_title,
-                                      'iso_abbrev' => $iso_abbrev,
-                                      'title' => $title,
-                                      'issn' => $issn,
-                                      'description' => $description,
-                                      'ranking_numerator' => $numer,
-                                      'ranking_denominator' => $denom,
-                                      'impact_factor' => trim($impact),
-                                      'cited_half_life' => trim($half_life),
-                                      'eigenfactor' => trim($eigenfactor)));
-                $alreadyDone[$issn.$description] = true;
+
+                $toBeInserted[$description.$type][] = 
+                    array('year' => 2017,
+                          'short_title' => $short_title,
+                          'iso_abbrev' => $iso_abbrev,
+                          'title' => $title,
+                          'issn' => $issn,
+                          'description' => $description,
+                          'ranking_numerator' => $numer,
+                          'impact_factor' => trim($impact),
+                          'cited_half_life' => trim($half_life),
+                          'eigenfactor' => trim($eigenfactor));
+                
+                $alreadyDone[$issn.$description.$type] = true;
             }
+        }
+    }
+    
+    foreach($toBeInserted as $descriptions){
+        $ranking_denominator = count($descriptions);
+        foreach($descriptions as $desc){
+            $desc['ranking_denominator'] = $ranking_denominator;
+            DBFunctions::insert('grand_journals',
+                                $desc);
         }
     }
 
