81024456c3f2519664638c92b1e38bc96af7ab7c
David Turner <finalfantasykid13@gmail.com>
2018-07-26 10:11:04 -0600

    -Changed the way the Case Number is determined

--- extensions/Reporting/Report/ReportItems/ReportItemSets/DeansPeopleReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/DeansPeopleReportItemSet.php
@@ -9,7 +9,16 @@ class DeansPeopleReportItemSet extends ReportItemSet {
         $end = $this->getAttr("end", REPORTING_CYCLE_END);
         $allPeople = Person::getAllPeople();
 
-        $fecTypes = array();
+        $data = DBFunctions::select(array('grand_personal_fec_info'),
+                                    array('user_id'),
+                                    array(),
+                                    array('date_of_appointment' => 'DESC'));
+        $fec = array();                  
+        foreach($data as $row){
+            $fec[$row['user_id']] = count($fec) + 1;
+        }
+        
+        $data = array();
         foreach($allPeople as $person){
             $found = false;
             foreach($roles as $role){
@@ -20,11 +29,10 @@ class DeansPeopleReportItemSet extends ReportItemSet {
             }
             if($found){
                 $tuple = self::createTuple();
-                
+                $index = $fec[$person->getId()];
                 $fecType = $person->getFECType();
-                @$fecTypes[$fecType]++;
                 $tuple['person_id'] = $person->getId();
-                $tuple['extra'] = $person->getFECType().str_pad($fecTypes[$fecType], 2, "0", STR_PAD_LEFT);
+                $tuple['extra'] = "<b>{$person->getFECType()}</b>".str_pad($index, 3, "0", STR_PAD_LEFT);
                 $data[] = $tuple;
             }
         }
--- extensions/Reporting/Report/ReportItems/ReportItemSets/DepartmentPeopleReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/DepartmentPeopleReportItemSet.php
@@ -10,17 +10,27 @@ class DepartmentPeopleReportItemSet extends ReportItemSet {
         $end = $this->getAttr("end", REPORTING_CYCLE_END);
         $excludeMe = (strtolower($this->getAttr("excludeMe", "false")) == "true");
         $allPeople = Person::getAllPeopleDuring(NI, $start, $end);
-        $fecTypes = array();
+        
+        $data = DBFunctions::select(array('grand_personal_fec_info'),
+                                    array('user_id'),
+                                    array(),
+                                    array('date_of_appointment' => 'DESC'));
+        $fec = array();                  
+        foreach($data as $row){
+            $fec[$row['user_id']] = count($fec) + 1;
+        }
+        
+        $data = array();
         foreach($allPeople as $person){
             if($person->isInDepartment($dept, $uni, $start, $end) && $person->getFECType() != ""){
                 if($excludeMe && $person->isMe()){
                     continue;
                 }
+                $index = $fec[$person->getId()];
                 $fecType = $person->getFECType();
                 $tuple = self::createTuple();
                 $tuple['person_id'] = $person->getId();
-                @$fecTypes[$fecType]++;
-                $tuple['extra'] = $person->getFECType().str_pad($fecTypes[$fecType], 2, "0", STR_PAD_LEFT);
+                $tuple['extra'] = "<b>{$person->getFECType()}</b>".str_pad($index, 3, "0", STR_PAD_LEFT);
                 $data[] = $tuple;
             }
         }
