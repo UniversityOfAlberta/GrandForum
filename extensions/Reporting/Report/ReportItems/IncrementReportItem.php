0d96785a93e6cd5c9b4a93d27105d3d6897a9e25
David Turner <finalfantasykid13@gmail.com>
2018-08-03 11:05:03 -0600

    -Fixed issue with course spring term

--- extensions/Reporting/Report/ReportItems/IncrementReportItem.php
+++ extensions/Reporting/Report/ReportItems/IncrementReportItem.php
@@ -69,6 +69,7 @@ class IncrementReportItem extends SelectReportItem {
 	    }
         if($increment > 0 && $maxSalary > 0){
             $exactIncrement = number_format(($maxSalary - $salary)/$increment, 2, '.', '');
+            
             if($exactIncrement > 0){
                 if(!in_array($exactIncrement, $options) && $exactIncrement < max($options)){
                     $options[] = $exactIncrement." (PTC)";
