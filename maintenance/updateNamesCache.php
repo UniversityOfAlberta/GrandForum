662738cb74c3adca2908e28d501d9f1072adc367
David Turner <finalfantasykid13@gmail.com>
2018-08-10 13:26:00 -0600

    -Delete names cache for users which don't exist anymore

--- maintenance/updateNamesCache.php
+++ maintenance/updateNamesCache.php
@@ -1,10 +1,15 @@
 <?php
     require_once('commandLine.inc');
     $wgUser = User::newFromId(1);
+    
+    DBFunctions::execSQL("DELETE FROM `grand_names_cache` WHERE user_id NOT IN (SELECT user_id FROM mw_user)", true);
+    DBFunctions::execSQL("DELETE FROM `grand_names_cache` WHERE user_id IN (SELECT user_id FROM mw_user WHERE deleted = 1)", true);
+    
     $people = Person::getAllPeople();
     $iterationsSoFar = 0;
     foreach($people as $person){
         $person->updateNamesCache();
         show_status(++$iterationsSoFar, count($people));
     }
+    
 ?>
