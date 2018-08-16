f93e129d36d0496564ddae091d6a2d42e6c44a4f
David Turner <finalfantasykid13@gmail.com>
2018-08-16 11:18:52 -0600

    -Added an extra step when deleting a publication

--- extensions/GrandObjectPage/Products/Views/ProductView.js
+++ extensions/GrandObjectPage/Products/Views/ProductView.js
@@ -1,5 +1,7 @@
 ProductView = Backbone.View.extend({
 
+    deleteDialog: null,
+
     initialize: function(){
         this.model.fetch({
             error: $.proxy(function(e){
@@ -21,28 +23,7 @@ ProductView = Backbone.View.extend({
     
     deleteProduct: function(){
         if(this.model.get('deleted') != true){
-            if(confirm("Use \"Exclude\" in Manage Outputs to disassociate this output from your record. \"Delete\" will delete it for all the co-authors. Press 'Ok' if you still want to delete it?")){
-                this.model.destroy({
-                    success: function(model, response) {
-                        if(response.deleted == true){
-                            model.set(response);
-                            clearSuccess();
-                            clearError();
-                            addSuccess('The ' + response.category + ' <i>' + response.title + '</i> was deleted sucessfully');
-                        }
-                        else{
-                            clearSuccess();
-                            clearError();
-                            addError('The ' + response.category + ' <i>' + response.title + '</i> was not deleted sucessfully');
-                        }
-                    },
-                    error: function(model, response) {
-                        clearSuccess();
-                        clearError();
-                        addError('The ' + response.category + ' <i>' + response.title + '</i> was not deleted sucessfully');
-                    }
-                });
-            }
+	        this.deleteDialog.dialog('open');
         }
         else{
             clearAllMessages();
@@ -127,6 +108,68 @@ ProductView = Backbone.View.extend({
             clearInfo();
             addInfo('This ' + this.model.get('category') + ' has been deleted, and will not show up anywhere else on the ' + siteName + '.  You may still edit the ' + this.model.get('category') + '.');
         }
+        this.deleteDialog = this.$("#deleteDialog").dialog({
+	            autoOpen: false,
+	            modal: true,
+	            show: 'fade',
+	            resizable: false,
+	            draggable: false,
+	            open: $.proxy(function(){
+	                $("html").css("overflow", "hidden");
+	                $(".ui-dialog-buttonpane button:contains('Yes')", this.deleteDialog.parent()).prop("disabled", true);
+	                $("#deleteCheck", this.deleteDialog).prop("checked", false);
+	                $("#deleteCheck", this.deleteDialog).change($.proxy(function(e){
+	                    var isChecked = $(e.currentTarget).is(":checked");
+	                    if(isChecked){
+	                        $(".ui-dialog-buttonpane button:contains('Yes')", this.deleteDialog.parent()).prop("disabled", false);
+	                    }
+	                    else{
+	                        $(".ui-dialog-buttonpane button:contains('Yes')", this.deleteDialog.parent()).prop("disabled", true);
+	                    }
+	                }, this));
+	            }, this),
+	            beforeClose: function(){
+	                $("html").css("overflow", "auto");
+	            },
+	            buttons: {
+	                "Yes": $.proxy(function(){
+	                    var model = this.model;
+	                    if(model.get('deleted') != true){
+	                        $("div.throbber", this.deleteDialog).show();
+                            model.destroy({
+                                success: $.proxy(function(model, response) {
+                                    this.deleteDialog.dialog('close');
+                                    $("div.throbber", this.deleteDialog).hide();
+                                    if(response.deleted == true){
+                                        model.set(response);
+                                        clearSuccess();
+                                        clearError();
+                                        addSuccess('The ' + response.category + ' <i>' + response.title + '</i> was deleted sucessfully');
+                                    }
+                                    else{
+                                        clearSuccess();
+                                        clearError();
+                                        addError('The ' + response.category + ' <i>' + response.title + '</i> was not deleted sucessfully');
+                                    }
+                                }, this),
+                                error: function(model, response) {
+                                    clearSuccess();
+                                    clearError();
+                                    addError('The ' + response.category + ' <i>' + response.title + '</i> was not deleted sucessfully');
+                                }
+                            });
+                        }
+                        else{
+                            this.deleteDialog.dialog('close');
+                            clearAllMessages();
+                            addError('This ' + model.get('category') + ' is already deleted');
+                        }
+	                }, this),
+	                "No": $.proxy(function(){
+	                    this.deleteDialog.dialog('close');
+	                }, this)
+	            }
+	        });
         return this.$el;
     }
 
