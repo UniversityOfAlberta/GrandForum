336065e38a571f321e1c14141b2857bfa036c68b
David Turner <finalfantasykid13@gmail.com>
2018-09-11 13:16:30 -0600

    -Fixed an issue with Google Scholar Citation importing

--- extensions/API/UpdateGoogleScholarCitationsAPI.php
+++ extensions/API/UpdateGoogleScholarCitationsAPI.php
@@ -9,16 +9,31 @@
             global $wgMessage;
             $person = Person::newFromId($_POST['id']);
 
-                //this is where google scholar url will be set and grabbed as an html
-	    $person_google_scholar = $person->getGoogleScholar();
+            //this is where google scholar url will be set and grabbed as an html
+            $person_google_scholar = $person->getGoogleScholar();
             if($person_google_scholar != ""){
-                $url = file_get_contents($person_google_scholar);
+                $url = @file_get_contents($person_google_scholar);
+                if($url === false){
+                    $wgMessage->addError("There was a problem retrieving your Google Scholar Profile");
+                    echo "<html>
+                        <head>
+                            <script type='text/javascript'>
+                                parent.ccvUploaded([], 'Updated');
+                            </script>
+                        </head>
+                    </html>";
+                    exit;
+                }
                 $gs_metric = new GsMetric(array());
                 $gs_metric->user_id = $person->getId();
                 //grabbing hindex and citation count information using regex
                 $index_regex = '/\<td class\=\"gsc\_rsb\_std\"\>(.+?)\<\/td\>/';
                 preg_match_all($index_regex, $url, $index);
                 //setting the info in gs_metric
+                if(!isset($index[1][0])){
+                    $wgMessage->addError("There was a problem retrieving your Google Scholar Profile");
+                    exit;
+                }
                 $gs_metric->citation_count = $index[1][0];
                 $gs_metric->hindex = $index[1][2];
                 $gs_metric->hindex_5_years = $index[1][3];
@@ -37,36 +52,16 @@
                 foreach($years as $year){
                     $citationArray[$year] = $counts[$i];
                     $i++;
-                    
                 }
                 //setting citation counts in array
                 $gs_metric->gs_citations = $citationArray;
                 //save to db
                 $status =$gs_metric->create();
                 $wgMessage->addSuccess("Updated Google Citations.");
-
-                echo <<<EOF
-                <html>
-                    <head>
-                        <script type='text/javascript'>
-                                            parent.ccvUploaded([], "Updated");
-                        </script>
-                    </head>
-                </html>
-EOF;
                 exit;
             }
             else{
-		$wgMessage->addError("Please update your bio with your Google Scholar information.");
-                echo <<<EOF
-                <html>
-                    <head>
-                        <script type='text/javascript'>
-                                            parent.ccvUploaded([], "Updated");
-                        </script>
-                    </head>
-                </html>
-EOF;
+                $wgMessage->addError("Please update your bio with your Google Scholar information.");
                 exit;
             }
         }
