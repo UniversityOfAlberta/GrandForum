95e3f9ba871f81d99fe87d8b20fbe9d9e1ed5f1b
David Turner <finalfantasykid13@gmail.com>
2018-09-17 14:54:19 -0600

    -Added a script to check the differences with numbers in the ccv and forum

---
+++ maintenance/testccv.php
@@ -0,0 +1,36 @@
+<?php
+
+    require_once('commandLine.inc');
+    global $wgUser;
+  
+    $wgUser = User::newFromId(1);
+    
+    if(count($argv) != 2){
+        echo "Must provide two inputs: php mergeUsers.php <username> <filename>\n";
+        exit;
+    }
+    
+    $person = Person::newFromName($argv[0]);
+    $ccv = file_get_contents($argv[1]);
+    
+    $publications = count($person->getPapers('Publication'));
+    $presentations = count($person->getPapers('Presentation'));
+    $grants = count($person->getGrants());
+    $hqp = count($person->getHQP(true));
+    
+    preg_match_all('/9a34d6b273914f18b2273e8de7c48fd6/', $ccv, $nJournals);
+    preg_match_all('/7cc778c33e64469987c55e2078be60d3/', $ccv, $nConferences);
+    preg_match_all('/fd8f2ffe3f5c43db8b5c3f72d8ffd994/', $ccv, $nBooks);
+    preg_match_all('/c7ce6f054e0941ea8b27127dbd4a26d0/', $ccv, $nPresentations);
+    preg_match_all('/aaedc5454412483d9131f7619d10279e/', $ccv, $nGrants);
+    preg_match_all('/4b36fa1eef2549f6ab3a3df7c1c81e0b/', $ccv, $nHQP);
+    
+    echo "============================\n";
+    echo "              Forum |  CCV |\n";
+    echo "============================\n";
+    printf("Publications:  %4s | %4s |\n", $publications, count($nJournals) + count($nConferences) + count($nBooks));
+    printf("Presentations: %4s | %4s |\n", $presentations, count($nPresentations));
+    printf("Grants:        %4s | %4s |\n", $grants, count($nGrants));
+    printf("HQP:           %4s | %4s |\n", $hqp, count($nHQP));
+    echo "============================\n";
+?>
