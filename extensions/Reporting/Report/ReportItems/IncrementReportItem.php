d63ca7e5571fb49b810fa6b45601bb07c6a0cca1
David Turner <finalfantasykid13@gmail.com>
2018-09-19 11:33:09 -0600

    -Fixed issue

--- extensions/Reporting/Report/ReportItems/IncrementReportItem.php
+++ extensions/Reporting/Report/ReportItems/IncrementReportItem.php
@@ -119,9 +119,9 @@ class IncrementReportItem extends SelectReportItem {
 	            });
 	        });
 	    </script>";
-	    $replacedText = '{$item}&nbsp;<a id="previousIncrementsLink'.$this->blobSubItem.'" title="Previous Increments" class="tooltip" style="cursor:pointer;display:inline-block;vertical-align:bottom;"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"/></svg></a>'.$popup;
-	    $this->value = str_replace('{$item}', $replacedText, $this->value);
-	    $this->value = str_replace('{$value}', $replacedText, $this->value);
+	    $replacedText = '&nbsp;<a id="previousIncrementsLink'.$this->blobSubItem.'" title="Previous Increments" class="tooltip" style="cursor:pointer;display:inline-block;vertical-align:bottom;"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"/></svg></a>'.$popup;
+	    $this->value = str_replace('{$item}', '{$item}'.$replacedText, $this->value);
+	    $this->value = str_replace('{$value}', '{$value}'.$replacedText, $this->value);
 	    parent::render();
 	}
 
