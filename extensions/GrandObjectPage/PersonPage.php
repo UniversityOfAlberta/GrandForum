5d831bc1fc512ec0c348f2b07c134e5b545d74fa
Finalfantasykid <finalfantasykid13@gmail.com>
2018-10-23 15:03:47 -0600

    -Added HQPDocs

--- extensions/GrandObjectPage/PersonPage.php
+++ extensions/GrandObjectPage/PersonPage.php
@@ -128,6 +128,7 @@ class PersonPage {
                     ($person->isRoleDuring(HQP, '0000-00-00 00:00:00', '2030-00-00 00:00:00') || 
                      $person->isRoleDuring(HQP."-Candidate", '0000-00-00 00:00:00', '2030-00-00 00:00:00'))){
                     $tabbedPage->addTab(new HQPEpicTab($person, $visibility));
+                    $tabbedPage->addTab(new HQPDocsTab($person, $visibility));
                 }
                 if($wgUser->isLoggedIn() && $person->isRoleDuring(HQP, '0000-00-00 00:00:00', '2030-00-00 00:00:00')){
                     $tabbedPage->addTab(new HQPExitTab($person, $visibility));
