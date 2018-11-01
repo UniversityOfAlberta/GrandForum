da2c2de869dfde32c8d43c14202643680ecdd4d9
David Turner <finalfantasykid13@gmail.com>
2018-11-01 13:13:43 -0600

    -Added IDQ processor

---
+++ maintenance/IDQ/processIDQ.php
@@ -0,0 +1,57 @@
+<?php
+
+    require_once("../commandLine.inc");
+    $wgUser = User::newFromId(1);
+    
+    $small  = array_slice(explode("\n", file_get_contents("science_".YEAR."_10to35.csv")), 17);
+    $medium = array_slice(explode("\n", file_get_contents("science_".YEAR."_36to100.csv")), 17);
+    $large  = array_slice(explode("\n", file_get_contents("science_".YEAR."_100plus.csv")), 17);
+    
+    $evals = array_merge($small, $medium, $large);
+    
+    $facultyPeople = new FacultyPeopleReportItemSet();
+    
+    $faculty = array();
+    
+    foreach($facultyPeople->getData() as $row){
+        $person = Person::newFromId($row['person_id']);
+        $person->extra = strip_tags($row['extra']);
+        $faculty[$row['person_id']] = $person;
+    }
+    
+    $dd = array();
+    $fec = array();
+    $nonfec = array();
+    
+    foreach($evals as $eval){
+        $csv = str_getcsv($eval);
+        if(count($csv) == 1 || trim($csv[6]) == "") continue;
+        $person = Person::newFromEmployeeId($csv[2]);
+        $isFEC = isset($faculty[$person->getId()]);
+        $isDD = $person->isSubRole("DD");
+        $classSize = ($csv[6] > 100) ? "large" : ($csv[6] > 36) ? "medium" : "small";
+        if($isFEC){
+            $fac = $faculty[$person->getId()];
+            if($isDD){
+                // Dean's Decision
+                $dd[] = '"'.implode('","', array($fac->extra, utf8_encode($csv[3]), $csv[5], $classSize, $csv[7], 
+                              $csv[8], $csv[9], $csv[10], $csv[11], $csv[12], $csv[13], $csv[14], $csv[15], $csv[16], $csv[17])).'"';
+            }
+            else{
+                // FEC
+                $fec[] = '"'.implode('","', array($fac->extra, utf8_encode($csv[3]), $csv[5], $classSize, $csv[7], 
+                               $csv[8], $csv[9], $csv[10], $csv[11], $csv[12], $csv[13], $csv[14], $csv[15], $csv[16], $csv[17])).'"';
+            }
+        }
+        else{
+            // Non-FEC
+            $nonfec[] = '"'.implode('","', array("", utf8_encode($csv[3]), $csv[5], $classSize, $csv[7], 
+                              $csv[8], $csv[9], $csv[10], $csv[11], $csv[12], $csv[13], $csv[14], $csv[15], $csv[16], $csv[17])).'"';
+        }
+    }
+    
+    file_put_contents("fec.csv", implode("\n", $fec));
+    file_put_contents("dd.csv", implode("\n", $dd));
+    file_put_contents("nonfec.csv", implode("\n", $nonfec));
+
+?>
