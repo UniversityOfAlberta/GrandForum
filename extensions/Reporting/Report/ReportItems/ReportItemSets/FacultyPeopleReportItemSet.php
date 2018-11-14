c0eec9fc9299ef5b6e4e35da16c205349c086492
David Turner <finalfantasykid13@gmail.com>
2018-11-14 09:55:58 -0700

    -Fixed case numbers

--- extensions/Reporting/Report/ReportItems/ReportItemSets/FacultyPeopleReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/FacultyPeopleReportItemSet.php
@@ -14,10 +14,11 @@ class FacultyPeopleReportItemSet extends ReportItemSet {
                                     array('date_of_appointment' => 'DESC'));
                                     
         $data2 = DBFunctions::select(array('grand_personal_fec_info'),
-                                    array('user_id'),
-                                    array('date_retirement' => EQ('0000-00-00 00:00:00')),
-                                    array('date_of_phd' => 'DESC',
-                                          'date_of_appointment' => 'DESC'));
+                                     array('user_id'),
+                                     array('date_retirement' => GTEQ($end),
+                                           WHERE_OR('date_retirement') => EQ('0000-00-00 00:00:00')),
+                                     array('date_of_phd' => 'DESC',
+                                           'date_of_appointment' => 'DESC'));
         
         $counts = array();
         
@@ -28,7 +29,25 @@ class FacultyPeopleReportItemSet extends ReportItemSet {
         
         $fec2 = array();
         foreach($data2 as $row){
-            $fec2[$row['user_id']] = count($fec2) + 1;
+            $person = Person::newFromId($row['user_id']);
+            if($person == null || $person->getId() == 0 || !$person->isRoleDuring(NI, $start, $end)){
+                // Check to make sure the person exists, and is an Faculty
+                continue;
+            }
+            if($person->getId() == 68 ||
+               $person->getId() == 298){
+                // Handle special cases
+                continue;
+            }
+            if($person->isRoleDuring(DEAN, $start, $end)){
+                // Don't show Deans
+                continue;
+            }
+            $fecType = $person->getFECType($end);
+            if(strstr($fecType, "C") !== false){
+                $index = @++$counts[$fecType];
+                $fec2[$row['user_id']] = $index;
+            }
         }
         
         $data = array();
@@ -42,17 +61,17 @@ class FacultyPeopleReportItemSet extends ReportItemSet {
                 // Don't show Deans
                 continue;
             }
-            if(isset($fec2[$row['user_id']]) && strstr($person->getFECType($end), "C") !== false){
+            if(isset($fec2[$person->getId()]) && strstr($person->getFECType($end), "C") !== false){
                 // Only do this for Professors right now, but this will eventually be used for everyone
                 $fecType = $person->getFECType($end);
-                $index = @++$counts[$fecType];
+                $index = @$fec2[$person->getId()];
                 $indexOld = @$fec[$person->getId()];
                 $tuple = self::createTuple();
                 $tuple['person_id'] = $person->getId();
                 $tuple['extra'] = "<b>{$person->getFECType($end)}</b>".str_pad($index, 3, "0", STR_PAD_LEFT)." <span style='color:#888888;margin-left:10px;'>(<b>{$person->getFECType($end)}</b>".str_pad($indexOld, 3, "0", STR_PAD_LEFT).")</span>";
                 $data[] = $tuple;
             }
-            else if(isset($fec[$row['user_id']]) && $person->getFECType($end) != ""){
+            else if(isset($fec[$person->getId()]) && $person->getFECType($end) != ""){
                 $index = @$fec[$person->getId()];
                 $fecType = $person->getFECType($end);
                 $tuple = self::createTuple();
