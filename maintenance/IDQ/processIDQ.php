21433cde5d58522414b8d6713a5fed3a22282a8a
David Turner <finalfantasykid13@gmail.com>
2018-11-28 09:46:15 -0700

    -Fixed IDQ

--- maintenance/IDQ/processIDQ.php
+++ maintenance/IDQ/processIDQ.php
@@ -34,12 +34,12 @@
             $fac = $faculty[$person->getId()];
             if($isDD){
                 // Dean's Decision
-                $dd[] = '"'.implode('","', array($fac->extra, utf8_encode($csv[3]), $csv[5], $csv[6], $csv[7], 
+                $dd[$fac->extra.$csv[5]] = '"'.implode('","', array($fac->extra, utf8_encode($csv[3]), $csv[5], $csv[6], $csv[7], 
                                                  $csv[8], $csv[9], $csv[10], $csv[11], $csv[12], $csv[13], $csv[14], $csv[15], $csv[16], $csv[17])).'"';
             }
             else{
                 // FEC
-                $fec[] = '"'.implode('","', array($fac->extra, utf8_encode($csv[3]), $csv[5], $csv[6], $csv[7], 
+                $fec[$fac->extra.$csv[5]] = '"'.implode('","', array($fac->extra, utf8_encode($csv[3]), $csv[5], $csv[6], $csv[7], 
                                                   $csv[8], $csv[9], $csv[10], $csv[11], $csv[12], $csv[13], $csv[14], $csv[15], $csv[16], $csv[17])).'"';
             }
         }
@@ -50,6 +50,9 @@
         }
     }
     
+    ksort($fec);
+    ksort($dd);
+
     file_put_contents("fec.csv", implode("\n", $fec));
     file_put_contents("dd.csv", implode("\n", $dd));
     file_put_contents("nonfec.csv", implode("\n", $nonfec));
