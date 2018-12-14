14a05707705bf7896e22189f24eb5a7db0cd0d3a
David Turner <finalfantasykid13@gmail.com>
2018-12-14 11:13:21 -0700

    -Added counts for merging

--- maintenance/mergeBulkUsers.php
+++ maintenance/mergeBulkUsers.php
@@ -3,7 +3,7 @@
     require_once('commandLine.inc');
     $wgUser = User::newFromId(1);
     
-    $data = DBFunctions::execSQL("SELECT user_email, COUNT(*) 
+    $data = DBFunctions::execSQL("SELECT user_email, COUNT(*) as count
                                   FROM `mw_user` 
                                   WHERE deleted = 0 
                                   AND candidate = 0 
@@ -12,8 +12,15 @@
                                   GROUP BY user_email 
                                   HAVING COUNT(*) > 1
                                   ORDER BY COUNT(*) DESC");
+    $count = count($data);
+    $sum = 0;
+    foreach($data as $row){
+        $sum += $row['count'];
+    }
+    echo "\nFound $count sets, {$sum} users\n";
     echo "\n=== Instructions ===\n";
     echo "\tType the id of the user to keep, or type 's' or 'skip' to skip the current set\n\n";
+    
     foreach($data as $row){
         echo "\nDuplicates For: {$row['user_email']}\n";
         echo "=============================================================================================================================================================\n";
