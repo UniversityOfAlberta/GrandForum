36c6fb45d902e9d89a768fce4432d06c4d1e0f34
David Turner <finalfantasykid13@gmail.com>
2018-12-17 15:12:09 -0700

    -Ignore Multimedia rows

--- extensions/API/UserGsmsBulkUploadAPI.php
+++ extensions/API/UserGsmsBulkUploadAPI.php
@@ -52,6 +52,10 @@ class UserGsmsBulkUploadAPI extends API{
             }
             //set student
             $array_info = array();
+            if($row[11] == "Multimedia"){
+                // Ignore Multimedia rows
+                continue;
+            }
             $array_info['name'] = $student_name;
             $array_info['department'] = $row[0];
             $array_info['gsms_id'] = $row[3];
