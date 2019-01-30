af51029afc6e6db26d7367a45a198f6e859bee42
David Turner <finalfantasykid13@gmail.com>
2019-01-30 09:25:01 -0700

    -Changed Undecided -> Not Submitted

--- extensions/GrandObjectPage/PersonPage/PersonSopTab.php
+++ extensions/GrandObjectPage/PersonPage/PersonSopTab.php
@@ -18,7 +18,7 @@ class PersonSopTab extends AbstractEditableTab {
     	$url = $person->getSopPdfUrl();
 
         $sop = $person->getSop();
-        $admissionStatus = ($sop != null) ? $sop->getFinalAdmit() : "Undecided";
+        $admissionStatus = ($sop != null) ? $sop->getFinalAdmit() : "Not Submitted";
         $this->html .= "<div name='container' style='display:flex;'>";
         $this->html .= "<div style='margin: 6px;padding: 0px 12px 15px 12px;'><h3>Application Decision</h3>$admissionStatus</div>";
 
