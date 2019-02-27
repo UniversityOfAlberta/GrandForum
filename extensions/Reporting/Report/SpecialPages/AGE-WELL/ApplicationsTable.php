a86213a59d8b706c7ccfa5fb26a7ed9cc35af53c
Finalfantasykid <finalfantasykid13@gmail.com>
2019-02-27 15:05:21 -0700

    -Added Summer Institute 2019

--- extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
@@ -332,6 +332,7 @@ class ApplicationsTable extends SpecialPage{
             }
         }
         $tabbedPage = new InnerTabbedPage("reports");
+        $tabbedPage->addTab(new ApplicationTab('RP_SUMMER', $summerHQPs, 2019, "2019"));
         $tabbedPage->addTab(new ApplicationTab('RP_SUMMER', $summerHQPs, 2018, "2018"));
         $tabbedPage->addTab(new ApplicationTab('RP_SUMMER', $summerHQPs, 2016, "2017"));
         $tabbedPage->addTab(new ApplicationTab('RP_SUMMER', $summerHQPs, 2015, "2016"));
