10909aed46c6df1572df573181f7e8a8031268eb
David Turner <finalfantasykid13@gmail.com>
2019-03-06 14:35:03 -0700

    -Added FEC Comments

--- extensions/Reporting/Report/ReportItems/ReportItemSets/FECReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/FECReportItemSet.php
@@ -2,12 +2,27 @@
 
 class FECReportItemSet extends ReportItemSet {
     
+    static $people = array();
+    
+    static function generateFECCache(){
+        if(count(self::$people) == 0){
+            $people = DBFunctions::select(array('grand_role_subtype'),
+                                          array('user_id'),
+                                          array('sub_role' => EQ('FEC')));
+            foreach($people as $row){
+                self::$people[] = Person::newFromId($row['user_id']);
+            }
+        }
+    }
+    
     function getData(){
-        $id = $this->getAttr("userid");
-	$data = array();
-        $tuple = self::createTuple();
-	$tuple['person_id'] = $id;
-        $data[] = $tuple;
+        $data = array();
+        self::generateFECCache();
+        foreach(self::$people as $person){
+            $tuple = self::createTuple();
+            $tuple['person_id'] = $person->getId();
+            $data[] = $tuple;
+        }
         return $data;
     }
 }
