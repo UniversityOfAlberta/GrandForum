04754cfd0b4d87aecca8c4d75a54507b4b16d845
David Turner <finalfantasykid13@gmail.com>
2019-03-07 14:22:36 -0700

    -Added a page for HQP Inconsistencies

--- extensions/Reporting/Report/ReportItems/StaticReportItems/ExpanderReportItem.php
+++ extensions/Reporting/Report/ReportItems/StaticReportItems/ExpanderReportItem.php
@@ -2,9 +2,9 @@
 
 class ExpanderReportItem extends StaticReportItem {
 
-	function render(){
-	    global $wgOut;
-	    $class = $this->getAttr('class', " ");
+    function render(){
+        global $wgOut;
+        $class = $this->getAttr('class', " ");
         $id = $this->projectId;
 
         $html =<<<EOF
@@ -12,30 +12,30 @@ class ExpanderReportItem extends StaticReportItem {
             $(document).ready(function(){ 
 
                 $('#expand-q_{$id}').click(function(e){
-                	e.preventDefault();
-                	
-                	var lbl = $('#expand-q_{$id}').text();
-                	if(lbl == "+Expand All"){
-                		$('.toggleDiv_{$id}').show(200);
-                		$('#expand-q_{$id}').text("-Shrink All");
-                	}else{
-                		$('.toggleDiv_{$id}').hide(200);
-                		$('#expand-q_{$id}').text("+Expand All");
-                	}
+                    e.preventDefault();
+
+                    var lbl = $('#expand-q_{$id}').text();
+                    if(lbl == "+Expand All"){
+                        $('.toggleDiv_{$id}').show(200);
+                        $('#expand-q_{$id}').text("-Shrink All");
+                    }else{
+                        $('.toggleDiv_{$id}').hide(200);
+                        $('#expand-q_{$id}').text("+Expand All");
+                    }
                 }); 
             })
         </script>
-	    <p><a style="color: #8C529D; font-weight:bold; font-size: 14px;" id="expand-q_{$id}" href="#">+Expand All</a></p>
+        <p><a style="color: #8C529D; font-weight:bold; font-size: 14px;" id="expand-q_{$id}" href="#">+Expand All</a></p>
 EOF;
 
         $item = $this->processCData($html);
-		$wgOut->addHTML($item);
-	}
-	
-	function renderForPDF(){
-	    $this->render();
-	}
-	
+        $wgOut->addHTML($item);
+    }
+
+    function renderForPDF(){
+        $this->render();
+    }
+
 }
 
 ?>
---
+++ extensions/Reporting/Report/ReportItems/StaticReportItems/HQPInconsistenciesReportItem.php
@@ -0,0 +1,36 @@
+<?php
+
+class HQPInconsistenciesReportItem extends StaticReportItem {
+
+    function render(){
+        global $wgOut;
+        $start = $this->getAttr('start', REPORTING_CYCLE_START);
+        $end = $this->getAttr('end', REPORTING_CYCLE_END);
+        $person = Person::newFromId($this->personId);
+        $html = "";
+        $rels = array_merge($person->getRelationsDuring(SUPERVISES, $start, $end), 
+                            $person->getRelationsDuring(CO_SUPERVISES, $start, $end));
+        foreach($rels as $rel){
+            if($rel->getType() == "Supervises"){
+                $hqp = $rel->getUser2();
+                $sups = $hqp->getSupervisors();
+                $unique = array();
+                foreach($sups as $sup){
+                    $unique[$sup->getId()] = $sup->getNameForForms();
+                }
+                if(count($unique) > 1){
+                    $html .= "<li><b>{$hqp->getNameForForms()}</b> is marked as a 'Supervises' relation, but has relations with other supervisors in the same timeframe (".implode("; ", $unique).")</li>";
+                }
+            }
+        }
+        $item = $this->processCData("<ul>$html</ul>");
+        $wgOut->addHTML($item);
+    }
+
+    function renderForPDF(){
+        global $wgOut;
+        $this->render();
+    }
+}
+
+?>
