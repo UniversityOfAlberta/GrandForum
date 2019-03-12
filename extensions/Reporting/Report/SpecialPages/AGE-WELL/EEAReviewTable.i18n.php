cf3962759174fc8ca9368adc35f5b2b7d8816c6a
Finalfantasykid <finalfantasykid13@gmail.com>
2019-03-12 14:38:14 -0600

    -Added EEAReview table

---
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/EEAReviewTable.i18n.php
@@ -0,0 +1,9 @@
+<?php
+
+$messages = array();
+$messages['en'] = array( 
+			   'EEAReviewTable' => 'EEA Review Table',
+			   'EEAreviewtable' => 'EEA Review Table',
+			   'eeareviewtable' => 'EEA Review Table',
+			   );
+?>
