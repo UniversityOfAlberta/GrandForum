b5896a79bda656c651e0121470e26f8d5723b0eb
Finalfantasykid <finalfantasykid13@gmail.com>
2019-05-06 13:13:41 -0600

    -Revert allowing PS to access the IHReport

--- extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
@@ -103,8 +103,7 @@ class Report extends AbstractReport {
             $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("CIP Application", "{$url}CIPApplication", $selected);*/
         }
         foreach($person->getProjects() as $project){
-            if ($project->getType() == 'Innovation Hub' && 
-                ($person->isRoleAtLeast(NI) || $person->isRole(PS, $project))){
+            if ($project->getType() == 'Innovation Hub' && $person->isRoleAtLeast(NI)){
                 $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "IHReport" && @$_GET['project'] == $project->getName())) ? "selected" : false;
                 $tabs["Reports"]['subtabs'][] = TabUtils::createSubTab("{$project->getName()}", "{$url}IHReport&project={$project->getName()}", $selected);
             }
