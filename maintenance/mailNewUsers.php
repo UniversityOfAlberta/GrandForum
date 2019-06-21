8a125de20e4f192bd81122166b66fd24c14a84e3
Ubuntu <ubuntu@mail.cs-can.ca>
2019-06-21 18:44:10 +0000

    -mailNewUsers update

--- maintenance/mailNewUsers.php
+++ maintenance/mailNewUsers.php
@@ -15,18 +15,31 @@ foreach($lines as $line){
         $department = trim($cells[3]);
         $university = trim($cells[4]);
         $role1 = trim($cells[5]);
-        $role2 = trim($cells[6]);
+        $role2 = isset($cells[6]) ? trim($cells[6]) : "";
         $roles = array($role1, $role2);
         
-        $person = Person::newFromEmail("$email");
-        if($person->getId() == ""){
-            $person = Person::newFromName("$first.$last");
+        // Check to see if the person is deleted first
+        $data = DBFunctions::select(array('mw_user'),
+                                    array('deleted'),
+                                    array('user_email' => "$email"));
+        if(isset($data[0]) && $data[0]['deleted'] == true){
+            // They were deleted, so skip them
+            continue;
         }
-        if($person->getId() == ""){
+        
+        $username = str_replace("'", "", 
+                    str_replace(" ", "", "$first.$last"));
+        
+        $person = Person::newFromEmail("$email");
+        if($person == null || $person->getId() == ""){
+            $person = Person::newFromName($username);
+        }
+        /*
+        if($person == null || $person->getId() == ""){
             // Create New User
-            User::createNew("$first.$last", array('real_name' => "$first $last", 
-                                                  'password' => User::crypt(mt_rand()), 
-                                                  'email' => "$email"));
+            User::createNew($username, array('real_name' => "$first $last", 
+                                             'password' => User::crypt(mt_rand()), 
+                                             'email' => "$email"));
             
             Person::$namesCache = array();
             Person::$cache = array();
@@ -42,7 +55,7 @@ foreach($lines as $line){
             Person::$disciplineMap = array();
             Person::$allPeopleCache = array();
             
-            $person = Person::newFromName("$first.$last");
+            $person = Person::newFromName($username);
         }
         $project = Project::newFromName($university);
         foreach($roles as $r){
@@ -86,57 +99,49 @@ foreach($lines as $line){
                     $leadership->doPOST();
                 }
             }
-        }
+        }*/
         
         // Now Send the Email
         $token = $person->getUser()->getToken();
         $deleteUrl = "https://forum.cscan-infocan.ca/index.php?action=deleteUser&user={$token}";
-        $message = "<p>Une version française suit ci-dessous.</p>
+        if($person->getUser()->getEmailAuthenticationTimestamp() != ""){
+            // User is activated, skip the email
+            continue;
+        }
+        $message = "<p>[ENGLISH]<br />
+You are receiving this message because you are a faculty member in a department, faculty, or school associated with CS-Can | Info-Can. 
+To facilitate future features of our mailing list (such as being able to select only the types of messages that you receive) and to provide a mechanism for individuals to vote in CS-Can | Info-Can elections, a User Account has been created for you on the CS-Can | Info-Can Forum (the back-end website for CS-Can | Info-Can). 
+Information about activating your account is on the main CS-Can | Info-Can website (<a href='https://cscan-infocan.ca/members/forum/forum-user-accounts/'>https://cscan-infocan.ca/members/forum/forum-user-accounts/</a>). 
+We hope that you will activate your account. 
+This will allow you to vote in the 2019 election when two vacancies will be filled on the Board of Directors based on voting by individual faculty members. 
+Voting will be done using the Forum. You must activate you User Account on the Forum in order to vote. 
+There is a \"practice poll\" you can fill out when you activate your account so you know how voting works. 
+You will receive email once the actual election is underway with instructions for voting. 
+If you have any questions (or if you do not wish to have a User Account) please contact Kellogg Booth (<a href='mailto:ksbooth@cs.ubc.ca'>ksbooth@cs.ubc.ca</a>) who is on the Board of Directors for CS-Can | Info-Can and is managing the process of adding accounts.</p>
+
+<p>[FRANÇAIS]<br />
+Vous recevez ce message parce que vous êtes membre du corps professoral d'un département, d'une faculté ou d'une école associé à CS-Can | Info-Can. 
+Pour faciliter les fonctionnalités futures de notre liste de diffusion (par exemple, être en mesure de sélectionner uniquement les types de messages que vous recevez) et fournir un mécanisme permettant aux personnes de voter dans CS-Can | Info-Can Elections, un compte utilisateur a été créé pour vous sur le CS-Can | Forum Info-Can (le site Web principal de CS-Can | Info-Can). 
+Des informations sur l'activation de votre compte se trouvent sur le CS-Can | Site Web Info-Can (<a href='https://cscan-infocan.ca/fr/members/forum-des-membres/comptes-dutilisateurs-du-forum/'>https://cscan-infocan.ca/fr/members/forum-des-membres/comptes-dutilisateurs-du-forum/</a>). 
+Nous espérons que vous activerez votre compte. 
+Cela vous permettra de voter lors de l'élection de 2019, lorsque deux postes vacants seront pourvus au conseil d'administration sur la base du vote de chaque membre du corps professoral. 
+Le vote se fera via le forum. 
+Vous devez activer votre compte d'utilisateur sur le forum pour pouvoir voter. 
+Il existe un \"sondage de pratique\" que vous pouvez remplir lorsque vous activez votre compte afin de savoir comment fonctionne le vote. 
+Une fois les élections en cours, vous recevrez un courrier électronique contenant les instructions de vote. 
+Si vous avez des questions (ou si vous ne souhaitez pas créer de compte utilisateur), veuillez contacter Kellogg Booth (<a href='mailto:ksbooth@cs.ubc.ca'>ksbooth@cs.ubc.ca</a>), membre du conseil d'administration de CS-Can | Info-Can et gère le processus d'ajout de comptes.</p>";			
         
-                    <hr />
+        $current_encoding = mb_detect_encoding($person->getEmail(), 'auto');
+        echo "{$person->getName()}: {$person->getEmail()}\n";
 
-                    <p>Welcome to the <a href='https://forum.cscan-infocan.ca'>CS-Can | Info-Can Forum</a>.</p>
-
-                    <p>You are receiving this email because you are one of the Beta-test users for user accounts that are being set up by CS-Can | Info-Can to allow the Canadian computer science community to have a collaboration space for shared information. The Forum is an adjunct to the main CS-Can | Info-Can website that will provide additional services such as managing subscriptions to mailing lists (you can opt out of any list at any time), voting in elections, and participating in surveys or other outreach activities.</p>
-
-                    <p>A user account has been set up for you associated with the email address to which this email has been sent.</p>
-
-                    <p>You can activate your account by going to the login page for the <a href='https://forum.cscan-infocan.ca'>Forum</a> and clicking on the Reset Password button. This will send email to the address associated with your account that will include a custom URL that will allow you to set a password for your user account. Once you have set a password, your account will be active.</p>
-
-                    <p>You can change the email address associated with your account once it has been activated. You can change the password at any time using the procedure above. You will be able to add information to your account profile such as your research areas of interest and other demographic data if you choose to do so.</p>
-
-                    <p>Additional information about the Forum is available on the CS-Can | Info-Can website.</p>
-
-                    <p><a href='https://cscan-infocan.ca/forum'>https://cscan-infocan.ca/forum</a></p>
-
-                    <p>If you have questions about any aspect of this, please send email to ksbooth@cs.ubc.ca (Kellogg Booth, UBC computer science and member of the CS-Can | Info-Can Board of Directors).</p>
-
-                    <p>Thank you.</p>
-
-                    <hr />
-
-                    <p>Bienvenue sur le <a href='https://forum.cscan-infocan.ca'>CS-Can | Forum Info-Can</a>.</p>
-
-                    <p>Vous recevez cet e-mail car vous êtes l'un des utilisateurs du test bêta des comptes d'utilisateur en cours de configuration par CS-Can | Info-Can permettra à la communauté informatique canadienne de disposer d'un espace de collaboration pour le partage d'informations. Le Forum est un complément du principal CS-Can | Le site Web d’Info-Can qui fournira des services supplémentaires tels que la gestion des abonnements aux listes de diffusion (vous pouvez vous désinscrire de toute liste à tout moment), le vote aux élections et la participation à des sondages ou à d’autres activités de sensibilisation.</p>
-
-                    <p>Un compte utilisateur a été configuré pour vous, associé à l'adresse de messagerie à laquelle cet e-mail a été envoyé.</p>
-
-                    <p>Vous pouvez activer votre compte en accédant à la page de connexion du <a href='https://forum.cscan-infocan.ca'>forum</a> et en cliquant sur le bouton Réinitialiser le mot de passe. Cela enverra un courrier électronique à l'adresse associée à votre compte, qui comprendra une URL personnalisée qui vous permettra de définir un mot de passe pour votre compte d'utilisateur. Une fois que vous avez défini un mot de passe, votre compte sera actif.</p>
-
-                    <p>Vous pouvez modifier l'adresse e-mail associée à votre compte une fois qu'il a été activé. Vous pouvez changer le mot de passe à tout moment en utilisant la procédure ci-dessus. Si vous le souhaitez, vous pourrez ajouter des informations au profil de votre compte, telles que vos domaines d’intérêt de recherche et d’autres données démographiques.</p>
-
-                    <p>Des informations supplémentaires sur le forum sont disponibles sur le site CS-Can | Site Internet Info-Can.</p>
-
-                    <p><a href='https://cscan-infocan.ca/forum/fr'>https://cscan-infocan.ca/forum/fr</a></p>
-
-                    <p>Si vous avez des questions sur l'un de ces aspects, veuillez envoyer un courrier électronique à ksbooth@cs.ubc.ca (Kellogg Booth, informaticien de UBC et membre du conseil d'administration de CS-Can | Info-Can).</p>
-
-                    <p>Je vous remercie.</p>";
+        $headers = array();
         // To send HTML mail, the Content-type header must be set
         $headers[] = 'MIME-Version: 1.0';
         $headers[] = 'Content-type: text/html; charset=utf-8';
         $headers[] = 'From: CS-CAN Forum <support@forum.cscan-infocan.ca>';
+        //mail("dwt@ualberta.ca", "CS-CAN Forum Account", $message, implode("\r\n", $headers));
         //mail($person->getEmail(), "CS-CAN Forum Account", $message, implode("\r\n", $headers));
+        //exit;
     }
 }
 
