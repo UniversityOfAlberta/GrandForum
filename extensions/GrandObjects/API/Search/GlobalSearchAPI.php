3c983396acb6b8f62e2f27ee97d79b5f39a37ead
Finalfantasykid <finalfantasykid13@gmail.com>
2019-06-26 14:59:25 -0600

    -Don't do wiki search (seems to break for Int-Comp)

--- extensions/GrandObjects/API/Search/GlobalSearchAPI.php
+++ extensions/GrandObjects/API/Search/GlobalSearchAPI.php
@@ -271,6 +271,7 @@ class GlobalSearchAPI extends RESTAPI {
                 }
                 break;
             case 'wikipage':
+                break;
                 $url = "{$wgServer}{$wgScriptPath}/api.php?action=query&generator=search&gsrwhat=title&gsrsearch=".$search."&format=json";
                 $ch = curl_init();
                 curl_setopt($ch, CURLOPT_URL, $url);
