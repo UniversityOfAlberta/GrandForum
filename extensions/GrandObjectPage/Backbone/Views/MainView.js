41aeffd545fee3cfb103691e4e58903db07719d0
David Turner <finalfantasykid13@gmail.com>
2019-07-19 10:16:00 -0600

    -Made it more clear that Awarded NSERC Grants are not used for the AR

--- extensions/GrandObjectPage/Backbone/Views/MainView.js
+++ extensions/GrandObjectPage/Backbone/Views/MainView.js
@@ -1,13 +1,38 @@
 MainView = Backbone.View.extend({
 
     initialize: function(){
-        this.model.bind('change:title', this.changeTitle);
+        this.listenTo(this.model, 'change:title', this.changeTitle);
         this.template = _.template($('#main_template').html());
     },
     
     changeTitle: function(){
-        $('#pageTitle').html(this.get('title'));
-        document.title = this.get('title');
+        $('#pageTitle').html(this.model.get('title'));
+        document.title = this.model.get('title');
+        this.$('#pageTitle .tooltip').qtip({
+            position: {
+                adjust: {
+	                x: -(this.$('#pageTitle .tooltip').width()/25),
+	                y: -(this.$('#pageTitle .tooltip').height()/2)
+                }
+            },
+            show: {
+                delay: 500
+            },
+            hide: {
+                fixed: true,
+                delay: 300
+            }
+        });
+        this.$('#pageTitle .clicktooltip').qtip({
+            position: {
+                adjust: {
+	                x: -(this.$('#pageTitle .clicktooltip').width()/25),
+	                y: -(this.$('#pageTitle .clicktooltip').height()/2)
+                }
+            },
+            show: 'click',
+            hide: 'click unfocus'
+        });
     },
     
     render: function(){ 
