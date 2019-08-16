a7ccb74746e4d1b1df53749039157b10514285c4
Finalfantasykid <finalfantasykid13@gmail.com>
2019-08-16 10:47:01 -0600

    -Added the rest of the validations to News Postings

--- extensions/GrandObjects/API/Posting/NewsPostingAPI.php
+++ extensions/GrandObjects/API/Posting/NewsPostingAPI.php
@@ -35,6 +35,24 @@ class NewsPostingAPI extends RESTAPI {
         if(strlen($this->POST('title')) > 300){
             $this->throwError("The news title must be no longer than 300 characters");
         }
+        if(strlen(trim($this->POST('summary'))) == 0){
+            $this->throwError("The summary must not be empty");
+        }
+        if(strlen($this->POST('summary')) > 2000){
+            $this->throwError("The summary must be no longer than 2000 characters");
+        }
+        if(strlen($this->POST('author')) > 70){
+            $this->throwError("The author must be no longer than 70 characters");
+        }
+        if(strlen($this->POST('sourceName')) > 70){
+            $this->throwError("The source name must be no longer than 70 characters");
+        }
+        if(trim($this->POST('sourceName')) == "" && trim($this->POST('sourceLink')) != ""){
+            $this->throwError("The source name must be not be empty when a source link is provided");
+        }
+        if(strlen($this->POST('imageCaption')) > 70){
+            $this->throwError("The image caption must be no longer than 70 characters");
+        }
         $this->checkFile();
     }
     
