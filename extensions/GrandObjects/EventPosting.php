5f50aa311cec07d1f68e44ef6faa76b0b9d766c9
Finalfantasykid <finalfantasykid13@gmail.com>
2019-08-26 11:30:05 -0600

    -Added start of EventPosting

---
+++ extensions/GrandObjects/EventPosting.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * @package GrandObjects
+ */
+
+class EventPosting extends Posting {
+    
+    static $dbTable = 'grand_event_postings';
+
+    var $address;
+    var $city;
+    var $province;
+    var $country;
+    
+    function NewsPosting($data){
+        if(count($data) > 0){
+            $row = $data[0];
+            parent::posting($data);
+            $this->address = $row['address'];
+            $this->city = $row['city'];
+            $this->province = $row['province'];
+            $this->country = $row['country'];
+        }
+    }
+    
+    function getAddress(){
+        return $this->address;
+    }
+    
+    function getCity(){
+        return $this->city;
+    }
+    
+    function getProvince(){
+        return $this->province;
+    }
+    
+    function getCountry(){
+        return $this->country;
+    }
+    
+    function toArray(){
+        $json = parent::toArray();
+        $json['address'] = $this->getAddress();
+        $json['city'] = $this->getCity();
+        $json['province'] = $this->getProvince();
+        $json['country'] = $this->getCountry();
+        return $json;
+    }
+    
+    function create(){
+        $status = parent::create();
+        if($status){
+            $status = DBFunctions::update(self::$dbTable,
+                                          array('address' => $this->address,
+                                                'city' => $this->city,
+                                                'province' => $this->province,
+                                                'country' => $this->country),
+                                          array('id' => $this->id));
+        }
+        return $status;
+    }
+    
+    function update(){
+        $status = parent::update();
+        if($status){
+            $status = DBFunctions::update(self::$dbTable,
+                                          array('address' => $this->address,
+                                                'city' => $this->city,
+                                                'province' => $this->province,
+                                                'country' => $this->country),
+                                          array('id' => $this->id));
+        }
+        return $status;
+    }
+}
+
+?>
