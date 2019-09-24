d0005874f56cd04baa9b85adc9f8865a328008a4
Finalfantasykid <finalfantasykid13@gmail.com>
2019-09-24 11:39:01 -0600

    -Dropped a couple more tables

---
+++ symfony/migrations/20190924171723_drop_tables.php
@@ -0,0 +1,36 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class DropTables extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+    */
+    
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+        $this->dropTable('grand_researcher_cv');
+        $this->dropTable('grand_review_results');
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+
+    }
+}
