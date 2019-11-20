f6124647329263524ca56a2f6f4b9bfe5c2b3ece
Finalfantasykid <finalfantasykid13@gmail.com>
2019-11-20 14:28:23 -0700

    -Changed milestone report to 2019

--- extensions/Reporting/Report/SpecialPages/GlycoNet/ApplicationsTable.php
+++ extensions/Reporting/Report/SpecialPages/GlycoNet/ApplicationsTable.php
@@ -338,6 +338,7 @@ class ApplicationsTable extends SpecialPage{
     function generateProjectMilestones(){
         global $wgOut;
         $tabbedPage = new InnerTabbedPage("reports");
+        $tabbedPage->addTab(new ApplicationTab(array('RP_MILE_REPORT'), $this->projects, 2019, "2019"));
         $tabbedPage->addTab(new ApplicationTab(array('RP_MILE_REPORT'), $this->projects, 2018, "2018"));
         $tabbedPage->addTab(new ApplicationTab(array('RP_MILE_REPORT'), $this->projects, 2017, "2017"));
         $wgOut->addHTML($tabbedPage->showPage());
