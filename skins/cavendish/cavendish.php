c38bf84cd483005d3b69b4d5b5631b81ba3e9047
Finalfantasykid <finalfantasykid13@gmail.com>
2020-02-27 15:03:07 -0700

    -Changed Single Sign On -> Login

--- skins/cavendish/cavendish.php
+++ skins/cavendish/cavendish.php
@@ -1200,7 +1200,7 @@ EOF;
                 echo "
                 <script type='text/javascript'>
                     $('.pBodyLogin').detach();
-                    $('#nav').append(\"<li style='font-weight:bold;font-size:1.25em;text-align:center;'><a id='ssoLogin' class='highlights-text highlights-background-hover' style='margin-top:62px;' href='{$config->getValue('shibLoginUrl')}'>Single Sign On</a></li>\");
+                    $('#nav').append(\"<li style='font-weight:bold;font-size:1.25em;text-align:center;'><a id='ssoLogin' class='highlights-text highlights-background-hover' style='margin-top:62px;' href='{$config->getValue('shibLoginUrl')}'>Login</a></li>\");
                 </script>
                 ";
             }
