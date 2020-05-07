3644d3e06effbd739d3d20b79cd379e79ca9afd6
Finalfantasykid <finalfantasykid13@gmail.com>
2020-05-07 11:27:31 -0600

    -Added missing toolbox header

--- skins/cavendish/cavendish.php
+++ skins/cavendish/cavendish.php
@@ -959,6 +959,7 @@ class CavendishTemplate extends QuickTemplate {
         
         $GLOBALS['toolbox']['People'] = TabUtils::createToolboxHeader("People");
         $GLOBALS['toolbox']['Products'] = TabUtils::createToolboxHeader("Outputs");
+        $GLOBALS['toolbox']['Postings'] = TabUtils::createToolboxHeader("Postings");
         $GLOBALS['toolbox']['Other'] = TabUtils::createToolboxHeader("Other");
         
 		if($wgUser->isLoggedIn()){
