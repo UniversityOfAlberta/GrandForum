8e47f2a61a721fb9ce81ce71bf47110aa13de1c1
Finalfantasykid <finalfantasykid13@gmail.com>
2020-05-13 09:43:48 -0600

    -Disabled Edge application

--- extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
@@ -48,10 +48,10 @@ class Report extends AbstractReport {
             $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "EEA")) ? "selected" : false;
             $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("Emerging Entrepreneur", "{$url}EEA", $selected);
         }*/
-        if($person->isRole(HQP) || $person->isRole(HQP.'-Candidate')){
+        /*if($person->isRole(HQP) || $person->isRole(HQP.'-Candidate')){
             $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "Edge")) ? "selected" : false;
             $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("Edge", "{$url}Edge", $selected);
-        }
+        }*/
         if($person->isRole(HQP)){
             $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "EPICConference")) ? "selected" : false;
             $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("EPIC Conference", "{$url}EPICConference", $selected);
