77e97785e2820cc265dea52d44bd2705e29bc9cb
David Turner <finalfantasykid13@gmail.com>
2020-06-18 14:36:01 -0600

    -Changed the date range for HQP publications & presentations

--- extensions/Reporting/Report/ReportItems/ReportItemSets/PersonProductsReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/PersonProductsReportItemSet.php
@@ -46,7 +46,8 @@ class PersonProductsReportItemSet extends ReportItemSet {
                         unset($products[$key]);
                     }
                 }
-                $hqps = $person->getHQPDuring($start_date, $end_date);
+                $yearAgo = strtotime("{$start_date} -2 year"); // Extend the year to 2 years ago so that publications after graduation are still counted
+                $hqps = $person->getHQPDuring($yearAgo, $end_date);
                 foreach($products as $key => $product){
                     $found = false;
                     foreach($hqps as $hqp){
