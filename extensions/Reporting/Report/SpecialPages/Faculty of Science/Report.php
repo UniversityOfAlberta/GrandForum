068fab72a534f4f064a8d43bfcdd63c7fda8843b
David Turner <finalfantasykid13@gmail.com>
2020-07-20 10:34:30 -0600

    -Enable the chair table tab

--- extensions/Reporting/Report/SpecialPages/Faculty of Science/Report.php
+++ extensions/Reporting/Report/SpecialPages/Faculty of Science/Report.php
@@ -45,11 +45,12 @@ class Report extends AbstractReport{
             $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "QACV")) ? "selected" : false;
             $tabs["CV"]['subtabs'][] = TabUtils::createSubTab("QA CV", "{$url}QACV", $selected);
         }
-        /*
+
         if($person->isRole(ISAC) || $person->isRoleDuring(ISAC, REPORTING_CYCLE_START, REPORTING_CYCLE_END) || $person->isRole(IAC) || $person->isRole(ACHAIR)){
             $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "ChairTable")) ? "selected" : false;
             $tabs["Chair"]['subtabs'][] = TabUtils::createSubTab("Annual Reports", "{$url}ChairTable", $selected);
         }
+        /*
         if($person->isRole(DEAN) || $person->isRoleDuring(DEAN, REPORTING_CYCLE_START, REPORTING_CYCLE_END) || $person->isRole(DEANEA)){
             $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "ChairTable")) ? "selected" : false;
             $tabs["Dean"]['subtabs'][] = TabUtils::createSubTab("Annual Reports", "{$url}ChairTable", $selected);
