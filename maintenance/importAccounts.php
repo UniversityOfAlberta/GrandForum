192f99b49d79d6e83c7d18431634b772c11a985a
David Turner <finalfantasykid13@gmail.com>
2020-07-23 13:06:05 -0600

    -Added account codes importing

---
+++ maintenance/importAccounts.php
@@ -0,0 +1,97 @@
+<?php
+require_once( 'commandLine.inc' );
+$wgUser = User::newFromId(1);
+
+require __DIR__ . '/../symfony/vendor/autoload.php';
+
+if (php_sapi_name() != 'cli') {
+    throw new Exception('This application must be run on the command line.');
+}
+
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Google Sheets API PHP Quickstart');
+    $client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
+    $client->setAuthConfig('credentials.json');
+    $client->setAccessType('offline');
+    $client->setPrompt('select_account consent');
+
+    // Load previously authorized token from a file, if it exists.
+    // The file token.json stores the user's access and refresh tokens, and is
+    // created automatically when the authorization flow completes for the first
+    // time.
+    $tokenPath = 'token.json';
+    if (file_exists($tokenPath)) {
+        $accessToken = json_decode(file_get_contents($tokenPath), true);
+        $client->setAccessToken($accessToken);
+    }
+
+    // If there is no previous token or it's expired.
+    if ($client->isAccessTokenExpired()) {
+        // Refresh the token if possible, else fetch a new one.
+        if ($client->getRefreshToken()) {
+            $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        } else {
+            // Request authorization from the user.
+            $authUrl = $client->createAuthUrl();
+            printf("Open the following link in your browser:\n%s\n", $authUrl);
+            print 'Enter verification code: ';
+            $authCode = trim(fgets(STDIN));
+
+            // Exchange authorization code for an access token.
+            $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+            $client->setAccessToken($accessToken);
+
+            // Check to see if there was an error.
+            if (array_key_exists('error', $accessToken)) {
+                throw new Exception(join(', ', $accessToken));
+            }
+        }
+        // Save the token to a file.
+        if (!file_exists(dirname($tokenPath))) {
+            mkdir(dirname($tokenPath), 0700, true);
+        }
+        file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+}
+
+
+// Get the API client and construct the service object.
+$client = getClient();
+$service = new Google_Service_Sheets($client);
+
+// Prints the names and majors of students in a sample spreadsheet:
+// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+$spreadsheetId = '1glH0IZ_YavOxxc4b0fthnceGKa6SYJVPJPED-Nmr0Fc';
+$range = 'Sheet1!A3:F';
+$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+$values = $response->getValues();
+
+if (empty($values)) {
+    print "No data found.\n";
+} else {
+    DBFunctions::begin();
+    DBFunctions::execSQL("TRUNCATE grand_account_codes", true);
+    foreach ($values as $row) {
+        $person = Person::newFromEmployeeId(trim($row[1]));
+        if($person != null && $person->getId() != 0){
+            DBFunctions::insert('grand_account_codes',
+                                array('user_id' => $person->getId(),
+                                      'speedcode' => @$row[2],
+                                      'combocode' => @$row[3],
+                                      'title' => @$row[4],
+                                      'project' => @$row[5]));
+            //printf("%s, %s, %s, %s, %s, %s\n", $row[0], $row[1], $row[2], $row[3], $row[4], $row[5]);
+        }
+        else {
+            echo "Not Found: {$row[0]}\n";
+        }
+    }
+    DBFunctions::commit();
+}
