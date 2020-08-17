d4e47cce2b6e42b29b267472a00e0d1ff67fe4ed
David Turner <finalfantasykid13@gmail.com>
2020-08-17 13:27:47 -0600

    -Changed styling for embeded

--- extensions/UofANews/SpecialUofANews.php
+++ extensions/UofANews/SpecialUofANews.php
@@ -24,22 +24,42 @@ class SpecialUofANews extends SpecialPage{
         $news = UofANews::getAllNews();
         if(count($news) > 0){
             foreach($news as $article){
-                $wgOut->addHTML("<div style='display: flex; border: 1px solid #EEEEEE; padding: 0 15px; margin-bottom: 15px;'>");
-                $wgOut->addHTML("<div style='width: 100%; padding-right: 20px;'><h3><a href='{$article->getUrl()}' target='_blank'>{$article->getTitle()}</a></h3>");
+                $wgOut->addHTML("<div class='newsArticle' style='display: flex; border: 1px solid #EEEEEE; padding: 0 15px; margin-bottom: 15px;'>");
+                $wgOut->addHTML("<div class='newsText' style='width: 100%; padding-right: 20px;'><h3><a href='{$article->getUrl()}' target='_blank'>{$article->getTitle()}</a></h3>");
                 $wgOut->addHTML("<p>{$article->getFirstSentences()}</p>");
                 $wgOut->addHTML("<p>Published: {$article->getDate()}</p></div>");
                 if($article->getImg() != ""){
-                    $wgOut->addHTML("<div style='width: 200px;'><img src='{$article->getImg()}' style='max-width: 200px; max-height: 200px; margin: 20px 20px 20px 0;' /></div>");
+                    $wgOut->addHTML("<div class='newsImage' style='width: 200px;'><img src='{$article->getImg()}' style='max-width: 200px; max-height: 200px; margin: 20px 20px 20px 0;' /></div>");
                 }
                 $wgOut->addHTML("</div>");
             }
         }
         if(isset($_GET['embed'])){
             $wgOut->addScript("<style> 
-                #bodyContent { 
+                #bodyContent {
+                    font-size: 13px;
                     max-height: 600px;
                     overflow-y: auto;
                     overflow-x: hidden;
+                }
+                
+                .newsArticle {
+                    display: block !important;
+                }
+                
+                .newsText {
+                    padding-right: 0 !important;
+                }
+                
+                .newsImage {
+                    width: 100% !important;
+                }
+                
+                .newsImage img {
+                    max-width: 100% !important;
+                    max-height: 100% !important;
+                    width: 100% !important;
+                    margin: 0 0 20px 0 !important;
                 }
             </style>");
         }
