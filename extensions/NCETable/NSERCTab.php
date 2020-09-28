b30921182cdda2dccffed7e7a1c15599098fc8a0
Finalfantasykid <finalfantasykid13@gmail.com>
2020-09-28 16:00:41 -0600

    -More changes for NCE tab

--- extensions/NCETable/NSERCTab.php
+++ extensions/NCETable/NSERCTab.php
@@ -19,10 +19,9 @@ class NSERCTab extends AbstractTab {
         
         $init_tabs = array("tabs_{$this->year}_Apr{$last_year}-Mar{$this_year}" => 0, 
                            "tabs_{$this->year}_Jan-Dec{$last_year}" => 1,
-		                   //"tabs_{$this->year}_Jan-Mar{$last_year}" => 2, 
-		                   //"tabs_{$this->year}_Apr-Dec{$last_year}" => 3, 
-		                   //"tabs_{$this->year}_Jan-Mar{$this_year}" => 4
-		                  );
+		                   "tabs_{$this->year}_Jan-Mar{$last_year}" => 2, 
+		                   "tabs_{$this->year}_Apr-Dec{$last_year}" => 3, 
+		                   "tabs_{$this->year}_Jan-Mar{$this_year}" => 4);
 
 		if(isset($_GET['year']) && isset($init_tabs[$_GET['year']])){
 	    	$init_tab = $init_tabs[$_GET['year']];
@@ -34,11 +33,11 @@ class NSERCTab extends AbstractTab {
 		$int_end =   $this_year.NCE_END_MONTH. ' 23:59:59';
 		$tabbedPage->addTab(new NSERCVariableTab("Apr{$last_year}-Mar{$this_year}", $int_start, $int_end, $this->year));
         
+        /*
         $int_start = $last_year.CYCLE_START_MONTH.' 00:00:00';
 		$int_end =   ($this_year-1).CYCLE_END_MONTH_ACTUAL. ' 23:59:59';
 		$tabbedPage->addTab(new NSERCVariableTab("Jan-Dec{$last_year}", $int_start, $int_end, $this->year));
         
-        /*
     	$int_start = $last_year.CYCLE_START_MONTH.' 00:00:00';
 		$int_end =   $last_year.NCE_END_MONTH. ' 23:59:59';
 		$tabbedPage->addTab(new NSERCVariableTab("Jan-Mar{$last_year}", $int_start, $int_end, $this->year));
