e094580df6b6634e59a8d32ba4d55ea6e830ae4f
Finalfantasykid <finalfantasykid13@gmail.com>
2020-12-16 15:27:01 -0700

    -Added deleted text to table deleting

--- extensions/GrandObjectPage/BSIPosting/Views/BSIPostingsView.js
+++ extensions/GrandObjectPage/BSIPosting/Views/BSIPostingsView.js
@@ -1,5 +1,63 @@
 BSIPostingsView = PostingsView.extend({
 
-    template: _.template($('#bsipostings_template').html())
+    template: _.template($('#bsipostings_template').html()),
+    
+    delete: function(e) {
+        var deleteDialog = this.$("#deleteDialog" + e.target.id).dialog({
+            autoOpen: false,
+	        modal: true,
+            buttons: {
+	            "Delete": function(){
+	                var model = this.model.get(e.target.id);
+	                if(model.get('deleted') != true){
+	                    $("div.throbber", deleteDialog).show();
+	                    model.save(null, {
+	                        success: function(){
+                                model.destroy({
+                                    success: function(model, response) {
+                                        deleteDialog.dialog('close');
+                                        $("div.throbber", deleteDialog).hide();
+                                        if(response.deleted == true){
+                                            model.set(response);
+                                            clearSuccess();
+                                            clearError();
+                                            addSuccess('The Posting was deleted sucessfully');
+                                        }
+                                        else{
+                                            clearSuccess();
+                                            clearError();
+                                            addError('The Posting was not deleted sucessfully');
+                                        }
+                                    }.bind(this),
+                                    error: function(model, response) {
+                                        deleteDialog.dialog('close');
+                                        clearSuccess();
+                                        clearError();
+                                        addError('The Posting was not deleted sucessfully');
+                                    }.bind(this)
+                                });
+                            }.bind(this),
+                            error: function(){
+                                deleteDialog.dialog('close');
+                                clearSuccess();
+                                clearError();
+                                addError('The Posting was not deleted sucessfully');
+                            }.bind(this)
+                        });
+                    }
+                    else{
+                        deleteDialog.dialog('close');
+                        clearAllMessages();
+                        addError('This Posting is already deleted');
+                    }
+	            }.bind(this),
+	            "Cancel": function(){
+	                deleteDialog.dialog('close');
+	            }.bind(this)
+	        }
+        });
+        deleteDialog.parent().appendTo(this.$el);
+        deleteDialog.dialog('open');
+    }
 
 });
