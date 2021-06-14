45f29da86e9e5fcdd25e89170f9e0d898e3b2518
Finalfantasykid <finalfantasykid13@gmail.com>
2021-06-14 10:05:21 -0600

    -Changed the appearance of the CalendarFields

--- extensions/UI/Elements/CalendarField.php
+++ extensions/UI/Elements/CalendarField.php
@@ -4,16 +4,21 @@ class CalendarField extends UIElement {
     
     function CalendarField($id, $name, $value, $validations=VALIDATE_NOTHING){
         parent::UIElement($id, $name, $value, $validations);
-        $this->attr('size', 12);
+        $this->attr('size', 8);
     }
     
     function render(){
+        global $wgServer, $wgScriptPath;
         return "<input type='text' {$this->renderAttr()} name='{$this->id}' value='{$this->value}' />
                 <script type='text/javascript'>
 		            $('input[name={$this->id}]').datepicker(
-		                {dateFormat: 'yy-mm-dd',
+		                {
+		                 dateFormat: 'yy-mm-dd',
 		                 changeMonth: true,
-                         changeYear: true
+                         changeYear: true,
+                         showOn: 'both',
+                         buttonImage: '{$wgServer}{$wgScriptPath}/skins/calendar.gif',
+                         buttonImageOnly: true
 		                });
                     $('input[name={$this->id}]').keydown(function(){
                         return false;
