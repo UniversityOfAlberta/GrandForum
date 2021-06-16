e908bfff507bf9906b0d2762ba114a0a1c47e31d
Finalfantasykid <finalfantasykid13@gmail.com>
2021-06-16 12:25:04 -0600

    -Fixed issue with event registation

--- extensions/EventRegistration/SpecialEventRegistration.php
+++ extensions/EventRegistration/SpecialEventRegistration.php
@@ -96,7 +96,7 @@ class SpecialEventRegistration extends SpecialPage{
         $roles = array("Audience", "Presenter", "Host");
         $misc = "";
         if($default->title == "Energy Hackathon 2021 - APIC"){
-            $miscField = new TextareaField("misc['Programming']", "misc", "");
+            $miscField = new TextareaField("misc[Programming]", "misc", "");
             $misc = "<h3>Which programming technologies or tools are you familiar with or would like to learn?</h3>
                      {$miscField->render()}";
             $roles = array("Audience", "Host");
