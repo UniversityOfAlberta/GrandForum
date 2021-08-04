acd29886a4d4feaba6acaca7dcb688aced986b2d
Finalfantasykid <finalfantasykid13@gmail.com>
2021-08-04 15:41:20 -0600

    -Added project application to candidate profile

--- extensions/Reporting/Report/ReportItems/ReportItemSets/ElitePostingsReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/ElitePostingsReportItemSet.php
@@ -7,9 +7,11 @@ class ElitePostingsReportItemSet extends ReportItemSet {
         $postings = ElitePosting::getAllPostings();
         if(is_array($postings)){
             foreach($postings as $posting){
-                $tuple = self::createTuple();
-                $tuple['product_id'] = $posting->getId();
-                $data[] = $tuple;
+                if($posting->visibility == "Accepted"){
+                    $tuple = self::createTuple();
+                    $tuple['product_id'] = $posting->getId();
+                    $data[] = $tuple;
+                }
             }
         }
         return $data;
