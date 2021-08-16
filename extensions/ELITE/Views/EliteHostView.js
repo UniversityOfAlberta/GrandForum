91af2fcef9c687200b0cd637b8506d3a390f1252
Finalfantasykid <finalfantasykid13@gmail.com>
2021-08-16 10:34:51 -0600

    -Added clone

--- extensions/ELITE/Views/EliteHostView.js
+++ extensions/ELITE/Views/EliteHostView.js
@@ -36,6 +36,33 @@ EliteHostPostingsView = PostingsView.extend({
         this.listenTo(this.model, "remove", this.render);
     },
     
+    clone: function(el){
+        var target = el.currentTarget;
+        var id = $(target).attr("id");
+        var copy = new ElitePosting(this.model.get(id).toJSON());
+        copy.set('id', null);
+        copy.set('visibility', 'Submitted');
+        copy.set('comments', "");
+        copy.save(null, {
+            success: function(){
+                clearSuccess();
+                clearError();
+                addSuccess('The project <i>' + copy.get('title') + '</i> was duplicated');
+                this.model.fetch();
+            }.bind(this),
+            error: function(){
+                clearSuccess();
+                clearError();
+                addError('There was a problem duplicating the project <i>' + copy.get('title') + '</i>');
+            }.bind(this)
+        });
+    },
+    
+    events:  {
+        "click .copy-icon": "clone",
+        "click .delete-icon": "delete",
+    },
+    
     render: function(){
         this.$el.html(this.template(this.model.toJSON()));
         this.$("table#postings").DataTable({
