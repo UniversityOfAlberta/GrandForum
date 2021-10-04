14fdccc57f2e07e33505c81b903554fe1086e1a6
Finalfantasykid <finalfantasykid13@gmail.com>
2021-10-04 11:57:53 -0600

    -Fixed issue with project selector

--- extensions/Reporting/Report/ReportItems/EliteProjectSelectorReportItem.php
+++ extensions/Reporting/Report/ReportItems/EliteProjectSelectorReportItem.php
@@ -9,114 +9,121 @@ class EliteProjectSelectorReportItem extends CheckboxReportItem {
 		$limit = $this->getAttr("limit", 5);
         $set = new ElitePostingsReportItemSet();
         $data = $set->getData();
-        $output = "<style>
-                        #projects #selected_projects li:hover {
-                            cursor: move;
-                            background: #eeeeee;
-                        }
-                        
-                        #projects .listcontainer {
-                            max-height: 300px;
-                            overflow-y: auto; 
-                            margin: 0 0 0.4em 0;
-                        }
-                    </style>
-                    <div id='projects' style='display:flex;border: 1px solid #dfdfdf;'>
-                    <div style='width:50%;padding:0 10px;border-right: 1px solid #dfdfdf;'>
-                        <h3 style='margin-top: 0;'>Selected Projects</h3>
-                        <div class='listcontainer'>
-                            <ol id='selected_projects'>
-                                
-                            </ol>
-                        </div>
-                    </div>
-                    <div style='width:50%;padding:0 10px;'>
-                        <h3 style='margin-top: 0;'>Available Projects</h3>
-                        <input type='hidden' name='{$this->getPostId()}[]' value='' />
-                        <div class='listcontainer'>
-                            <ul id='available_projects' >";
-        foreach(@$blob as $row){
-            $posting = ElitePosting::newFromId($row);
-            $output .= "<li>
-                            <input type='checkbox' name='{$this->getPostId()}[]' value='{$posting->getId()}' checked />
-                            <a id='{$posting->getId()}' class='elite_link' style='cursor:pointer;'>{$posting->getExtra('companyName')} - {$posting->getTitle()}</a>
-                        </li>";
+        if(count($data) == 0){
+            $output = "There are currently no available projects";
         }
-        
-        foreach($data as $row){
-            $posting = ElitePosting::newFromId($row['product_id']);
-            if(@in_array($posting->getId(), $blob)){
-                continue;
+        else{
+            $output = "<style>
+                            #projects #selected_projects li:hover {
+                                cursor: move;
+                                background: #eeeeee;
+                            }
+                            
+                            #projects .listcontainer {
+                                max-height: 300px;
+                                overflow-y: auto; 
+                                margin: 0 0 0.4em 0;
+                            }
+                        </style>
+                        <div id='projects' style='display:flex;border: 1px solid #dfdfdf;'>
+                        <div style='width:50%;padding:0 10px;border-right: 1px solid #dfdfdf;'>
+                            <h3 style='margin-top: 0;'>Selected Projects</h3>
+                            <div class='listcontainer'>
+                                <ol id='selected_projects'>
+                                    
+                                </ol>
+                            </div>
+                        </div>
+                        <div style='width:50%;padding:0 10px;'>
+                            <h3 style='margin-top: 0;'>Available Projects</h3>
+                            <input type='hidden' name='{$this->getPostId()}[]' value='' />
+                            <div class='listcontainer'>
+                                <ul id='available_projects' >";
+            if(is_array($blob)){
+                foreach(@$blob as $row){
+                    $posting = ElitePosting::newFromId($row);
+                    $output .= "<li>
+                                    <input type='checkbox' name='{$this->getPostId()}[]' value='{$posting->getId()}' checked />
+                                    <a id='{$posting->getId()}' class='elite_link' style='cursor:pointer;'>{$posting->getExtra('companyName')} - {$posting->getTitle()}</a>
+                                </li>";
+                }
             }
             
-            $output .= "<li>
-                            <input type='checkbox' name='{$this->getPostId()}[]' value='{$posting->getId()}' />
-                            <a id='{$posting->getId()}' class='elite_link' style='cursor:pointer;'>{$posting->getExtra('companyName')} - {$posting->getTitle()}</a>
-                        </li>";
-        }
-        $output .= "</ul></div>
-                </div></div>
-                <div id='postingDialog' title='Project Proposal' style='display:none;'></div>
-                <script type='text/javascript'>
-                    $('#projects #selected_projects').sortable({
-                        stop: function(){
-                            saveAll();
-                        }
-                    });
-                    
-                    function updateProjects(){
-                        $('#projects input').each(function(i, el){
-                            var checked = $(el).is(':checked');
-                            if(checked){
-                                var row = $(el).closest('li').detach();
-                                $('#projects #selected_projects').append(row);
-                            }
-                            else{
-                                var row = $(el).closest('li').detach();
-                                $('#projects #available_projects').append(row);
-                            }
-                        });
-                        if($('#projects #selected_projects li').length >= $limit){
-                            $('#projects #available_projects input').prop('disabled', true);
-                        }
-                        else {
-                            $('#projects #available_projects input').prop('disabled', false);
-                        }
-                    }
-                    
-                    $('#projects input').change(updateProjects);
-                    
-                    updateProjects();
+            foreach($data as $row){
+                $posting = ElitePosting::newFromId($row['product_id']);
+                if(@in_array($posting->getId(), $blob)){
+                    continue;
+                }
                 
-                    var postingDialog = $('#postingDialog').dialog({
-                        autoOpen: false,
-                        modal: true,
-                        show: 'fade',
-                        resizable: false,
-                        draggable: false,
-                        beforeClose: function(){
-                            postingDialog.view.stopListening();
-                            postingDialog.view.undelegateEvents();
-                            postingDialog.view.\$el.empty();
-                        }.bind(this)
-                    });
-                    
-                    $(window).resize(function(){
-                        postingDialog.dialog({height: $(window).height()*0.75});
-                    }.bind(this));
-                    
-                    $('.elite_link').click(function(e){
-                        var id = $(e.target).attr('id');
-                        var model = new ElitePosting({id: id});
-                        var view = new ElitePostingView({el: postingDialog, model: model, isDialog: true});
-                        postingDialog.view = view;
-                        postingDialog.dialog({
-                            height: $(window).height()*0.75, 
-                            width: 800
+                $output .= "<li>
+                                <input type='checkbox' name='{$this->getPostId()}[]' value='{$posting->getId()}' />
+                                <a id='{$posting->getId()}' class='elite_link' style='cursor:pointer;'>{$posting->getExtra('companyName')} - {$posting->getTitle()}</a>
+                            </li>";
+            }
+            $output .= "</ul></div>
+                    </div></div>
+                    <div id='postingDialog' title='Project Proposal' style='display:none;'></div>
+                    <script type='text/javascript'>
+                        $('#projects #selected_projects').sortable({
+                            stop: function(){
+                                saveAll();
+                            }
                         });
-                        postingDialog.dialog('open');
-                    });
-                </script>";
+                        
+                        function updateProjects(){
+                            $('#projects input').each(function(i, el){
+                                var checked = $(el).is(':checked');
+                                if(checked){
+                                    var row = $(el).closest('li').detach();
+                                    $('#projects #selected_projects').append(row);
+                                }
+                                else{
+                                    var row = $(el).closest('li').detach();
+                                    $('#projects #available_projects').append(row);
+                                }
+                            });
+                            if($('#projects #selected_projects li').length >= $limit){
+                                $('#projects #available_projects input').prop('disabled', true);
+                            }
+                            else {
+                                $('#projects #available_projects input').prop('disabled', false);
+                            }
+                        }
+                        
+                        $('#projects input').change(updateProjects);
+                        
+                        updateProjects();
+                    
+                        var postingDialog = $('#postingDialog').dialog({
+                            autoOpen: false,
+                            modal: true,
+                            show: 'fade',
+                            resizable: false,
+                            draggable: false,
+                            beforeClose: function(){
+                                postingDialog.view.stopListening();
+                                postingDialog.view.undelegateEvents();
+                                postingDialog.view.\$el.empty();
+                            }.bind(this)
+                        });
+                        
+                        $(window).resize(function(){
+                            postingDialog.dialog({height: $(window).height()*0.75});
+                        }.bind(this));
+                        
+                        $('.elite_link').click(function(e){
+                            var id = $(e.target).attr('id');
+                            var model = new ElitePosting({id: id});
+                            var view = new ElitePostingView({el: postingDialog, model: model, isDialog: true});
+                            postingDialog.view = view;
+                            postingDialog.dialog({
+                                height: $(window).height()*0.75, 
+                                width: 800
+                            });
+                            postingDialog.dialog('open');
+                        });
+                    </script>";
+        }
         $output = $this->processCData("<div>{$output}</div>");
 		$wgOut->addHTML($output);
 	}
