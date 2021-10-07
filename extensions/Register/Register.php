167a116776325e7c39e7e6177fd108338a27b3c7
Finalfantasykid <finalfantasykid13@gmail.com>
2021-10-07 09:52:34 -0600

    -Revert

--- extensions/Register/Register.php
+++ extensions/Register/Register.php
@@ -176,10 +176,6 @@ class Register extends SpecialPage{
             $_POST['wpSendMail'] = "true";
             $_POST['candidate'] = "1";
             
-            if($config->getValue('networkName') == "ELITE"){
-                $_POST['candidate'] = "0";
-            }
-            
             $splitEmail = explode("@", $_POST['wpEmail']);
             $domain = @$splitEmail[1];
             
