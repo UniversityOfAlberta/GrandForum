dc3cd063da7d02262d21d7756c61da380d9e3a1d
Ubuntu <finalfantasykid13@gmail.com>
2021-12-23 14:46:20 -0700

    -Added some AVOID placeholders

---
+++ extensions/Reporting/Report/SpecialPages/AVOID/DummyReport.php
@@ -0,0 +1,38 @@
+<?php
+
+class DummyReport extends AbstractReport{
+    
+    function __construct($reportType, $person, $project=null, $year=REPORTING_YEAR, $quick=false){
+        global $config;
+        if(is_numeric($reportType) || ReportXMLParser::findReport($reportType) != ""){
+            $fileName = ReportXMLParser::findReport($reportType);
+        }
+        else{
+            $fileName = dirname(__FILE__)."/../../ReportXML/{$config->getValue('networkName')}/$reportType.xml";
+        }
+  
+        $this->readOnly = true;
+        $projectName = null;
+        if($project != null){
+            if($project instanceof Project){
+                if($project->getName() == ""){
+                    $projectName = $project->getId();
+                }
+                else{
+                    $projectName = $project->getName();
+                }
+            }
+            else if($project instanceof Theme){
+                $projectName = $project->getAcronym();
+            }
+        }
+        $topProjectOnly = false;
+        /*if($projectName != null){
+            $topProjectOnly = true;
+        }*/
+        $this->AbstractReport($fileName, $person->getId(), $projectName, $topProjectOnly, $year, $quick);
+    }
+    
+}
+
+?>
---
+++ extensions/Reporting/Report/SpecialPages/AVOID/Report.php
@@ -0,0 +1,44 @@
+<?php
+
+$dir = dirname(__FILE__) . '/';
+$wgSpecialPages['Report'] = 'Report'; # Let MediaWiki know about the special page.
+$wgExtensionMessagesFiles['Report'] = $dir . 'Report.i18n.php';
+$wgSpecialPageGroups['Report'] = 'reporting-tools';
+
+$wgHooks['TopLevelTabs'][] = 'Report::createTab';
+$wgHooks['SubLevelTabs'][] = 'Report::createSubTabs';
+$wgHooks['ToolboxLinks'][] = 'Report::createToolboxLinks';
+
+class Report extends AbstractReport{
+    
+    function __construct(){
+        global $config;
+        $report = @$_GET['report'];
+        parent::__construct(dirname(__FILE__)."/../../ReportXML/{$config->getValue('networkName')}/$report.xml", -1, false, false);
+    }
+
+    static function createTab(&$tabs){
+        global $wgServer, $wgScriptPath, $wgUser, $wgTitle, $special_evals;
+        //$tabs["Reports"] = TabUtils::createTab("My Reports");
+        return true;
+    }
+    
+    static function createSubTabs(&$tabs){
+        global $wgServer, $wgScriptPath, $wgUser, $wgTitle, $special_evals;
+        $person = Person::newFromWgUser();
+        $url = "$wgServer$wgScriptPath/index.php/Special:Report?report=";
+        /*if(){
+            $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "FocusResults")) ? "selected" : false;
+            $tabs["Surveys"]['subtabs'][] = TabUtils::createSubTab("FOCUS Results", "{$url}FocusResults", $selected);
+        }*/
+        return true;
+    }
+    
+    static function createToolboxLinks(&$toolbox){
+        global $wgServer, $wgScriptPath, $config;
+
+        return true;
+    }
+}
+
+?>
