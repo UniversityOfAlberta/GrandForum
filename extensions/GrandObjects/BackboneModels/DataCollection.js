2c9720ea6d83036c2ceb6dd284e8de7fdadc5fa0
David Turner <finalfantasykid13@gmail.com>
2022-01-14 09:42:08 -0700

    -Added some video helper functions

--- extensions/GrandObjects/BackboneModels/DataCollection.js
+++ extensions/GrandObjects/BackboneModels/DataCollection.js
@@ -1,10 +1,12 @@
 DataCollection = Backbone.Model.extend({
 
     initialize: function(){
-        this.fetch();
-        this.on('change:data', _.throttle(function(){
-            this.save();
-        }.bind(this), 3000));
+        var xhr = this.fetch();
+        $.when(xhr).always(function(){
+            this.on('change:data', _.throttle(function(){
+                this.save();
+            }.bind(this), 3000));
+        }.bind(this));
     },
 
     url: function(){
@@ -17,8 +19,11 @@ DataCollection = Backbone.Model.extend({
         return "index.php?action=api.datacollection";
     },
     
-    getField: function(field){
+    getField: function(field, def){
         var data = this.get('data');
+        if(data[field] == undefined && def != undefined){
+            data[field] = def;
+        }
         return data[field];
     },
     
@@ -29,13 +34,21 @@ DataCollection = Backbone.Model.extend({
         this.trigger("change:data");
     },
     
+    // Increments a value, useful for keeping track of counters
     increment: function(field){
-        var fieldValue = this.getField(field);
-        if(fieldValue == undefined){
-            fieldValue = 0;
-        }
+        var fieldValue = this.getField(field, 0);
         fieldValue++;
         this.setField(field, fieldValue);
+    },
+    
+    // Initializes an array which can be used for tracking how much of a video has been watched
+    video: function(field, duration){
+        return this.getField(field, _.omit([].fill.call({length: Math.ceil(duration)}, 0), 'length'));
+    },
+    
+    // Used to sum one of the fields (if it is an array)
+    sum: function(field){
+        return _.reduce(this.video(field, 1), function(memo, num){ return memo + num; });
     },
 
     defaults: function() {
