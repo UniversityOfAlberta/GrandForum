324c1b2179085e9661dccab47ed0f0617382b602
David Turner <finalfantasykid13@gmail.com>
2022-02-07 11:27:28 -0700

    -Improved NextPrev

--- extensions/Reporting/Report/ReportSections/NextPrevReportSection.php
+++ extensions/Reporting/Report/ReportSections/NextPrevReportSection.php
@@ -10,17 +10,30 @@ class NextPrevReportSection extends EULAReportSection {
             $disabled = "disabled='disabled'";
         }
         $wgOut->addHTML("<script type='text/javascript'>
+            $('#reportBody').after(\"<div id='reportMessages'></div>\");
             $('input[name=submit][value=Next]').before(\"<input type='submit' value='Previous' name='submit' style='width:100px;' $disabled />&nbsp;&nbsp;\");
             $('input[name=submit][value=Previous]').click(function(){
                 _.defer(function(){
                     $('a.reportTab.selectedReportTab').prev().click();
                 });
             });
-            if($('a.reportTab.selectedReportTab').prev().length == 0){
+            if($('a.reportTab.selectedReportTab').prev('a').length == 0){
                 $('input[name=submit][value=Previous]').prop('disabled', true);
             }
-            if($('a.reportTab.selectedReportTab').next().length == 0){
-                $('input[name=submit][value=Next]').prop('disabled', true);
+            if($('a.reportTab.selectedReportTab').next('a').length == 0){
+                $('input[name=submit][value=Next]').val('Submit');
+                $('input[name=submit][value=Submit]').click(function(){
+                    if(typeof submitInterval != 'undefined'){
+                        clearInterval(submitInterval);
+                    }
+                    submitInterval = setInterval(function(){
+                        if(!$('#submit_throbber').is(':visible')){
+                            clearSuccess('#reportMessages');
+                            addSuccess('Thank-you for submitting', false, '#reportMessages');
+                            clearInterval(submitInterval);
+                        }
+                    }, 100);
+                });
             }
         </script>");
     }
