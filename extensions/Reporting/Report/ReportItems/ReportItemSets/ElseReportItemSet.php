dad2fdbd810e317e4b4effc869cc7c3bd650d6c5
Finalfantasykid <finalfantasykid13@gmail.com>
2022-05-02 13:12:53 -0600

    -Fixed issue with else/if report items

--- extensions/Reporting/Report/ReportItems/ReportItemSets/ElseReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/ElseReportItemSet.php
@@ -8,6 +8,9 @@ class ElseReportItemSet extends IfReportItemSet {
         while($prev instanceof IfReportItemSet || $prev instanceof IfReportItem){
             $cond = $prev->getAttr("if", '');
             $ret = $ret && !($cond == "1");
+            if(get_class($prev) == "IfReportItemSet" || get_class($prev) == "IfReportItem"){
+                break;
+            }
             $prev = $prev->getPrev();
         }
         return $ret;
