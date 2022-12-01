91b7272a6fa867bef5e85e0346f8df1ef00a20a1
David Turner <finalfantasykid13@gmail.com>
2022-12-01 10:32:24 -0700

    -Removed p-value

--- extensions/Reporting/Report/SpecialPages/AVOID/Descriptors.php
+++ extensions/Reporting/Report/SpecialPages/AVOID/Descriptors.php
@@ -75,7 +75,6 @@ class Descriptors extends SpecialPage {
                     <th>Dimension</th>
                     <th>Baseline<br />n (%)</th>
                     <th>Follow-up<br />n (%)</th>
-                    <th>P Value</th>
                 </tr>
             </thead>
             <tbody>
@@ -84,31 +83,26 @@ class Descriptors extends SpecialPage {
                     <td>No problems</td>
                     <td>{$mobility[1]} (".number_format($mobility[1]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$mobility6[1]} (".number_format($mobility6[1]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Slight problems</td>
                     <td>{$mobility[2]} (".number_format($mobility[2]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$mobility6[2]} (".number_format($mobility6[2]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Moderate problems</td>
                     <td>{$mobility[3]} (".number_format($mobility[3]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$mobility6[3]} (".number_format($mobility6[3]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Severe problems</td>
                     <td>{$mobility[4]} (".number_format($mobility[4]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$mobility6[4]} (".number_format($mobility6[4]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Unable to walk about</td>
                     <td>{$mobility[5]} (".number_format($mobility[5]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$mobility6[5]} (".number_format($mobility6[5]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 
                 
@@ -117,31 +111,26 @@ class Descriptors extends SpecialPage {
                     <td>No problems</td>
                     <td>{$selfcare[1]} (".number_format($selfcare[1]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$selfcare6[1]} (".number_format($selfcare6[1]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Slight problems</td>
                     <td>{$selfcare[2]} (".number_format($selfcare[2]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$selfcare6[2]} (".number_format($selfcare6[2]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Moderate problems</td>
                     <td>{$selfcare[3]} (".number_format($selfcare[3]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$selfcare6[3]} (".number_format($selfcare6[3]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Severe problems</td>
                     <td>{$selfcare[4]} (".number_format($selfcare[4]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$selfcare6[4]} (".number_format($selfcare6[4]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Unable to wash or dress</td>
                     <td>{$selfcare[5]} (".number_format($selfcare[5]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$selfcare6[5]} (".number_format($selfcare6[5]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 
                 
@@ -150,31 +139,26 @@ class Descriptors extends SpecialPage {
                     <td>No problems</td>
                     <td>{$activities[1]} (".number_format($activities[1]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$activities6[1]} (".number_format($activities6[1]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Slight problems</td>
                     <td>{$activities[2]} (".number_format($activities[2]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$activities6[2]} (".number_format($activities6[2]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Moderate problems</td>
                     <td>{$activities[3]} (".number_format($activities[3]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$activities6[3]} (".number_format($activities6[3]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Severe problems</td>
                     <td>{$activities[4]} (".number_format($activities[4]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$activities6[4]} (".number_format($activities6[4]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Unable to do usual activities</td>
                     <td>{$activities[5]} (".number_format($activities[5]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$activities6[5]} (".number_format($activities6[5]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 
                 
@@ -183,31 +167,26 @@ class Descriptors extends SpecialPage {
                     <td>No pain/discomfort</td>
                     <td>{$pain[1]} (".number_format($pain[1]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$pain6[1]} (".number_format($pain6[1]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Slight pain/discomfort</td>
                     <td>{$pain[2]} (".number_format($pain[2]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$pain6[2]} (".number_format($pain6[2]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Moderate pain/discomfort</td>
                     <td>{$pain[3]} (".number_format($pain[3]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$pain6[3]} (".number_format($pain6[3]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Severe pain/discomfort</td>
                     <td>{$pain[4]} (".number_format($pain[4]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$pain6[4]} (".number_format($pain6[4]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Extreme pain/discomfort</td>
                     <td>{$pain[5]} (".number_format($pain[5]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$pain6[5]} (".number_format($pain6[5]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 
                 
@@ -216,31 +195,26 @@ class Descriptors extends SpecialPage {
                     <td>Not anxious/depressed</td>
                     <td>{$anxiety[1]} (".number_format($anxiety[1]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$anxiety6[1]} (".number_format($anxiety6[1]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Slightly anxious/depressed</td>
                     <td>{$anxiety[2]} (".number_format($anxiety[2]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$anxiety6[2]} (".number_format($anxiety6[2]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Moderately anxious/depressed</td>
                     <td>{$anxiety[3]} (".number_format($anxiety[3]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$anxiety6[3]} (".number_format($anxiety6[3]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Severely anxious/depressed</td>
                     <td>{$anxiety[4]} (".number_format($anxiety[4]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$anxiety6[4]} (".number_format($anxiety6[4]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
                 <tr>
                     <td>Extremely anxious/depressed</td>
                     <td>{$anxiety[5]} (".number_format($anxiety[5]/max(1, $nIntake)*100, 1).")</td>
                     <td>{$anxiety6[5]} (".number_format($anxiety6[5]/max(1, $n6Month)*100, 1).")</td>
-                    <td></td>
                 </tr>
             </tbody>
         </table>");
