267575f8ed570cb7f630db6b8a6e8d4f14c8de72
David Turner <finalfantasykid13@gmail.com>
2023-01-23 12:33:46 -0700

    -Fixed error

--- extensions/Reporting/Reporting.php
+++ extensions/Reporting/Reporting.php
@@ -4,7 +4,7 @@ $extras = $config->getValue('reportingExtras');
 require_once("Report/AbstractReport.php");
 require_once("PDFGenerator/PDFGenerator.php");
 require_once("ReportArchive/ReportArchive.php");
-if($extras['AdminVisualizations']){
+if($extras['AdminVisualizations'] && isExtensionEnabled("Visualizations")){
     require_once("AdminVisualizations/AdminVisualizations.php");
 }
 
