d3a7569a268401f600bec9c5fe8c6162711746cb
David Turner <finalfantasykid13@gmail.com>
2023-04-26 15:37:57 -0600

    -Added project creation

--- extensions/Reporting/Report/SpecialPages/CIC/LOITable.php
+++ extensions/Reporting/Report/SpecialPages/CIC/LOITable.php
@@ -22,17 +22,92 @@ class LOITable extends SpecialPage{
         return ($person->isRoleAtLeast(STAFF));
     }
     
-    function handleEdit(){
+    function saveDates(){
         $this->saveBlobValue($_POST['blobItem'], 
                              $_POST['user_id'], 
                              $_POST['project_id'], 
                              $_POST['value']);
     }
+    
+    function createProject(){
+        $person = Person::newFromId($_POST['user_id']);
+        $projectId = $_POST['project_id'];
+        
+        // First create the project
+        $data = DBFunctions::execSQL("SELECT name 
+                                      FROM `grand_project` 
+                                      WHERE name LIKE 'P%'
+                                      ORDER BY id DESC
+                                      LIMIT 1");
+        $pId = intval(str_replace("P", "", $data[0]['name'])) + 1;
+        $_POST['acronym'] = "P{$pId}";
+        $_POST['fullName'] = $this->getBlobValue('TITLE', $person, $projectId);
+        $_POST['status'] = "Active";
+        $_POST['type'] = "Research";
+        $_POST['phase'] = PROJECT_PHASE;
+        $_POST['effective_date'] = date('Y-m-d');
+        $_POST['description'] = $this->getBlobValue('PROBLEM', $person, $projectId);
+        
+        APIRequest::doAction('CreateProject', false);
+        $project = Project::newFromName("P{$pId}");
+        
+        if($project == null || $project->getId() == 0){
+            echo "There was an error creating the project";
+            exit;
+        }
+        
+        $this->saveBlobValue('CREATED', 
+                             $person->getId(), 
+                             $projectId, 
+                             1);
+        
+        // Add Leader
+        $_POST['userId'] = $person->getId();
+        $_POST['name'] = PL;
+        $_POST['projects'] = array((object) array('id' => $project->getId(), 'name' => $project->getName()));
+        $_POST['startDate'] = date('Y-m-d');
+        $api = new RoleAPI();
+        $api->doPOST();
+        
+        // Copy LOI data to Project Table
+        $data = DBFunctions::execSQL("SELECT *
+                                      FROM grand_report_blobs 
+                                      WHERE user_id = '{$person->getId()}'
+                                      AND proj_id = '{$projectId}'
+                                      AND rp_type = 'RP_LOI'");
+        foreach($data as $row){
+            $blb = new ReportBlob($row['blob_type'], $row['year'], $row['user_id'], $row['proj_id']);
+            $addr = ReportBlob::create_address($row['rp_type'], $row['rp_section'], $row['rp_item'], $row['rp_subitem']);
+            $result = $blb->load($addr);
+            $blbdata = $blb->getData();
+        
+            $blb = new ReportBlob($row['blob_type'], 0, 0, $project->getId());
+            $addr = ReportBlob::create_address('RP_PROJECT_TABLE', 'PROJECTS', $row['rp_item'], $row['rp_subitem']);
+            $blb->store($blbdata, $addr);
+        }
+        
+        // Copy LOI PDF
+        $data = DBFunctions::execSQL("SELECT *
+                                      FROM grand_pdf_report 
+                                      WHERE user_id = '{$person->getId()}'
+                                      AND proj_id = '{$projectId}'
+                                      AND type = 'RP_LOI'");
+        if(isset($data[0])){   
+            $sto = new ReportStorage(new Person(array()), $project);
+            $rpData = "";
+            $sto->store_report($rpData, $data[0]['html'], $data[0]['pdf'], 0, 0, 'RP_LOI', 0, 0);
+        }
+    }
 
     function execute($par){
         global $wgOut, $wgUser, $wgServer, $wgScriptPath, $wgTitle, $wgMessage;
-        if(isset($_POST['value'])){
-            $this->handleEdit();
+        if(isset($_GET['saveDates'])){
+            $this->saveDates();
+            exit;
+        }
+        if(isset($_GET['createProject'])){
+            $this->createProject();
+            header('Content-Type: text/html');
             exit;
         }
         $people = Person::getAllCandidates();
@@ -54,9 +129,13 @@ class LOITable extends SpecialPage{
         foreach($people as $person){
             $projectId = -1;
             while(true){
-                $report = new DummyReport("LOI", $person, ++$projectId, 0, true);
+                $projectId++;
+                $report = new DummyReport("LOI", $person, $projectId, 0, true);
                 if(!$report->isGenerated()){
                     break;
+                }
+                if($this->getBlobValue('CREATED', $person, $projectId) == 1){
+                    //continue;
                 }
                 $pdf = $report->getPDF();
                 $wgOut->addHTML("<tr class='loiRow' data-person='{$person->getId()}' data-project='{$projectId}'>
@@ -101,17 +180,23 @@ class LOITable extends SpecialPage{
                             </tr>
                         </table>
                     </td>
-                    <td></td>
+                    <td><button class='createProject' type='button'>Create Project</button></td>
                 </tr>");
             }
         }
         $wgOut->addHTML("</tbody></table>
         <script type='text/javascript'>
-            $('#loi_table').DataTable({
-                'aLengthMenu': [[100,-1], [100,'All']], 
-                'iDisplayLength': -1, 
-                'autoWidth': false
-             });
+             var createTable = function(){
+                 return $('#loi_table').DataTable({
+                    'aLengthMenu': [[100,-1], [100,'All']], 
+                    'iDisplayLength': -1, 
+                    'autoWidth': false
+                 });
+             }
+             
+             var order = '';
+             var search = '';
+             var table = createTable();
              
              $.each($('input[type=datepicker]'), function(index, val){
                  $(val).datepicker({
@@ -133,8 +218,34 @@ class LOITable extends SpecialPage{
                     value: $(this).val()
                 };
                     
-                $.post('{$wgServer}{$wgScriptPath}/index.php/Special:LOITable', data, function(response){
+                $.post('{$wgServer}{$wgScriptPath}/index.php/Special:LOITable?saveDates', data, function(response){
                     
+                });
+            });
+            
+            $('.createProject').click(function(){
+                var tr = $(this).closest('.loiRow');
+                var data = {
+                    user_id: $(tr).attr('data-person'),
+                    project_id: $(tr).attr('data-project')
+                };
+                
+                $.post('{$wgServer}{$wgScriptPath}/index.php/Special:LOITable?createProject', data, function(response){
+                    clearError();
+                    clearSuccess();
+                    if(response != ''){
+                        addError(response);
+                    }
+                    else{
+                        addSuccess('The project was created');
+                        order = table.order();
+                        search = table.search();
+                        table.destroy();
+                        $(tr).remove();
+                        table = createTable();
+                        table.order(order);
+	                    table.search(search);
+                    }
                 });
             });
         </script>");
