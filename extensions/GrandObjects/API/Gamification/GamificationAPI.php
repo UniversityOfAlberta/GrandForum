fa5a1a9ce3853080233c3bfed7ec24c168d0598b
David Turner <finalfantasykid13@gmail.com>
2023-05-04 14:14:23 -0600

    -Adjust permissions

--- extensions/GrandObjects/API/Gamification/GamificationAPI.php
+++ extensions/GrandObjects/API/Gamification/GamificationAPI.php
@@ -3,10 +3,13 @@
 class GamificationAPI extends RESTAPI {
     
     function doGET(){
+        $me = Person::newFromWgUser();
         $userId = $this->getParam('userId');
         if($userId == ""){
-            $me = Person::newFromWgUser();
             $userId = $me->getId();
+        }
+        if(!$me->isLoggedIn() || ($me->getId() != $userId && !$me->isRoleAtLeast(STAFF))){
+            $this->throwError("You are not allowed to access this api");
         }
         $person = Person::newFromId($userId);
         return Gamification::calculatePoints($person);
