2ab1447afc492fa27f5f5239fcdec00582aba830
Finalfantasykid <finalfantasykid13@gmail.com>
2023-07-11 09:23:56 -0600

    -Fixed grouping

--- extensions/GrandObjectPage/LIMS/Views/LIMSContactsTableView.js
+++ extensions/GrandObjectPage/LIMS/Views/LIMSContactsTableView.js
@@ -55,9 +55,9 @@ LIMSContactsTableView = Backbone.View.extend({
             order = this.table.order();
             searchStr = this.table.search();
         }
-        var rowsGroup = [1,0,2,3,4,5,6,7];
+        var rowsGroup = [1,0,2,3,4,5,6];
         if(isAllowedToCreateLIMSContacts){
-            rowsGroup = [1,0,2,3,4,5,6,7,8];
+            rowsGroup = [1,0,2,3,4,5,6,7];
         }
         if(this.groupBy != null){
             rowsGroup = [this.groupBy].concat(rowsGroup);
