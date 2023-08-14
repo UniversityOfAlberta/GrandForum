d3edb87826f9d8128859d292cd5c04e854798fe5
David Turner <finalfantasykid13@gmail.com>
2023-08-14 13:58:22 -0600

    -Store past years

--- extensions/Reporting/Report/SpecialPages/Faculty of Science/FECReflections.php
+++ extensions/Reporting/Report/SpecialPages/Faculty of Science/FECReflections.php
@@ -20,87 +20,107 @@ class FECReflections extends SpecialPage {
     
     function showStats($year){
         global $wgOut;
-        $people = Person::getAllPeopleDuring(NI, "2000-01-01", "2100-01-01");
-    
-        $faculty = array();
-        $publications = array('pr' => array(),
-                              'nonpr' => array());
-        $rankings = array();
-        $nPeople = 0;
-        foreach($people as $person){
-            $case = $person->getCaseNumber($year);
-            if($case == "" ||
-               strstr($case, "D") !== false ||
-               strstr($case, "E") !== false ||
-               strstr($case, "F") !== false ||
-               strstr($case, "T") !== false){
-                continue;
-            }
-            $nPeople++;
-            $report = new Report();
-            $section = new EditableReportSection();
-            $pubs = new PersonProductsReportItemSet();
-            $report->year = $year;
-            $pubs->parent = $section;
-            $section->parent = $report;
-            $pubs->personId = $person->getId();
-            $pubs->attributes['category'] = "Publication";
-            $pubs->attributes['useProductYear'] = "true";
-            $pubs->attributes['includeHQP'] = "false";
-            $pubs->attributes['start'] = ($year-1)."-07-01";
-            $pubs->attributes['end'] = ($year)."-06-30";
-            // First do Peer Reviewed
-            $pubs->attributes['peerReviewed'] = "Yes";
-            foreach($pubs->getData() as $pub){
-                $pub = Product::newFromId($pub['product_id']);
-                if($pub->getType() == "Journal Paper"){
-                    $publications['pr']['journals'][$pub->getId()] = $pub;
-                    $ranking = $pub->getData(array('category_ranking'));
-                    $ranking = explode("/", $ranking);
-                    if(count($ranking) == 2){
-                        // Ranked
-                        $rankings[$pub->getId()] = $ranking[0]/$ranking[1];
+        $blob = new ReportBlob(BLOB_ARRAY, $year, 0, 0);
+	    $blob_address = ReportBlob::create_address("RP_FEC_REFLECTIONS", "REFLECTIONS", "REFLECTIONS", 0);
+	    $blob->load($blob_address);
+	    $blob_data = $blob->getData();
+	    
+        if(date('Y-m-d') >= ($year+1)."01-01" && is_array($blob_data)){
+            $nPeople = $blob_data['nPeople'];
+            $publications = $blob_data['publications'];
+            $rankings = $blob_data['rankings'];
+        }
+        else{
+            $people = Person::getAllPeopleDuring(NI, "2000-01-01", "2100-01-01");
+            $publications = array('pr' => array(),
+                                  'nonpr' => array());
+            $rankings = array();
+            $nPeople = 0;
+            foreach($people as $person){
+                $case = $person->getCaseNumber($year);
+                if($case == "" ||
+                   strstr($case, "D") !== false ||
+                   strstr($case, "E") !== false ||
+                   strstr($case, "F") !== false ||
+                   strstr($case, "T") !== false){
+                    continue;
+                }
+                $nPeople++;
+                $report = new Report();
+                $section = new EditableReportSection();
+                $pubs = new PersonProductsReportItemSet();
+                $report->year = $year;
+                $pubs->parent = $section;
+                $section->parent = $report;
+                $pubs->personId = $person->getId();
+                $pubs->attributes['category'] = "Publication";
+                $pubs->attributes['useProductYear'] = "true";
+                $pubs->attributes['includeHQP'] = "false";
+                $pubs->attributes['start'] = ($year-1)."-07-01";
+                $pubs->attributes['end'] = ($year)."-06-30";
+                // First do Peer Reviewed
+                $pubs->attributes['peerReviewed'] = "Yes";
+                foreach($pubs->getData() as $pub){
+                    $pub = Product::newFromId($pub['product_id']);
+                    if($pub->getType() == "Journal Paper"){
+                        $publications['pr']['journals'][$pub->getId()] = $pub;
+                        $ranking = $pub->getData(array('category_ranking'));
+                        $ranking = explode("/", $ranking);
+                        if(count($ranking) == 2){
+                            // Ranked
+                            $rankings[$pub->getId()] = $ranking[0]/$ranking[1];
+                        }
+                        else{
+                            // Unranked
+                            $rankings[$pub->getId()] = -1;
+                        }
+                    }
+                    else if($pub->getType() == "Conference Paper"){
+                        $publications['pr']['conference'][$pub->getId()] = $pub;
+                    }
+                    else if($pub->getType() == "Book Chapter"){
+                        $publications['pr']['book_chapters'][$pub->getId()] = $pub;
                     }
                     else{
-                        // Unranked
-                        $rankings[$pub->getId()] = -1;
+                        $publications['pr']['others'][$pub->getId()] = $pub;
                     }
                 }
-                else if($pub->getType() == "Conference Paper"){
-                    $publications['pr']['conference'][$pub->getId()] = $pub;
+                
+                // Then do Non-Peer Reviewed and Books
+                $pubs->attributes['peerReviewed'] = "";
+                foreach($pubs->getData() as $pub){
+                    $pub = Product::newFromId($pub['product_id']);
+                    if($pub->getData('peer_reviewed') == "" || $pub->getData('peer_reviewed') == "No"){
+                        if($pub->getType() != "Book"){
+                            $publications['nonpr']['journals'][$pub->getId()] = $pub;
+                        }
+                    }
+                    if($pub->getType() == "Book"){
+                        $publications['nonpr']['books'][$pub->getId()] = $pub;
+                    }
                 }
-                else if($pub->getType() == "Book Chapter"){
-                    $publications['pr']['book_chapters'][$pub->getId()] = $pub;
-                }
-                else{
-                    $publications['pr']['others'][$pub->getId()] = $pub;
+                
+                // Then do Patents
+                $pubs->attributes['category'] = "Patent/Spin-Off";
+                $pubs->attributes['peerReviewed'] = "";
+                $pubs->attributes['includeHQP'] = "false";
+                foreach($pubs->getData() as $pub){
+                    $pub = Product::newFromId($pub['product_id']);
+                    if($pub->getType() == "Patent" && ($pub->getStatus() == "Awarded" || $pub->getStatus() == "Published")){
+                        $publications['nonpr']['patents'][$pub->getId()] = $pub;
+                    }
                 }
             }
             
-            // Then do Non-Peer Reviewed and Books
-            $pubs->attributes['peerReviewed'] = "";
-            foreach($pubs->getData() as $pub){
-                $pub = Product::newFromId($pub['product_id']);
-                if($pub->getData('peer_reviewed') == "" || $pub->getData('peer_reviewed') == "No"){
-                    if($pub->getType() != "Book"){
-                        $publications['nonpr']['journals'][$pub->getId()] = $pub;
-                    }
-                }
-                if($pub->getType() == "Book"){
-                    $publications['nonpr']['books'][$pub->getId()] = $pub;
-                }
-            }
-            
-            // Then do Patents
-            $pubs->attributes['category'] = "Patent/Spin-Off";
-            $pubs->attributes['peerReviewed'] = "";
-            $pubs->attributes['includeHQP'] = "false";
-            foreach($pubs->getData() as $pub){
-                $pub = Product::newFromId($pub['product_id']);
-                if($pub->getType() == "Patent" && ($pub->getStatus() == "Awarded" || $pub->getStatus() == "Published")){
-                    $publications['nonpr']['patents'][$pub->getId()] = $pub;
-                }
-            }
+            if(date('Y-m-d') >= ($year+1)."01-01"){
+                $data = array('nPeople' => $nPeople,
+	                          'publications' => $publications,
+	                          'rankings' => $rankings);
+                
+                $blob = new ReportBlob(BLOB_ARRAY, $year, 0, 0);
+	            $blob_address = ReportBlob::create_address("RP_FEC_REFLECTIONS", "REFLECTIONS", "REFLECTIONS", 0);
+	            $blob->store($data, $blob_address, false);
+	        }
         }
         
         $n1_10 = 0;
