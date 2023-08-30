0a1cae1477875c86bd6ba430746dec6d81a9e728
Finalfantasykid <finalfantasykid13@gmail.com>
2023-08-30 13:56:08 -0600

    -Added column specific sorting

--- extensions/GrandObjectPage/CRM/Views/CRMContactsTableView.js
+++ extensions/GrandObjectPage/CRM/Views/CRMContactsTableView.js
@@ -62,6 +62,10 @@ CRMContactsTableView = Backbone.View.extend({
         if(this.groupBy != null){
             rowsGroup = [this.groupBy].concat(rowsGroup);
         }
+        this.$("table#contacts thead tr")
+            .clone(true)
+            .addClass('filters')
+            .appendTo('table#contacts thead');
         this.table = this.$("table#contacts").DataTable({
             "autoWidth": true,
             'bPaginate': false,
@@ -70,6 +74,7 @@ CRMContactsTableView = Backbone.View.extend({
             'aLengthMenu': [[-1], ['All']],
             'rowsGroup': rowsGroup,
             'dom': 'Blfrtip',
+            'orderCellsTop': true,
             'buttons': [
                 {
                     extend: 'excel',
@@ -85,7 +90,48 @@ CRMContactsTableView = Backbone.View.extend({
                         }
                     }
                 }
-            ]
+            ],
+            initComplete: function () {
+                var api = this.api();
+                // For each column
+                api
+                    .columns()
+                    .eq(0)
+                    .each(function (colIdx) {
+                        // Set the header cell to contain the input element
+                        var cell = $('.filters th').eq(
+                            $(api.column(colIdx).header()).index()
+                        );
+                        var title = $(cell).text();
+                        $(cell).html('<input type="text" placeholder="' + title + '" style="width:100%;" />');
+     
+                        // On every keypress in this input
+                        $(
+                            'input',
+                            $('.filters th').eq($(api.column(colIdx).header()).index())
+                        )
+                            .off('keyup change')
+                            .on('change', function (e) {
+                                // Get the search value
+                                $(this).attr('title', $(this).val());
+     
+                                cursorPosition = this.selectionStart;
+                                // Search the column for that value
+                                api
+                                    .column(colIdx)
+                                    .search(this.value, false, false)
+                                    .draw();
+                            })
+                            .on('keyup', function (e) {
+                                e.stopPropagation();
+     
+                                $(this).trigger('change');
+                                $(this)
+                                    .focus()[0]
+                                    .setSelectionRange(cursorPosition, cursorPosition);
+                            });
+                    });
+            }
         });
         table = this.table;
         this.table.order(order);
