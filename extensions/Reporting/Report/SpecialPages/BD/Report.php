cbc1a5f9135f0078822a6c42c3cd13a7e327b206
David Turner <finalfantasykid13@gmail.com>
2023-10-04 10:27:59 -0600

    -Added BD files

---
+++ extensions/Reporting/Report/SpecialPages/BD/Report.php
@@ -0,0 +1,43 @@
+<?php
+
+$dir = dirname(__FILE__) . '/';
+$wgSpecialPages['Report'] = 'Report'; # Let MediaWiki know about the special page.
+$wgExtensionMessagesFiles['Report'] = $dir . 'Report.i18n.php';
+$wgSpecialPageGroups['Report'] = 'reporting-tools';
+
+$wgHooks['TopLevelTabs'][] = 'Report::createTab';
+$wgHooks['SubLevelTabs'][] = 'Report::createSubTabs';
+$wgHooks['ToolboxLinks'][] = 'Report::createToolboxLinks';
+
+class Report extends AbstractReport{
+    
+    function __construct(){
+        global $config;
+        $report = @$_GET['report'];
+        $topProjectOnly = false;
+        parent::__construct(dirname(__FILE__)."/../../ReportXML/{$config->getValue('networkName')}/$report.xml", -1, false, $topProjectOnly);
+    }
+
+    static function createTab(&$tabs){
+        global $wgServer, $wgScriptPath, $wgUser, $wgTitle, $special_evals;
+        $tabs["Reports"] = TabUtils::createTab("My Reports");
+        $tabs["Applications"] = TabUtils::createTab("Applications");
+        return true;
+    }
+    
+    static function createSubTabs(&$tabs){
+        global $wgServer, $wgScriptPath, $wgUser, $wgTitle, $special_evals;
+        $person = Person::newFromWgUser();
+        $url = "$wgServer$wgScriptPath/index.php/Special:Report?report=";
+
+        return true;
+    }
+    
+    static function createToolboxLinks(&$toolbox){
+        global $wgServer, $wgScriptPath, $config;
+
+        return true;
+    }
+}
+
+?>
