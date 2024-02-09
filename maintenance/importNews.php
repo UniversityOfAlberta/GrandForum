0951129587ff758cf4f644bfd94b1dfd0f5634f4
David Turner <finalfantasykid13@gmail.com>
2024-02-09 15:37:53 -0700

    -Changed news

--- maintenance/importNews.php
+++ maintenance/importNews.php
@@ -9,19 +9,21 @@
     
     $allPeople = array_merge(Person::getAllPeopleDuring(NI, $start, $end),
                              Person::getAllPeopleDuring("ATS", $start, $end));
-    $json = file_get_contents("https://www.ualberta.ca/api/coveo/search/ualberta-news-feed?category=('Science%20and%20Technology')&news_source=('folio')&count=50");
+    $json = file_get_contents("https://www.ualberta.ca/api/coveo/search/ualberta-news-feed?category=('Science%20and%20Technology')&news_source=('folio')&count=100");
     $obj = json_decode($json);
     $results = array_reverse($obj->data->results);
     foreach($allPeople as $person){
         foreach($results as $news){
             $title = $news->title;
             $excerpt = $news->excerpt;
+            $image = $news->raw->ua__image;
+            $description = $news->raw->ua__description;
             $keywords = $news->raw->ua__keywords;
             $date = $news->raw->ua__news_date;
             $url = $news->uri;
             
-            $paragraph = "$title $excerpt $keywords";
-            $text = "{$title}<br />{$excerpt}<br /><a href='{$url}' target='_blank'>Full Story</a>";
+            $paragraph = "$title $excerpt $description $keywords";
+            $text = "<b>{$title}</b><br />{$description}<br /><a href='{$url}' target='_blank'>Full Story</a>";
             if(strstr($paragraph, $person->getNameForForms()) !== false ||
                strstr($paragraph, "{$person->getFirstName()} {$person->getLastName()}") !== false){
                 $data = DBFunctions::select(array('grand_stories'),
