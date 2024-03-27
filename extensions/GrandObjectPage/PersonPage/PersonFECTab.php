03c3404f814860625e72b0658351a6641df99629
David Turner <finalfantasykid13@gmail.com>
2024-03-27 13:19:31 -0600

    -Added sabbatical dates

--- extensions/GrandObjectPage/PersonPage/PersonFECTab.php
+++ extensions/GrandObjectPage/PersonPage/PersonFECTab.php
@@ -16,6 +16,11 @@ class PersonFECTab extends AbstractEditableTab {
         $me = Person::newFromWgUser();
         $this->person->getFecPersonalInfo();
         if($me->isRoleAtLeast(STAFF)){
+            $sabbaticals = array();
+            foreach(@$_POST['sabbatical_start'] as $key => $start){
+                $sabbaticals[] = array('start' => $start,
+                                       'duration' => @$_POST['sabbatical_duration'][$key]);
+            }
             $this->person->dateOfPhd = @$_POST['dateOfPhd'];
             $this->person->dateOfAppointment = @$_POST['dateOfAppointment'];
             $this->person->dateOfAssistant = @$_POST['dateOfAssistant'];
@@ -24,6 +29,7 @@ class PersonFECTab extends AbstractEditableTab {
             $this->person->dateOfTenure = @$_POST['dateOfTenure'];
             $this->person->dateOfProbation1 = @$_POST['dateOfProbation1'];
             $this->person->dateOfProbation2 = @$_POST['dateOfProbation2'];
+            $this->person->sabbatical = $sabbaticals;
             $this->person->dateOfRetirement = @$_POST['dateOfRetirement'];
             $this->person->dateOfLastDegree = @$_POST['dateOfLastDegree'];
             $this->person->dateFso2 = @$_POST['dateFso2'];
@@ -146,24 +152,32 @@ class PersonFECTab extends AbstractEditableTab {
         $hrNoteLink = ($hrNoteMD5 != "") ? "<a href='{$wgServer}{$wgScriptPath}/index.php?action=downloadBlob&id={$hrNoteMD5}'>Download</a>" : "";
         
         $this->html .= "<table>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of PhD:</b></td><td>".substr($this->person->dateOfPhd, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of Appointment:</b></td><td>".substr($this->person->dateOfAppointment, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of Assistant:</b></td><td>".substr($this->person->dateOfAssistant, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of Associate:</b></td><td>".substr($this->person->dateOfAssociate, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of Professor:</b></td><td>".substr($this->person->dateOfProfessor, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of FSO II:</b></td><td>".substr($this->person->dateFso2, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of FSO III:</b></td><td>".substr($this->person->dateFso3, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of FSO IV:</b></td><td>".substr($this->person->dateFso4, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of ATS I:</b></td><td>".substr($this->person->dateAtsec1, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of ATS II:</b></td><td>".substr($this->person->dateAtsec2, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of ATS III:</b></td><td>".substr($this->person->dateAtsec3, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>End of Probation 1:</b></td><td>".substr($this->person->dateOfProbation1, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>End of Probation 2:</b></td><td>".substr($this->person->dateOfProbation2, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of Tenure:</b></td><td>".substr($this->person->dateOfTenure, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of Retirement:</b></td><td>".substr($this->person->dateOfRetirement, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Date of Last Degree:</b></td><td>".substr($this->person->dateOfLastDegree, 0, 10)."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>Last Degree:</b></td><td>".$this->person->lastDegree."</td></tr>";
-        $this->html .= "<tr><td align='right' style='white-space:nowrap;'><b>HR Note:</b></td><td>{$hrNoteLink}</td></tr>";
+        $this->html .= ($this->person->dateOfPhd != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of PhD:</b></td><td>".substr($this->person->dateOfPhd, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateOfAppointment != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of Appointment:</b></td><td>".substr($this->person->dateOfAppointment, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateOfAssistant != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of Assistant:</b></td><td>".substr($this->person->dateOfAssistant, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateOfAssociate != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of Associate:</b></td><td>".substr($this->person->dateOfAssociate, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateOfProfessor != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of Professor:</b></td><td>".substr($this->person->dateOfProfessor, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateFso2 != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of FSO II:</b></td><td>".substr($this->person->dateFso2, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateFso3 != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of FSO III:</b></td><td>".substr($this->person->dateFso3, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateFso4 != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of FSO IV:</b></td><td>".substr($this->person->dateFso4, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateAtsec1 != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of ATS I:</b></td><td>".substr($this->person->dateAtsec1, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateAtsec2 != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of ATS II:</b></td><td>".substr($this->person->dateAtsec2, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateAtsec3 != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of ATS III:</b></td><td>".substr($this->person->dateAtsec3, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateOfProbation1 != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>End of Probation 1:</b></td><td>".substr($this->person->dateOfProbation1, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateOfProbation2 != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>End of Probation 2:</b></td><td>".substr($this->person->dateOfProbation2, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateOfTenure != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of Tenure:</b></td><td>".substr($this->person->dateOfTenure, 0, 10)."</td></tr>" : "";
+        if(!empty($this->person->sabbatical)){
+            $sabbs = array();
+            foreach($this->person->sabbatical as $sabbatical){
+                $end = date('Y-m-d', strtotime("+{$sabbatical['duration']} month", strtotime($sabbatical['start'])));
+                $sabbs[] = "{$sabbatical['start']} - {$end}";
+            }
+            $this->html .= "<tr><td align='right' valign='top' style='white-space:nowrap;'><b>Sabbaticals:</b></td><td>".implode("<br />", $sabbs)."</td></tr>";
+        }
+        $this->html .= ($this->person->dateOfRetirement != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of Retirement:</b></td><td>".substr($this->person->dateOfRetirement, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->dateOfLastDegree != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Date of Last Degree:</b></td><td>".substr($this->person->dateOfLastDegree, 0, 10)."</td></tr>" : "";
+        $this->html .= ($this->person->lastDegree != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>Last Degree:</b></td><td>".$this->person->lastDegree."</td></tr>" : "";
+        $this->html .= ($hrNoteLink != "") ? "<tr><td align='right' style='white-space:nowrap;'><b>HR Note:</b></td><td>{$hrNoteLink}</td></tr>" : "";
         $this->html .= "</table>";
     }
     
@@ -175,6 +189,18 @@ class PersonFECTab extends AbstractEditableTab {
         $this->person->getFecPersonalInfo();
         $this->html .= "<table>";
         if($me->isRoleAtLeast(STAFF)){
+            $sabbaticalsHTML = array();
+            if(!empty($this->person->sabbatical)){
+                foreach($this->person->sabbatical as $sabbatical){
+                    $sixSelected = ($sabbatical['duration'] == "6") ? " selected" : "";
+                    $twelveSelected = ($sabbatical['duration'] == "12") ? " selected" : "";
+                    $sabbaticalsHTML[] = "<div class='sabbatical'>
+                                            <input type='text' class='calendar' name='sabbatical_start[]' value='{$sabbatical['start']}' />&nbsp;
+                                            <select name='sabbatical_duration[]'><option value='6' {$sixSelected}>6 Months</option><option value='12' {$twelveSelected}>12 Months</option></select>
+                                          </div>";
+                }
+            }
+            
             $this->html .= "<tr><td align='right'><b>Date of PhD:</b></td><td><input type='text' name='dateOfPhd' class='calendar' style='display:none;' value='".substr($this->person->dateOfPhd, 0, 10)."' /></td></tr>";
             $this->html .= "<tr><td align='right'><b>Date of Appointment:</b></td><td><input type='text' name='dateOfAppointment' class='calendar' style='display:none;' value='".substr($this->person->dateOfAppointment, 0, 10)."' /></td></tr>";
             $this->html .= "<tr><td align='right'><b>Date of Assistant:</b></td><td><input type='text' name='dateOfAssistant' class='calendar' style='display:none;' value='".substr($this->person->dateOfAssistant, 0, 10)."' /></td></tr>";
@@ -189,6 +215,7 @@ class PersonFECTab extends AbstractEditableTab {
             $this->html .= "<tr><td align='right'><b>End of Probation1:</b></td><td><input type='text' name='dateOfProbation1' class='calendar' style='display:none;' value='".substr($this->person->dateOfProbation1, 0, 10)."' /></td></tr>";
             $this->html .= "<tr><td align='right'><b>End of Probation2:</b></td><td><input type='text' name='dateOfProbation2' class='calendar' style='display:none;' value='".substr($this->person->dateOfProbation2, 0, 10)."' /></td></tr>";
             $this->html .= "<tr><td align='right'><b>Date of Tenure:</b></td><td><input type='text' name='dateOfTenure' class='calendar' style='display:none;' value='".substr($this->person->dateOfTenure, 0, 10)."' /></td></tr>";
+            $this->html .= "<tr><td align='right' class='label'><b>Sabbaticals:</b></td><td><div id='sabbaticals'>".implode("<hr />", $sabbaticalsHTML)."</div><button id='minus' type='button'>-</button><button id='plus' type='button'>+</button></td></tr>";
             $this->html .= "<tr><td align='right'><b>Date of Retirement:</b></td><td><input type='text' name='dateOfRetirement' class='calendar' style='display:none;' value='".substr($this->person->dateOfRetirement, 0, 10)."' /></td></tr>";
             $this->html .= "<tr><td align='right'><b>Date of Last Degree:</b></td><td><input type='text' name='dateOfLastDegree' class='calendar' style='display:none;' value='".substr($this->person->dateOfLastDegree, 0, 10)."' /></td></tr>";
             $this->html .= "<tr><td align='right'><b>Last Degree:</b></td><td><input type='text' name='lastDegree' value='".$this->person->lastDegree."' /></td></tr>";
@@ -198,29 +225,44 @@ class PersonFECTab extends AbstractEditableTab {
         
         $this->html .= "<script type='text/javascript'>
             $(document).ready(function(){
-                $('input.calendar').show();
-                $('input.calendar').keyup(function(){ return false; });
-                $('input.calendar').keydown(function(){ return false; });
-                $('input.calendar').datepicker({dateFormat: 'yy-mm-dd', 
-                                                changeYear: true, 
-                                                changeMonth: true, 
-                                                yearRange: '1900:".(date('Y')+3)."',
-                                                onChangeMonthYear: function (year, month, inst) {
-                                                    var curDate = $(this).datepicker('getDate');
-                                                    if (curDate == null)
-                                                        return;
-                                                    if (curDate.getYear() != year || curDate.getMonth() != month - 1) {
-                                                        curDate.setYear(year);
-                                                        curDate.setMonth(month - 1);
-                                                        while(curDate.getMonth() != month -1){
-                                                            curDate.setDate(curDate.getDate() - 1);
+                function initCals(){
+                    $('input.calendar:not(.initialized)').show();
+                    $('input.calendar:not(.initialized)').datepicker({dateFormat: 'yy-mm-dd', 
+                                                    changeYear: true, 
+                                                    changeMonth: true, 
+                                                    yearRange: '1900:".(date('Y')+3)."',
+                                                    onChangeMonthYear: function (year, month, inst) {
+                                                        var curDate = $(this).datepicker('getDate');
+                                                        if (curDate == null)
+                                                            return;
+                                                        if (curDate.getYear() != year || curDate.getMonth() != month - 1) {
+                                                            curDate.setYear(year);
+                                                            curDate.setMonth(month - 1);
+                                                            while(curDate.getMonth() != month -1){
+                                                                curDate.setDate(curDate.getDate() - 1);
+                                                            }
+                                                            $(this).datepicker('setDate', curDate);
+                                                            $(this).trigger('change');
                                                         }
-                                                        $(this).datepicker('setDate', curDate);
-                                                        $(this).trigger('change');
                                                     }
-                                                }
-                                              });
-                $('<span style=\"vertical-align: middle;\" class=\"delete-icon\" title=\"Clear Date\"></span>').insertAfter('input.calendar').click(function(){ $(this).prev().val('0000-00-00'); });
+                                                  });
+                    $('<span style=\"vertical-align: middle;\" class=\"delete-icon\" title=\"Clear Date\"></span>').insertAfter('input.calendar:not(.initialized):not([name=sabbatical_start\\\[\\\]])').click(function(){ $(this).prev().val('0000-00-00'); });
+                    $('input.calendar:not(.initialized)').addClass('initialized');
+                }
+                initCals();
+                
+                $('#plus').click(function(){
+                    if($('.sabbatical').length > 0){
+                        $('#sabbaticals').append('<hr />');
+                    }
+                    $('#sabbaticals').append(\"<div class='sabbatical'><input type='text' class='calendar' name='sabbatical_start[]' />&nbsp;<select name='sabbatical_duration[]'><option value='6'>6 Months</option><option value='12'>12 Months</option></select></div>\");
+                    initCals();
+                });
+                
+                $('#minus').click(function(){
+                    $('#sabbaticals hr').last().remove();
+                    $('.sabbatical').last().remove();
+                });
             });
         </script>";
     }
