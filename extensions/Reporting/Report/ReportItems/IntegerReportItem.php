45ff22df396613b4d6b077241b7814ebbf0938b5
David Turner <finalfantasykid13@gmail.com>
2024-04-09 14:29:46 -0600

    -Added placeholder text

--- extensions/Reporting/Report/ReportItems/IntegerReportItem.php
+++ extensions/Reporting/Report/ReportItems/IntegerReportItem.php
@@ -8,12 +8,14 @@
 		$max = $this->getAttr('max', 1000000000);
         $default = $this->getAttr('default', "");
         $value = $this->getBlobValue();
+        $placeholder = $this->getAttr('placeholder', '');
+        $placeholder = str_replace("'", "&#39;", $placeholder);
         if($value == ""){
             $value = $default;
         }
         $decimals = $this->getAttr('decimals', 0);
         $width = (isset($this->attributes['width'])) ? $this->attributes['width'] : "150px";
-        $item = "<input type='text' name='{$this->getPostId()}' style='width:{$width};text-align:right;' value='{$value}' />";
+        $item = "<input type='text' name='{$this->getPostId()}' style='width:{$width};text-align:right;' placeholder='{$placeholder}' value='{$value}' />";
         $item = $this->processCData($item);
         $wgOut->addHTML("$item");
         $wgOut->addHTML("<script type='text/javascript'>
