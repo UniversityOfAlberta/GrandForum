a78506d3ccf58680caf567f8060948a1df8fd719
David Turner <finalfantasykid13@gmail.com>
2024-04-10 11:22:13 -0600

    -Changed the hqp supervison section

--- extensions/Reporting/Report/ReportItems/ReportItemSets/StringReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/StringReportItemSet.php
@@ -5,12 +5,19 @@ class StringReportItemSet extends ReportItemSet {
     function getData(){
         $data = array();
         $array = explode("|", $this->getAttr("array"));
+        $labels = ($this->getAttr("labels") != "") ? explode("|", $this->getAttr("labels")) : array();
         foreach($array as $key => $el){
             if($el == null || $el == ""){
                 continue;
             }
             $tuple = self::createTuple();
-            $tuple['extra'] = $el;
+            if(isset($labels[$key])){
+                // Label found
+                $tuple['extra'] = array('label' => $labels[$key], 'value' => $el);
+            }
+            else{
+                $tuple['extra'] = $el;
+            }
             $data[$key] = $tuple;
         }
         return $data;
