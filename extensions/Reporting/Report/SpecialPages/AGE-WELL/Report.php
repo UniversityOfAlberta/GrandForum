066bdb822f99ee0b2a7317b71d881603801f1ae0
Finalfantasykid <finalfantasykid13@gmail.com>
2024-05-06 09:30:59 -0600

    -Closed EPIC-AT

--- extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/Report.php
@@ -131,10 +131,10 @@ class Report extends AbstractReport {
             $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("ECR", "{$url}ECR", $selected);
         }*/
         
-        if($person->isLoggedIn()){
+        /*if($person->isLoggedIn()){
             $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "EPIC-AT")) ? "selected" : false;
             $tabs["Applications"]['subtabs'][] = TabUtils::createSubTab("EPIC-AT Award 2024", "{$url}EPIC-AT", $selected);
-        }
+        }*/
         
         /*if($person->isLoggedIn()){
             $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "EPIC-AT2023")) ? "selected" : false;
