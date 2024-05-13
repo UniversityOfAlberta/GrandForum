74e9354553cfb85079c600b304eead1ab5706a05
David Turner <finalfantasykid13@gmail.com>
2024-05-13 11:53:52 -0600

    -Fixed error

--- extensions/Shibboleth/ShibAuthPlugin.php
+++ extensions/Shibboleth/ShibAuthPlugin.php
@@ -391,9 +391,11 @@ function ShibUserLoadFromSession($user, &$result)
 		impersonate();
 
         $me = Person::newFromWgUser();
-        $me->getFecPersonalInfo();
-        if($me->faculty != getFaculty() && !$me->isRoleAtLeast(MANAGER)){
-            permissionError("You are on the wrong AIMS instance.  Make sure you clicked on the correct faculty.  Contact <a href='mailto:{$config->getValue('supportEmail')}'>{$config->getValue('supportEmail')}</a> if you still don't have access.");
+        if($me instanceof FullPerson){
+            $me->getFecPersonalInfo();
+            if($me->faculty != getFaculty() && !$me->isRoleAtLeast(MANAGER)){
+                permissionError("You are on the wrong AIMS instance.  Make sure you clicked on the correct faculty.  Contact <a href='mailto:{$config->getValue('supportEmail')}'>{$config->getValue('supportEmail')}</a> if you still don't have access.");
+            }
         }
 		return true;
 	}
