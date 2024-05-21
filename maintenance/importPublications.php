356e25b03c3b61adbdb9275d184d04599998fec4
David Turner <finalfantasykid13@gmail.com>
2024-05-21 11:32:28 -0600

    -Updated publication importing

--- maintenance/importPublications.php
+++ maintenance/importPublications.php
@@ -3,6 +3,51 @@
 require_once('commandLine.inc');
 global $wgUser;
 
+function fixAuthors($pub, $authors){
+    $pub->authors = array();
+    foreach($authors as $author){
+        if($author->author->orcid != ""){
+            $p = Person::newFromOrcid($author->author->orcid);
+        }
+        else{
+            $p = new LimitedPerson(array());
+        }
+        
+        if($p->getId() != 0){
+            // ORCID located
+            $pub->authors[] = $p;
+        }
+        else{
+            // Simple author matching
+            $obj = new stdClass;
+            $found = false;
+            foreach($author->institutions as $institution){
+                if($institution->display_name == "University of Alberta" || 
+                   strstr($institution->display_name, 'Alberta') !== false || 
+                   strstr($institution->display_name, 'Edmonton') !== false){
+                    $found = true;
+                }
+            }
+            if($found){
+                // At UofA (or probably anyways)
+                $obj->name = trim($author->author->display_name);
+            }
+            else{
+                // Not at UofA
+                $names = explode(" ", trim($author->author->display_name), 2);
+                if(count($names) > 1){
+                    $obj->name = "\"{$names[1]}, {$names[0]}\"";
+                }
+                else{
+                    $obj->name = $names[0];
+                }
+            }
+            $pub->authors[] = $obj;
+        }
+    }
+    return $pub;
+}
+
 $wgUser = User::newFromId(1);
 
 $people = array_merge(Person::getAllPeople("Faculty"), Person::getAllPeople("ATS"));
@@ -23,8 +68,10 @@ foreach($people as $person){
                 $alreadyImported[$doi] = true;
                 echo "\t{$doi} ... ";
                 $res = array('created' => array());
-                if(!Product::newFromTitle($title)->exists() &&
-                   !Product::newFromBibTeXId(str_replace("https://doi.org/", "", $doi))->exists()){
+                $pubByTitle = Product::newFromTitle($title);
+                $pubByDoi = Product::newFromBibTeXId(str_replace("https://doi.org/", "", $doi));
+                if(!$pubByTitle->exists() &&
+                   !$pubByDoi->exists()){
                     $_POST['doi'] = $doi;
                     $api = new ImportDOIAPI();
                     $res = $api->doAction();
@@ -38,7 +85,6 @@ foreach($people as $person){
                     $pub->access_id = 0;
                     $pub->acceptance_date = $result->publication_date;
                     $pub->date = $result->publication_date;
-                    $pub->authors = array();
                     $description = array();
                     if(isset($result->abstract_inverted_index)){
                         foreach($result->abstract_inverted_index as $word => $indices){
@@ -49,48 +95,17 @@ foreach($people as $person){
                     }
                     ksort($description);
                     $pub->description = html_entity_decode(implode(" ", $description));
-                    foreach($authors as $author){
-                        if($author->author->orcid != ""){
-                            $p = Person::newFromOrcid($author->author->orcid);
-                        }
-                        else{
-                            $p = new LimitedPerson(array());
-                        }
-                        
-                        if($p->getId() != 0){
-                            // ORCID located
-                            $pub->authors[] = $p;
-                        }
-                        else{
-                            // Simple author matching
-                            $obj = new stdClass;
-                            $found = false;
-                            foreach($author->institutions as $institution){
-                                if($institution->display_name == "University of Alberta" || 
-                                   strstr($institution->display_name, 'Alberta') !== false || 
-                                   strstr($institution->display_name, 'Edmonton') !== false){
-                                    $found = true;
-                                }
-                            }
-                            if($found){
-                                // At UofA (or probably anyways)
-                                $obj->name = trim($author->author->display_name);
-                            }
-                            else{
-                                // Not at UofA
-                                $names = explode(" ", trim($author->author->display_name), 2);
-                                if(count($names) > 1){
-                                    $obj->name = "\"{$names[1]}, {$names[0]}\"";
-                                }
-                                else{
-                                    $obj->name = $names[0];
-                                }
-                            }
-                            $pub->authors[] = $obj;
-                        }
-                    }
+                    $pub = fixAuthors($pub, $authors);
                     $pub->update(true);
                     echo "created\n";
+                }
+                else if($pubByDoi->exists() && abs(strtotime($pubByDoi->dateCreated) - strtotime($pubByDoi->lastModified)) <= 10){
+                    $pubByDoi = fixAuthors($pubByDoi, $authors);
+                    $pubByDoi->update(true);
+                    DBFunctions::update('grand_products',
+                                        array('date_changed' => $pubByDoi->lastModified),
+                                        array('id' => $pubByDoi->getId()));
+                    echo "updating authors\n";
                 }
                 else{
                     echo "skip\n";
