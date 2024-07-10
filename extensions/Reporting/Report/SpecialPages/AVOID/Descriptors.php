60514a20fa76872250d64785f86893454ac16cc8
David Turner <finalfantasykid13@gmail.com>
2024-07-10 14:09:08 -0600

    -Changed sig digs

--- extensions/Reporting/Report/SpecialPages/AVOID/Descriptors.php
+++ extensions/Reporting/Report/SpecialPages/AVOID/Descriptors.php
@@ -977,14 +977,14 @@ class Descriptors extends SpecialPage {
 	                            <td>".number_format(avg($frailty6[$i]), 2)."</td>
 	                            <td>".number_format(median($frailty6[$i]), 2)."</td>
 	                            <td>".number_format(stdev($frailty6[$i]), 2)."</td>
-	                            <td>".number_format(avg($means6[$i]), 4)."</td>
-	                            <td>".number_format(pow(avg($means6[$i]), 2), 5)."</td>
+	                            <td>".number_format(avg($means6[$i]), 3)."</td>
+	                            <td>".number_format(pow(avg($means6[$i]), 2), 6)."</td>
 	                            <td style='white-space:nowrap;'>".count($frailty12[$i])." (".number_format(count($frailty12[$i])/max(1, $n12Month)*100, 1).")</td>
 	                            <td>".number_format(avg($frailty12[$i]), 2)."</td>
 	                            <td>".number_format(median($frailty12[$i]), 2)."</td>
 	                            <td>".number_format(stdev($frailty12[$i]), 2)."</td>
-	                            <td>".number_format(avg($means12[$i]), 4)."</td>
-	                            <td>".number_format(pow(avg($means12[$i]), 2), 5)."</td>
+	                            <td>".number_format(avg($means12[$i]), 3)."</td>
+	                            <td>".number_format(pow(avg($means12[$i]), 2), 6)."</td>
 	                        </tr>");
         }
         @$wgOut->addHTML("
