ad21f34fc432d7d8168b0db656d6fce8458873f5
David Turner <finalfantasykid13@gmail.com>
2024-07-10 10:40:44 -0600

    -Fixed warning

--- extensions/GrandObjects/Person.php
+++ extensions/GrandObjects/Person.php
@@ -3250,7 +3250,7 @@ class Person extends BackboneModel {
                 }
             }
             foreach($tmp as $i => $t){
-                $ret[] = array('id' => $i, 'question' => $qTexts[$i], 'votes' => $t);
+                $ret[] = array('id' => $i, 'question' => str_replace(",", "&#44;", $qTexts[$i]), 'votes' => $t);
             }
             return $ret;
         }
