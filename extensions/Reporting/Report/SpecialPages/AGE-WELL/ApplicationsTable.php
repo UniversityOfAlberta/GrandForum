45556416771f3caf1c92cf53fff2a73aa77bee66
Finalfantasykid <finalfantasykid13@gmail.com>
2024-07-16 12:58:18 -0600

    -Changed year for EEA

--- extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
@@ -861,6 +861,7 @@ class ApplicationsTable extends SpecialPage{
     function generateEEA(){
         global $wgOut;
         $tabbedPage = new InnerTabbedPage("reports");
+        $tabbedPage->addTab(new ApplicationTab('RP_EEA', null, 2024, "2024"));
         $tabbedPage->addTab(new ApplicationTab('RP_EEA', null, 2023, "2023"));
         $tabbedPage->addTab(new ApplicationTab('RP_EEA', null, 2022, "2022"));
         $tabbedPage->addTab(new ApplicationTab('RP_EEA', null, 2021, "2021"));
