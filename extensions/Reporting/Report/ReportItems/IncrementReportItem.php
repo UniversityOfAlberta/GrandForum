dfea9cee6b2ee0fc7fdfbba4eaa72f9d89b194d6
David Turner <finalfantasykid13@gmail.com>
2024-10-02 14:55:12 -0600

    -Don't show previous increments

--- extensions/Reporting/Report/ReportItems/IncrementReportItem.php
+++ extensions/Reporting/Report/ReportItems/IncrementReportItem.php
@@ -130,7 +130,7 @@ class IncrementReportItem extends SelectReportItem {
 	    $history = (strtolower($this->getAttr("history", "true")) == "true");
 	    $person = Person::newFromId($this->blobSubItem);
 	    $replacedText = "";
-	    if($history){
+	    /*if($history){
 	        $popup = "<div id='previousIncrements{$this->blobSubItem}' title='Previous Increments' style='display:none;'>
 	                    <center><b>{$person->getNameForForms()}</b></center>
 	                    <table data-fecid='{getExtra()}' class='wikitable' frame='box' rules='all' width='100%'>
@@ -162,7 +162,7 @@ class IncrementReportItem extends SelectReportItem {
 	            });
 	        </script>";
 	        $replacedText = '&nbsp;<a id="previousIncrementsLink'.$this->blobSubItem.'" title="Previous Increments" class="tooltip" style="cursor:pointer;display:inline-block;vertical-align:bottom;"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"/></svg></a>'.$popup;
-	    }
+	    }*/
 	    $this->value = str_replace('{$item}', '{$item}'.$replacedText, $this->value);
 	    $this->value = str_replace('{$value}', '{$value}'.$replacedText, $this->value);
 	    parent::render();
