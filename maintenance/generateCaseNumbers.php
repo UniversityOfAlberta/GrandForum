576639967eee4cb3474fbdd0e2bdc5d5e7afe4ef
David Turner <finalfantasykid13@gmail.com>
2024-10-09 14:11:17 -0600

    -Handle Engineering case number sorting

--- maintenance/generateCaseNumbers.php
+++ maintenance/generateCaseNumbers.php
@@ -19,62 +19,95 @@
                              Person::getAllPeopleDuring("ATS", $start, $end));
                              
     $allPeople = Person::filterFaculty($allPeople);
-
-    $data = DBFunctions::execSQL("SELECT `user_id`, `date_of_phd`, `date_of_appointment` 
+    
+    $counts = array();
+    $fec = array();
+    
+    function isValid($person){
+        global $start, $end;
+        return !($person == null || $person->getId() == 0 || $person->isSubRole("NoAR") || (!$person->isRoleDuring(NI, $start, $end) && !$person->isRoleDuring("ATS", $start, $end)));
+    }
+    
+    if(getFaculty() == "Engineering"){
+        // Sort by Salary
+        $data = DBFunctions::execSQL("SELECT `user_id`
+                                      FROM `grand_user_salaries`
+                                      WHERE `year` = '".YEAR."'
+                                      ORDER BY salary ASC");
+        foreach($data as $row){
+            $person = Person::newFromId($row['user_id']);
+            if(!isValid($person)){
+                // Check to make sure the person exists, and is an Faculty
+                continue;
+            }
+            $fecType = $person->getFECType($end);
+            $index = @++$counts[$fecType];
+            $fec[$row['user_id']] = $index;
+        }
+        foreach($allPeople as $person){
+            $fecType = $person->getFECType($end);
+            if(!isset($fec[$person->getId()]) && isValid($person) && $fecType != ""){
+                echo "Missing salary information: {$person->getNameForForms()}\n";
+                $index = @++$counts[$fecType];
+                $fec[$person->getId()] = $index;
+            }
+        }
+    }
+    else{
+        // Sort by PhD or Appointment date
+        $data = DBFunctions::execSQL("SELECT `user_id`, `date_of_phd`, `date_of_appointment` 
                                   FROM `grand_personal_fec_info` 
                                   WHERE `date_retirement` >= '{$end}' OR 
                                         `date_retirement`  = '0000-00-00 00:00:00' 
                                   ORDER BY REPLACE(`date_of_phd`, '0000-00-00 00:00:00', `date_of_appointment`) DESC");
                                   
-    $data2 = DBFunctions::execSQL("SELECT `user_id`, `date_of_phd`, `date_of_appointment` 
-                                   FROM `grand_personal_fec_info` 
-                                   WHERE `date_retirement` >= '{$end}' OR 
-                                         `date_retirement`  = '0000-00-00 00:00:00' 
-                                   ORDER BY `date_of_appointment` DESC");
-    
-    $counts = array();
-    $fec = array();
-    foreach($data as $row){
-        // Ordered by PhD Date
-        $person = Person::newFromId($row['user_id']);
-        if($person == null || $person->getId() == 0 || $person->isSubRole("NoAR") || (!$person->isRoleDuring(NI, $start, $end) && !$person->isRoleDuring("ATS", $start, $end))){
-            // Check to make sure the person exists, and is an Faculty
-            continue;
-        }
-        $fecType = $person->getFECType($end);
-        if($fecType == "B1" ||
-           $fecType == "B2" ||
-           $fecType == "C1"){
-            if($row['date_of_phd'] == "0000-00-00 00:00:00"){
-                echo "Missing PhD date: {$person->getNameForForms()}\n";
+        $data2 = DBFunctions::execSQL("SELECT `user_id`, `date_of_phd`, `date_of_appointment` 
+                                       FROM `grand_personal_fec_info` 
+                                       WHERE `date_retirement` >= '{$end}' OR 
+                                             `date_retirement`  = '0000-00-00 00:00:00' 
+                                       ORDER BY `date_of_appointment` DESC");
+        foreach($data as $row){
+            // Ordered by PhD Date
+            $person = Person::newFromId($row['user_id']);
+            if(!isValid($person)){
+                // Check to make sure the person exists, and is an Faculty
+                continue;
             }
-            $index = @++$counts[$fecType];
-            $fec[$row['user_id']] = $index;
-        }
-    }
-    
-    foreach($data2 as $row){
-        // Ordered by Appointment Date
-        $person = Person::newFromId($row['user_id']);
-        if($person == null || $person->getId() == 0 || $person->isSubRole("NoAR") || (!$person->isRoleDuring(NI, $start, $end) && !$person->isRoleDuring("ATS", $start, $end))){
-            // Check to make sure the person exists, and is an Faculty
-            continue;
-        }
-        $fecType = $person->getFECType($end);
-        if($fecType == "N1" ||
-           $fecType == "M1" ||
-           $fecType == "A1" ||
-           $fecType == "D1" ||
-           $fecType == "E1" ||
-           $fecType == "F1" ||
-           $fecType == "T1" ||
-           $fecType == "T2" ||
-           $fecType == "T3"){
-            if($row['date_of_appointment'] == "0000-00-00 00:00:00"){
-                echo "Missing Appointment date: {$person->getNameForForms()}\n";
+            $fecType = $person->getFECType($end);
+            if($fecType == "B1" ||
+               $fecType == "B2" ||
+               $fecType == "C1"){
+                if($row['date_of_phd'] == "0000-00-00 00:00:00"){
+                    echo "Missing PhD date: {$person->getNameForForms()}\n";
+                }
+                $index = @++$counts[$fecType];
+                $fec[$row['user_id']] = $index;
             }
-            $index = @++$counts[$fecType];
-            $fec[$row['user_id']] = $index;
+        }
+        
+        foreach($data2 as $row){
+            // Ordered by Appointment Date
+            $person = Person::newFromId($row['user_id']);
+            if(!isValid($person)){
+                // Check to make sure the person exists, and is an Faculty
+                continue;
+            }
+            $fecType = $person->getFECType($end);
+            if($fecType == "N1" ||
+               $fecType == "M1" ||
+               $fecType == "A1" ||
+               $fecType == "D1" ||
+               $fecType == "E1" ||
+               $fecType == "F1" ||
+               $fecType == "T1" ||
+               $fecType == "T2" ||
+               $fecType == "T3"){
+                if($row['date_of_appointment'] == "0000-00-00 00:00:00"){
+                    echo "Missing Appointment date: {$person->getNameForForms()}\n";
+                }
+                $index = @++$counts[$fecType];
+                $fec[$row['user_id']] = $index;
+            }
         }
     }
                                       
