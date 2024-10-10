2deef6869379208ac80a63dabeb4d86a3be18af8
David Turner <finalfantasykid13@gmail.com>
2024-10-10 09:53:19 -0600

    -Added Rebuttal

--- extensions/Reporting/Report/SpecialPages/Faculty of Engineering/Report.php
+++ extensions/Reporting/Report/SpecialPages/Faculty of Engineering/Report.php
@@ -5,6 +5,35 @@ class Report extends TemplateReport{
     function Report(){
         parent::TemplateReport();
     }
+    
+    static function createTab(&$tabs){
+        parent::createTab($tabs);
+        $person = Person::newFromWgUser();
+        if($person->isLoggedIn() && $person instanceof FullPerson){
+            $person->getFecPersonalInfo();
+            if($person->faculty == getFaculty()){
+                $tabs["Rebuttal"] = TabUtils::createTab("Rebuttal");
+            }
+        }
+        return true;
+    }
+    
+    static function createSubTabs(&$tabs){
+        global $wgServer, $wgScriptPath, $wgUser, $wgTitle;
+        parent::createSubTabs($tabs);
+        $person = Person::newFromWgUser();
+        if($person->isLoggedIn() && $person instanceof FullPerson){
+            $person->getFecPersonalInfo();
+            if($person->faculty == getFaculty()){
+                $url = "$wgServer$wgScriptPath/index.php/Special:Report?report=";
+                if($person->isRole(NI) || $person->isRole("ATS")){
+                    $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "Rebuttal")) ? "selected" : false;
+                    $tabs["Rebuttal"]['subtabs'][] = TabUtils::createSubTab("Rebuttal", "{$url}Rebuttal", $selected);
+                }
+            }
+        }
+        return true;
+    }
 }
 
 ?>
