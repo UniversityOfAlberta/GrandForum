3e06db6e8ccd062f934081af79f13fa99ece421a
David Turner <finalfantasykid13@gmail.com>
2024-10-17 11:15:36 -0600

    -Disabled rebuttal

--- extensions/Reporting/Report/SpecialPages/Faculty of Engineering/Report.php
+++ extensions/Reporting/Report/SpecialPages/Faculty of Engineering/Report.php
@@ -26,10 +26,10 @@ class Report extends TemplateReport{
             $person->getFecPersonalInfo();
             $url = "$wgServer$wgScriptPath/index.php/Special:Report?report=";
             if($person->faculty == getFaculty()){
-                if($person->isRole(NI) || $person->isRole("ATS")){
+                /*if($person->isRole(NI) || $person->isRole("ATS")){
                     $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "Rebuttal")) ? "selected" : false;
                     $tabs["Rebuttal"]['subtabs'][] = TabUtils::createSubTab("Rebuttal", "{$url}Rebuttal", $selected);
-                }
+                }*/
             }
             
             // FEC Table
