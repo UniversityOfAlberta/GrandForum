1890e6fa42400219e828b81febff6c87594d4b11
David Turner <finalfantasykid13@gmail.com>
2024-10-31 09:46:11 -0600

    -Allow HR comments

--- extensions/Reporting/Report/ReportItems/ReportItemSets/FECReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/FECReportItemSet.php
@@ -4,18 +4,24 @@ class FECReportItemSet extends ReportItemSet {
     
     static $people = null;
     static $vdeans = array();
+    static $hrs = array();
     
     static function generateFECCache(){
         if(self::$people == null){
-            self::$people = Person::filterFaculty(Person::getAllPeople('FEC'));
+            self::$people = Person::filterFaculty(array_merge(Person::getAllPeople('FEC')));
         }
     }
     
     function getData(){
         $data = array();
         $includeVDean = (strtolower($this->getAttr("includeVDean", "false")) == "true");
+        $includeHR = (strtolower($this->getAttr("includeHR", "false")) == "true");
+        $excludeMe = (strtolower($this->getAttr("excludeMe", "false")) == "true");
         self::generateFECCache();
         foreach(self::$people as $person){
+            if($excludeMe && $person->isMe()){
+                continue;
+            }
             $tuple = self::createTuple();
             $tuple['person_id'] = $person->getId();
             $data[] = $tuple;
@@ -25,6 +31,22 @@ class FECReportItemSet extends ReportItemSet {
                 self::$vdeans = Person::getAllPeople(VDEAN);
             }
             foreach(self::$vdeans as $person){
+                if($excludeMe && $person->isMe()){
+                    continue;
+                }
+                $tuple = self::createTuple();
+                $tuple['person_id'] = $person->getId();
+                $data[] = $tuple;
+            }
+        }
+        if($includeHR){
+            if(empty(self::$hrs)){
+                self::$hrs = Person::getAllPeople(HR);
+            }
+            foreach(self::$hrs as $person){
+                if($excludeMe && $person->isMe()){
+                    continue;
+                }
                 $tuple = self::createTuple();
                 $tuple['person_id'] = $person->getId();
                 $data[] = $tuple;
