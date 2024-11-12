8870197da14f755f027b8f7a832bc4569fef1bc7
David Turner <finalfantasykid13@gmail.com>
2024-11-12 10:05:33 -0700

    -Added COI

--- extensions/Reporting/Report/ReportItems/ReportItemSets/FacultyPeopleReportItemSet.php
+++ extensions/Reporting/Report/ReportItems/ReportItemSets/FacultyPeopleReportItemSet.php
@@ -85,6 +85,9 @@ class FacultyPeopleReportItemSet extends ReportItemSet {
             }
             
             // SPECIAL CASES BELOW
+            if($me->getName() == "Samer.Adeeb" && $person->getName() == "Lindsey.Westover"){
+                continue;
+            }
             
             $index = @$fec[$person->getId()];
             $tuple = self::createTuple();
