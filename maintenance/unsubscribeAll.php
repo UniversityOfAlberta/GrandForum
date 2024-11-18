cca2642ff82689885cdc763f7739abf09dff128b
David Turner <finalfantasykid13@gmail.com>
2024-11-18 14:24:26 -0700

    -Changed how mailing list rules behave

--- maintenance/unsubscribeAll.php
+++ maintenance/unsubscribeAll.php
@@ -7,12 +7,22 @@
 	$iterationsSoFar = 0;
 	$allLists = MailingList::getAllMailingLists();
 	foreach($people as $person){
+	    $lists = MailingList::getPersonLists($person);
 	    if($person->isRole(INACTIVE)){
-	        $lists = MailingList::getPersonLists($person);
+	        // Unsubscribe from all lists if inactive
 	        foreach($lists as $list){
 	            if(in_array($list, $allLists)){ // Only unsubscribe from 'Managed' lists
                     MailingList::unsubscribe($list, $person);
                 }
+	        }
+	    }
+	    else{
+	        // Unsubscribe from lists which no longer pass the rules
+	        $ruleLists = MailingList::getPersonListsByRules($person);
+	        foreach($lists as $list){
+	            if(in_array($list, $allLists) && !in_array($list, $ruleLists)){ // Only unsubscribe from 'Managed' lists
+	                MailingList::unsubscribe($list, $person);
+	            }
 	        }
 	    }
 	    show_status(++$iterationsSoFar, count($people));
