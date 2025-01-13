9c6fae3e4913b24e228e362430b709912b5635ba
David Turner <finalfantasykid13@gmail.com>
2025-01-13 10:09:38 -0700

    -Added second bd reminder email for projects

--- maintenance/processBDReminders.php
+++ maintenance/processBDReminders.php
@@ -17,17 +17,35 @@ $year = date('Y');
 $today = date('Y-m-d');
 $future = date('m-d', time() + 86400*14);
 $futureDate = date('F j, Y', time() + 86400*14);
+$future2 = date('m-d', time() + 86400*30);
+$future2Date = date('F j, Y', time() + 86400*30);
 
 $projectQ = Report::dateToProjectQuarter($today);
 $themeQ = Report::dateToThemeQuarter($today);
 
-// Project Reports
+// Project Reports (2 weeks)
 if($future == "02-15" ||
    $future == "06-15" ||
    $future == "10-15"){
     $message = "Dear Project Leads,
 
                 This is a kind reminder to submit your <b>".str_replace("Q", "R", str_replace("_", ":", $projectQ))." project report(s) by {$futureDate}</b> via the <a href='https://forum.bridgingdivides.ca/index.php/Main_Page'>Bridging Divides Forum</a>. Instructions on how to navigate the Forum and submit reports can be found <a href='https://www.torontomu.ca/bridging-divides/documents/manuals/bd-forum-instructions.pdf'>here</a>. Please note that questions regarding Knowledge mobilization and community engagement and Changes in the research teams have been added to the reporting form.
+                
+                If you need further assistance, please contact <a href='skornicer@torontomu.ca'>Sanja Kornicer</a> or <a href='mailto:bridging.divides@torontomu.ca'>bridging.divides@torontomu.ca</a>.
+
+                Warm regards,
+
+                The Bridging Divides Team";
+    sendMail("Project Reports Due", $message, "bd-leaders@forum.bridgingdivides.ca");
+}
+
+// Project Reports (30 days)
+if($future2 == "02-15" ||
+   $future2 == "06-15" ||
+   $future2 == "10-15"){
+    $message = "Dear Project Leads,
+
+                This is a kind reminder to submit your <b>".str_replace("Q", "R", str_replace("_", ":", $projectQ))." project report(s) by {$future2Date}</b> via the <a href='https://forum.bridgingdivides.ca/index.php/Main_Page'>Bridging Divides Forum</a>. Instructions on how to navigate the Forum and submit reports can be found <a href='https://www.torontomu.ca/bridging-divides/documents/manuals/bd-forum-instructions.pdf'>here</a>. Please note that questions regarding Knowledge mobilization and community engagement and Changes in the research teams have been added to the reporting form.
                 
                 If you need further assistance, please contact <a href='skornicer@torontomu.ca'>Sanja Kornicer</a> or <a href='mailto:bridging.divides@torontomu.ca'>bridging.divides@torontomu.ca</a>.
 
