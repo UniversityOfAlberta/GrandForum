3bf0cdc417813780bf0efca4d4c37a8725f04b7e
Finalfantasykid <finalfantasykid13@gmail.com>
2025-01-21 15:48:36 -0700

    -Changes to epic conference

--- extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
+++ extensions/Reporting/Report/SpecialPages/AGE-WELL/ApplicationsTable.php
@@ -815,6 +815,7 @@ class ApplicationsTable extends SpecialPage{
     function generateEpicConference(){
         global $wgOut;
         $tabbedPage = new InnerTabbedPage("reports");
+        $tabbedPage->addTab(new ApplicationTab('RP_EPIC_CONFERENCE', null, 2025, "2025"));
         $tabbedPage->addTab(new ApplicationTab('RP_EPIC_CONFERENCE', null, 2024, "2024"));
         $tabbedPage->addTab(new ApplicationTab('RP_EPIC_CONFERENCE', null, 2023, "2023"));
         $tabbedPage->addTab(new ApplicationTab('RP_EPIC_CONFERENCE', null, 2022, "2022"));
