f4ca6d7fb5dfe51905948259499abaaa16d889f0
David Turner <finalfantasykid13@gmail.com>
2025-03-20 11:25:07 -0600

    -Removed a few unused files

--- extensions/GlobalSearch/Views/GlobalSearchView.js
+++ extensions/GlobalSearch/Views/GlobalSearchView.js
@@ -101,12 +101,6 @@ GlobalSearchResultsView = Backbone.View.extend({
         },
         "productResults" : function(){
             return new ProductResultsView({parent: this, model: new GlobalSearch({group: 'products', search: ''})});
-        },
-        "wikiResults" : function(){
-            return new WikiResultsView({parent: this, model: new GlobalSearch({group: 'wikipage', search: ''})});
-        },
-        "pdfResults" : function(){
-            return new PDFResultsView({parent: this, model: new GlobalSearch({group: 'pdf', search: ''})});
         }
     },
     
@@ -392,37 +386,5 @@ ProductResultsView = ResultsView.extend({
     
     render: function(){
         this.$el.html(this.template({group: productsTerm.pluralize()}));
-    }
-});
-
-WikiResultsView = ResultsView.extend({
-    maxResults: 3,
-
-    createCardView: function(model){
-        return new SmallWikiCardView({model: model});
-    },
-    
-    createModel: function(obj){
-        return new WikiPage({id: obj});
-    },
-    
-    render: function(){
-        this.$el.html(this.template({group: "Wiki Pages"}));
-    }
-});
-
-PDFResultsView = ResultsView.extend({
-    maxResults: 4,
-
-    createCardView: function(model){
-        return new SmallPDFCardView({model: model});
-    },
-    
-    createModel: function(obj){
-        return new PDF({id: obj});
-    },
-    
-    render: function(){
-        this.$el.html(this.template({group: "Reports"}));
     }
 });
