77b1b9a8f0367b3f7ce3e297320cee718d866a4c
GemmaryChang <gemmary@ualberta.ca>
2025-03-20 11:31:00 -0600

    assignee -> assignees

--- extensions/GrandObjects/LIMSTaskPmm.php
+++ extensions/GrandObjects/LIMSTaskPmm.php
@@ -43,16 +43,48 @@ class LIMSTaskPmm extends BackboneModel
         return $tasks;
     }
 
+     function getAssignees()
+    {
+        $data = DBFunctions::select(
+            array('grand_pmm_task_assginees'),
+            array('*'),
+            array('task_id' => $this->id)
+        );
+        $assignees = array();
+        foreach ($data as $row) {
+            $assignee = Person::newFromId($row['assignee']);
+            $assignees[] = $assignee;
+        }
+        return $assignees;
+    }
+
+    // static function getStatuses($opportunity_id)
+    // {
+    //     $data = DBFunctions::select(
+    //         array('grand_pmm_task'),
+    //         array('*'),
+    //         array('opportunity' => $opportunity_id)
+    //     );
+    //     $tasks = array();
+    //     foreach ($data as $row) {
+    //         $task = new LIMSTaskPmm(array($row));
+    //         if ($task->isAllowedToView()) {
+    //             $tasks[] = $task;
+    //         }
+    //     }
+    //     return $tasks;
+    // }
+
     function __construct($data)
     {
         if (count($data) > 0) {
             $this->id = $data[0]['id'];
             $this->opportunity = $data[0]['opportunity'];
-            $this->assignee = $data[0]['assignee'];
+            // $this->assignee = $data[0]['assignee'];
             $this->task = $data[0]['task'];
             $this->dueDate = $data[0]['due_date'];
             $this->comments = $data[0]['comments'];
-            $this->status = $data[0]['status'];
+            // $this->status = $data[0]['status'];
         }
     }
 
@@ -66,10 +98,10 @@ class LIMSTaskPmm extends BackboneModel
         return LIMSOpportunityPmm::newFromId($this->opportunity);
     }
 
-    function getAssignee()
-    {
-        return $this->assignee;
-    }
+    // function getAssignee()
+    // {
+    //     return $this->assignee;
+    // }
 
     function getPerson()
     {
@@ -120,21 +152,24 @@ class LIMSTaskPmm extends BackboneModel
     function toArray()
     {
         if ($this->isAllowedToView()) {
-            $person = $this->getPerson();
-            $assignee = array(
-                'id' => $person->getId(),
-                'name' => $person->getNameForForms(),
-                'url' => $person->getUrl()
-            );
+            $people = $this->getAssignees();
+            $assignees = array();
+            foreach($people as $person){
+                $assignees[] = array(
+                    'id' => $person->getId(),
+                    'name' => $person->getNameForForms(),
+                    'url' => $person->getUrl()
+                );
+            }
 
             $json = array(
                 'id' => $this->getId(),
                 'opportunity' => $this->getOpportunity()->getId(),
-                'assignee' => $assignee,
+                'assignees' => $assignees,
                 'task' => $this->getTask(),
                 'dueDate' => $this->getDueDate(),
                 'comments' => $this->getComments(),
-                'status' => $this->getStatus(),
+                // 'status' => $this->getStatus(),
                 'isAllowedToEdit' => $this->isAllowedToEdit()
             );
             return $json;
@@ -151,17 +186,17 @@ class LIMSTaskPmm extends BackboneModel
                 'grand_pmm_task',
                 array(
                     'opportunity' => $this->opportunity,
-                    'assignee' => $this->assignee,
+                    // 'assignee' => $this->assignee,
                     'task' => $this->task,
                     'due_date' => $this->dueDate,
                     'comments' => $this->comments,
-                    'status' => $this->status
+                    // 'status' => $this->status
                 )
             );
             $this->id = DBFunctions::insertId();
             // Send mail to assignee
-            $assignee = Person::newFromId($this->assignee);
-            Notification::addNotification($me, $assignee, "Task Created", "The task <b>{$this->task}</b> has been created", $this->getOpportunity()->getContact()->getProject()->getUrl() . "?tab=activity-management", false);
+            // $assignee = Person::newFromId($this->assignee);
+            // Notification::addNotification($me, $assignee, "Task Created", "The task <b>{$this->task}</b> has been created", $this->getOpportunity()->getContact()->getProject()->getUrl() . "?tab=activity-management", false);
         }
     }
 
@@ -174,7 +209,7 @@ class LIMSTaskPmm extends BackboneModel
                 array('*'),
                 array('id' => $this->id)
             );
-            $assignee = Person::newFromId($this->assignee);
+            // $assignee = Person::newFromId($this->assignee);
 
             // if(@$data[0]['assignee'] != $this->assignee){
             //     // If the assignee was changed, send email to new assignee
@@ -188,26 +223,26 @@ class LIMSTaskPmm extends BackboneModel
                 'grand_pmm_task',
                 array(
                     'opportunity' => $this->opportunity,
-                    'assignee' => $this->assignee,
+                    // 'assignee' => $this->assignee,
                     'task' => $this->task,
                     'due_date' => $this->dueDate,
                     'comments' => $this->comments,
-                    'status' => $this->status
+                    // 'status' => $this->status
                 ),
                 array('id' => $this->id)
             );
-            if ($assignee != null && $assignee->getId() != 0) {
-                if ($data[0]['status'] != 'Closed' && $this->status == 'Closed') {
-                    Notification::addNotification($me, $assignee, "Thank You for Completing <b>{$this->task}</b>!",
-                    "Hello <b>{$assignee->getNameForForms()}</b>, thank you for completing <b>{$this->task}</b> on I-CONNECTS.
-                    We truly appreciate your effort and timely contribution.
-                    Your Impact:
-                    Your work helps us maintain momentum and reach our goals in collaborative, open team science.
-                    The insights or data you provided will guide the next steps for our project and benefit fellow team members.", $this->getOpportunity()->getContact()->getProject()->getUrl() . "?tab=activity-management", false);
-                } else {
-                    Notification::addNotification($me, $assignee, "Task Updated", "The task <b>{$this->task}</b> has been updated", $this->getOpportunity()->getContact()->getProject()->getUrl() . "?tab=activity-management", false);
-                }
-            }
+            // if ($assignee != null && $assignee->getId() != 0) {
+            //     if ($data[0]['status'] != 'Closed' && $this->status == 'Closed') {
+            //         Notification::addNotification($me, $assignee, "Thank You for Completing <b>{$this->task}</b>!",
+            //         "Hello <b>{$assignee->getNameForForms()}</b>, thank you for completing <b>{$this->task}</b> on I-CONNECTS.
+            //         We truly appreciate your effort and timely contribution.
+            //         Your Impact:
+            //         Your work helps us maintain momentum and reach our goals in collaborative, open team science.
+            //         The insights or data you provided will guide the next steps for our project and benefit fellow team members.", $this->getOpportunity()->getContact()->getProject()->getUrl() . "?tab=activity-management", false);
+            //     } else {
+            //         Notification::addNotification($me, $assignee, "Task Updated", "The task <b>{$this->task}</b> has been updated", $this->getOpportunity()->getContact()->getProject()->getUrl() . "?tab=activity-management", false);
+            //     }
+            // }
         }
     }
 
