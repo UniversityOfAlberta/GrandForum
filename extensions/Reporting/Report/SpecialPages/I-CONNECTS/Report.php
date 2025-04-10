b57fac9382e79c7e65b606483dc81c17ce098da3
GemmaryChang <gemmary@ualberta.ca>
2025-04-10 14:50:45 -0600

    add hidding script for I-CONNECTS, now the sidebar is hidden to member

---
+++ extensions/Reporting/Report/SpecialPages/I-CONNECTS/Report.php
@@ -0,0 +1,79 @@
+<?php
+
+$dir = dirname(__FILE__) . '/';
+$wgSpecialPages['Report'] = 'Report'; # Let MediaWiki know about the special page.
+$wgExtensionMessagesFiles['Report'] = $dir . 'Report.i18n.php';
+$wgSpecialPageGroups['Report'] = 'reporting-tools';
+$wgHooks['BeforePageDisplay'][] = 'Report::addFooter';
+
+$wgHooks['TopLevelTabs'][] = 'Report::createTab';
+$wgHooks['SubLevelTabs'][] = 'Report::createSubTabs';
+$wgHooks['ToolboxLinks'][] = 'Report::createToolboxLinks';
+
+class Report extends AbstractReport{
+    
+    function __construct(){
+        global $config;
+        $report = @$_GET['report'];
+        $topProjectOnly = false;
+        if(isset($_GET['project']) && ($report == "NIReport" || $report == "HQPReport" || $report == "SABReport")){
+            $topProjectOnly = true;
+        }
+        parent::__construct(dirname(__FILE__)."/../../ReportXML/{$config->getValue('networkName')}/$report.xml", -1, false, $topProjectOnly);
+    }
+
+    static function createTab(&$tabs){
+        global $wgServer, $wgScriptPath, $wgUser, $wgTitle, $special_evals;
+        $tabs["Reports"] = TabUtils::createTab("My Reports");
+        
+        return true;
+    }
+    
+    static function createSubTabs(&$tabs){
+        global $wgServer, $wgScriptPath, $wgUser, $wgTitle, $special_evals;
+        $person = Person::newFromWgUser();
+        $url = "$wgServer$wgScriptPath/index.php/Special:Report?report=";
+        
+        
+        return true;
+    }
+    
+    static function createToolboxLinks(&$toolbox){
+        global $wgServer, $wgScriptPath, $config;
+
+        return true;
+    }
+
+    static function addFooter($wgOut, $skin){
+        global $wgServer, $wgScriptPath, $config;
+        $me = Person::newFromWgUser();
+
+        if($me->isLoggedIn() && !$me->isRoleAtLeast(STAFF) ){
+            $wgOut->addHTML("<style>
+                #side {
+                    display: none !important;
+                }
+
+                #bodyContent {
+                    left: 0;
+                }
+
+                #outerHeader {
+                    left: 0;
+                }
+
+                #sideToggle{
+                    display: none;
+                }
+            </style>");
+        }
+    }
+
+
+
+
+
+
+}
+
+?>
