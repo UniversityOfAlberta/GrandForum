b5991616e8a787c391d1e2cfa73da55587ce2823
David Turner <finalfantasykid13@gmail.com>
2025-04-16 13:20:28 -0600

    -Removed couple permissions in GrandAccess

--- extensions/AccessControls/GrandAccess.php
+++ extensions/AccessControls/GrandAccess.php
@@ -21,23 +21,13 @@ class GrandAccess {
 	        $aRights[$i++] = RMC;
 	    }
 	    if($me->isRole(NI)){
-	        $aRights[$i++] = "Researcher";
-	        $aRights[$i++] = "Researcher+";
 	        $aRights[$i++] = "NI";
 	        $aRights[$i++] = "NI+";
 	    }
 	    foreach(array_keys($wgRoleValues) as $role){
 	        if($me->isRoleAtLeast($role)){
 	            $aRights[$i++] = $role.'+';
-	            $aRights[$i++] = $role.'During+';
-	            if(($role == STAFF || $role == MANAGER || $role == ADMIN) && array_search('Researcher+', $aRights) === false){
-	                $aRights[$i++] = 'Researcher+';
-	            }
 	        }
-	    }
-	    foreach($me->getRolesDuring(CYCLE_START, CYCLE_END) as $role){
-            $aRights[$i++] = $role->getRole().'During';
-            $aRights[$i++] = $role->getRole().'During+';
 	    }
 	    if(count($me->getRoles()) > 0){
 	        foreach($me->getRoles() as $role){
