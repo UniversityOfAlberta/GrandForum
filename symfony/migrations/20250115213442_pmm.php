9249cb4487def261c9f43764ca8b9ef5fb75e202
David Turner <finalfantasykid13@gmail.com>
2025-04-25 13:19:05 -0600

    -Fixed migration

--- symfony/migrations/20250115213442_pmm.php
+++ symfony/migrations/20250115213442_pmm.php
@@ -52,7 +52,7 @@ final class Pmm extends AbstractMigration
         $table = $this->table('grand_pmm_files', array('id' => 'id'));
         $table->addColumn('opportunity_id', 'integer')
             ->addColumn('filename', 'string', array('limit' => 128))
-            ->addColumn('type', 'string', array('limit' => '64'))
+            ->addColumn('type', 'string', array('limit' => 64))
             ->addColumn('data', 'text', array('limit' => MysqlAdapter::TEXT_MEDIUM))
             ->addIndex('opportunity_id')
             ->create();
