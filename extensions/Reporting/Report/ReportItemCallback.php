faa46e31f117eca6c5ffe6b3b32074182d42e68d
David Turner <finalfantasykid13@gmail.com>
2025-05-23 14:48:27 -0600

    -Added courses file uploads

--- extensions/Reporting/Report/ReportItemCallback.php
+++ extensions/Reporting/Report/ReportItemCallback.php
@@ -28,6 +28,7 @@ class ReportItemCallback {
             "end_date" => "getEndDate",
             // Courses
             "getCourseCount" => "getCourseCount",
+            "course_id" => "getCourseId",
             "course_term" => "getCourseTerm",
             "course_start" => "getCourseStart",
             "course_end" => "getCourseEnd",
@@ -404,6 +405,10 @@ class ReportItemCallback {
             }
         }
         return $count;
+    }
+    
+    function getCourseId(){
+        return $this->reportItem->projectId;
     }
 
     function getCourseTerm(){
