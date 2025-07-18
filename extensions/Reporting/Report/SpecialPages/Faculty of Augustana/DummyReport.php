fe0a537f8a49fe1a926c762a449e198047261ffc
David Turner <finalfantasykid13@gmail.com>
2025-07-18 10:24:57 -0600

    -Fixed php

--- extensions/Reporting/Report/SpecialPages/Faculty of Augustana/DummyReport.php
+++ extensions/Reporting/Report/SpecialPages/Faculty of Augustana/DummyReport.php
@@ -2,15 +2,11 @@
 
 class DummyReport extends AbstractReport{
     
-    function DummyReport($reportType, $person, $project=null, $year=REPORTING_YEAR, $quick=false){
+    function __construct($reportType, $person, $project=null, $year=YEAR, $quick=false){
         global $config;
         $this->readOnly = true;
-        $projectName = null;
-        if($project != null){
-            $projectName = $project->getName();
-        }
         $topProjectOnly = false;
-        $this->AbstractReport(dirname(__FILE__)."/../../ReportXML/{$config->getValue('networkName')}/$reportType.xml", $person->getId(), $projectName, $topProjectOnly, $year, $quick);
+        parent::__construct(dirname(__FILE__)."/../../ReportXML/{$config->getValue('networkName')}/$reportType.xml", $person->getId(), null, $topProjectOnly, $year, $quick);
     }
     
 }
