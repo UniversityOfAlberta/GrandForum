4117fbeb8f50a49911f7a252079060b802f94c86
KasukabeDefenceForce <abhinavohri13@gmail.com>
2025-09-03 15:22:02 +0530

    Rename activity manangment to activities

--- extensions/GrandObjectPage/LIMSPmm/routes.js
+++ extensions/GrandObjectPage/LIMSPmm/routes.js
@@ -35,7 +35,7 @@ allPeople.roles = [STAFF,MANAGER,ADMIN];
 allPeopleXHR = allPeople.fetch();
 
 pageRouter.on('route:showLIMSContactsTable', function(){
-    main.set('title', "Activity Management");
+    main.set('title', "Activities");
     $.when(allPeopleXHR).done(function(){
         // Get All LIMSContacts
         var contacts = new LIMSContactsPmm();
