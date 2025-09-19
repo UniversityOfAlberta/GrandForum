c274290b8c140e80484ac1c4ae273ab5145aa617
David Turner <finalfantasykid13@gmail.com>
2025-09-19 10:51:44 -0600

    -Fixed issue with pdf links

--- extensions/Reporting/PDFGenerator/PDFGenerator.php
+++ extensions/Reporting/PDFGenerator/PDFGenerator.php
@@ -983,6 +983,7 @@ EOF;
                 -dBATCH \\
                 -dNOPAUSE \\
                 -sDEVICE=pdfwrite \\
+                -dPrinted=false \\
                 -dPDFSETTINGS=/prepress \\
                 -sOutputFile=\"/tmp/{$name}{$rand}withmarks\" \"/tmp/{$name}{$rand}nomarks\" \"/tmp/{$name}{$rand}pdfmarks\""); // Add Bookmarks
         
