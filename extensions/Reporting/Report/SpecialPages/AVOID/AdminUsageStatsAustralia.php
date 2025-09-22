bc61b90f9b8f95a9ff4453c4480a9c04eb3cf320
David Turner <finalfantasykid13@gmail.com>
2025-09-22 10:06:29 -0600

    -Changes for australia

--- extensions/Reporting/Report/SpecialPages/AVOID/AdminUsageStatsAustralia.php
+++ extensions/Reporting/Report/SpecialPages/AVOID/AdminUsageStatsAustralia.php
@@ -15,6 +15,14 @@ class AdminUsageStatsAustralia extends AdminUsageStats {
 
     function execute($par){
         global $wgUser, $wgOut, $wgServer, $wgScriptPath, $wgTitle;
+        $group = (isset($_GET['group'])) ? $_GET['group'] : "GroupA";
+        $wgOut->addHTML("<div style='font-size: 1.25em;'>
+                            <a class='GroupA' href='{$wgServer}{$wgScriptPath}/index.php/Special:AdminUsageStatsAustralia?group=GroupA'>Group A</a> | 
+                            <a class='GroupB' href='{$wgServer}{$wgScriptPath}/index.php/Special:AdminUsageStatsAustralia?group=GroupB'>Group B</a>
+                         </div>
+                         <script type='text/javascript'>
+                            $('.$group').css('font-weight', 'bold');
+                         </script>");
         $this->getOutput()->setPageTitle("Admin");
         $this->showActionPlanStats();
         $this->showRegistrantsStats();
@@ -28,7 +36,8 @@ class AdminUsageStatsAustralia extends AdminUsageStats {
         $person = Person::newFromId($userId);
         if($person->getId() == 0){ return true; }
         $postal_code = AdminDataCollection::getBlobValue(BLOB_TEXT, YEAR, "RP_AVOID", "AVOID_Questions_tab0", "POSTAL", $person->getId());
-        if($person->isRoleAtLeast(STAFF) || $postal_code == "CFN" || !$person->isRole("GroupA")){
+        $group = (isset($_GET['group'])) ? $_GET['group'] : "GroupA";
+        if($person->isRoleAtLeast(STAFF) || $postal_code == "CFN" || !$person->isRole($group)){
             return true;
         }
         return false;
