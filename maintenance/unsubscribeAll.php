fd109284fa7fb68f03eca10f0a92217c0186024f
Finalfantasykid <finalfantasykid13@gmail.com>
2025-09-24 09:35:17 -0600

    Changes to unsubscribeAll

--- maintenance/unsubscribeAll.php
+++ maintenance/unsubscribeAll.php
@@ -5,26 +5,26 @@
 	$people = Person::getAllCandidates();
 	
 	$iterationsSoFar = 0;
-	$allLists = MailingList::getAllMailingLists();
+	$allLists = array_column(MailingList::getAllMailingLists(), 'name');
 	foreach($people as $person){
-	    $lists = MailingList::getPersonLists($person);
-	    if($person->isRole(INACTIVE)){
-	        // Unsubscribe from all lists if inactive
-	        foreach($lists as $list){
-	            if(in_array($list, $allLists)){ // Only unsubscribe from 'Managed' lists
+        $lists = MailingList::getPersonLists($person);
+        if($person->isRole(INACTIVE) && count($config->getValue('positionList')) == 0){
+            // Unsubscribe from all lists if inactive
+            foreach($lists as $list){
+                if(in_array($list, $allLists)){ // Only unsubscribe from 'Managed' lists
                     MailingList::unsubscribe($list, $person);
                 }
-	        }
-	    }
-	    else{
-	        // Unsubscribe from lists which no longer pass the rules
-	        $ruleLists = MailingList::getPersonListsByRules($person);
-	        foreach($lists as $list){
-	            if(in_array($list, $allLists) && !in_array($list, $ruleLists)){ // Only unsubscribe from 'Managed' lists
-	                MailingList::unsubscribe($list, $person);
-	            }
-	        }
-	    }
+            }
+        }
+        else{
+            // Unsubscribe from lists which no longer pass the rules
+            $ruleLists = MailingList::getPersonListsByRules($person);
+            foreach($lists as $list){
+                if(in_array($list, $allLists) && !in_array($list, $ruleLists)){ // Only unsubscribe from 'Managed' lists
+                    MailingList::unsubscribe($list, $person);
+                }
+            }
+        }
 	    show_status(++$iterationsSoFar, count($people));
 	}
 ?>
