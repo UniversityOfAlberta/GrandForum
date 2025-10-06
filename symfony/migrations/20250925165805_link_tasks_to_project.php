b08091769c51c9012d1b55fd44abdd3a6c89d68c
KasukabeDefenceForce <abhinavohri13@gmail.com>
2025-10-07 00:22:09 +0530

    Shift project_ids from contact table to task table

--- symfony/migrations/20250925165805_link_tasks_to_project.php
+++ symfony/migrations/20250925165805_link_tasks_to_project.php
@@ -22,6 +22,17 @@ final class LinkTasksToProject extends AbstractMigration
         $table = $this->table('grand_pmm_task');
         $table->addColumn('project_id', 'integer', ['null' => false])
             ->addIndex('project_id')
+            ->update();
+
+        $this->execute("
+            UPDATE grand_pmm_task t
+            INNER JOIN grand_pmm_opportunity o ON t.opportunity = o.id
+            INNER JOIN grand_pmm_contact c ON o.contact = c.id
+            INNER JOIN grand_project p ON c.project_id = p.id
+            SET t.project_id = p.id
+        ");
+
+        $table->changeColumn('project_id', 'integer', ["null" => false])
             ->addForeignKey('project_id','grand_project','id', [
                 'delete'=> 'CASCADE'
                 ]
