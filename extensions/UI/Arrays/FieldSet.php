f51fcdbe250a3e2eee47d0a1b9269c05f5d30ef6
KasukabeDefenceForce <abhinavohri13@gmail.com>
2025-10-08 21:54:16 +0530

    Shidt theme fieldset after status

--- extensions/UI/Arrays/FieldSet.php
+++ extensions/UI/Arrays/FieldSet.php
@@ -4,14 +4,18 @@ class FieldSet extends UIElementArray {
     
     var $legend;
     
-    function __construct($id, $legend){
+    function __construct($id, $legend=null){
         parent::__construct($id);
         $this->legend = $legend;
         $this->attr('style', 'display:inline;min-width:500px;');
     }
     
     function render(){
-        $html = "<fieldset {$this->renderAttr()}><legend>{$this->legend}</legend>";
+        $legendHtml = '';
+        if (!empty($this->legend)) {
+            $legendHtml = "<legend>{$this->legend}</legend>";
+        }
+        $html = "<fieldset {$this->renderAttr()}>{$legendHtml}";
         foreach($this->elements as $element){
             $html .= $element->render();
         }
