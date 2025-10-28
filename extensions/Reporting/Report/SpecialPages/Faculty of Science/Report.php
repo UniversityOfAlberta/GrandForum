12df591e814b3c8e66fa63dec5a091c0bd37cf3c
David Turner <finalfantasykid13@gmail.com>
2025-10-28 14:19:01 -0600

    -Fixed url

--- extensions/Reporting/Report/SpecialPages/Faculty of Science/Report.php
+++ extensions/Reporting/Report/SpecialPages/Faculty of Science/Report.php
@@ -24,15 +24,14 @@ class Report extends TemplateReport{
         $person = Person::newFromWgUser();
         if($person->isLoggedIn() && $person instanceof FullPerson){
             $person->getFecPersonalInfo();
+            $url = "$wgServer$wgScriptPath/index.php/Special:Report?report=";
             if($person->inFaculty()){
-                $url = "$wgServer$wgScriptPath/index.php/Special:Report?report=";
                 if($person->isRole(NI)){
                     $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "SabbaticalApplication")) ? "selected" : false;
                     $tabs["Sabbatical"]['subtabs'][] = TabUtils::createSubTab("Sabbatical Application", "{$url}SabbaticalApplication", $selected);
                 }
             }
             if($person->isRole(CHAIR)){
-                $url = "$wgServer$wgScriptPath/index.php/Special:Report?report=";
                 if($person->isRole(CHAIR)){
                     $selected = @($wgTitle->getText() == "Report" && ($_GET['report'] == "Letters/Base")) ? "selected" : false;
                     $tabs["Chair"]['subtabs'][] = TabUtils::createSubTab("Letters", "{$url}Letters/Base", $selected);
