b96bb2f2b3bb5bbf1017710d7e79107327eecd23
David Turner <finalfantasykid13@gmail.com>
2025-11-20 10:07:32 -0700

    -Removed old sub-roles

--- extensions/QASummary/QACVGenerator.php
+++ extensions/QASummary/QACVGenerator.php
@@ -19,22 +19,10 @@ class QACVGenerator extends SpecialPage{
         global $wgOut, $wgServer, $wgScriptPath;
         $tabbedPage = new TabbedAjaxPage("qacv");
         $person = Person::newFromWgUser();
-        
-        if($person->isRoleAtLeast(STAFF) || $person->isSubRole("QA_PHYS"))
-            $tabbedPage->addTab(new QACVDepartmentTab("Physics", array("PHYS")));
-        if($person->isRoleAtLeast(STAFF) || $person->isSubRole("QA_CHEM"))
-            $tabbedPage->addTab(new QACVDepartmentTab("Chemistry", array("CHEM")));
-        if($person->isRoleAtLeast(STAFF) || $person->isSubRole("QA_BIOL"))
-            $tabbedPage->addTab(new QACVDepartmentTab("Biological Sciences", array("BIOL")));
-        if($person->isRoleAtLeast(STAFF) || $person->isSubRole("QA_CMPUT"))
-            $tabbedPage->addTab(new QACVDepartmentTab("Computing Science", array("CMPUT")));
-        if($person->isRoleAtLeast(STAFF) || $person->isSubRole("QA_MATH"))
-            $tabbedPage->addTab(new QACVDepartmentTab("Mathematical And Statistical Sciences", array("MATH", "STAT")));
-        if($person->isRoleAtLeast(STAFF) || $person->isSubRole("QA_EAS"))
-            $tabbedPage->addTab(new QACVDepartmentTab("Earth And Atmospheric Sciences", array("EAS")));
-        if($person->isRoleAtLeast(STAFF) || $person->isSubRole("QA_EAS"))
-            $tabbedPage->addTab(new QACVDepartmentTab("Psychology", array("PSYCH")));
-        
+        foreach($facultyMapSimple[getFaculty()] as $key => $dept){
+            if($person->isRoleAtLeast(STAFF))
+                $tabbedPage->addTab(new QACVDepartmentTab($dept, array($key)));
+        }        
         $tabbedPage->showPage();
         
         $wgOut->addHTML("<script type='text/javascript'>
@@ -44,13 +32,7 @@ class QACVGenerator extends SpecialPage{
     
     function userCanExecute($user){
         $person = Person::newFromUser($user);
-        return ($person->isRoleAtLeast(STAFF) || $person->isSubRole('QA_PHYS') ||
-                                                 $person->isSubRole('QA_CHEM') ||
-                                                 $person->isSubRole('QA_BIOL') ||
-                                                 $person->isSubRole('QA_CMPUT') ||
-                                                 $person->isSubRole('QA_MATH') ||
-                                                 $person->isSubRole('QA_EAS') ||
-                                                 $person->isSubRole('QA_PSYCH'));
+        return ($person->isRoleAtLeast(STAFF));
     }
     
     static function createSubTabs(&$tabs){
--- extensions/QASummary/QASummary.php
+++ extensions/QASummary/QASummary.php
@@ -17,27 +17,17 @@ class QASummary extends SpecialPage{
     }
 
     function execute($par){
-        global $wgOut, $wgServer, $wgScriptPath;
+        global $wgOut, $wgServer, $wgScriptPath, $facultyMapSimple;
         
         $tabbedPage = new TabbedAjaxPage("qasummary");
         $person = Person::newFromWgUser();
         $person->getFecPersonalInfo();
         $departments = @array_keys($person->departments);
         $department = @$departments[0];
-        if(self::checkRole($person) || $person->isSubRole("QA_PHYS")  || $department == "Physics")
-            $tabbedPage->addTab(new DepartmentTab("Physics", array("PHYS")));
-        if(self::checkRole($person) || $person->isSubRole("QA_CHEM")  || $department == "Chemistry")
-            $tabbedPage->addTab(new DepartmentTab("Chemistry", array("CHEM")));
-        if(self::checkRole($person) || $person->isSubRole("QA_BIOL")  || $department == "Biological Sciences")
-            $tabbedPage->addTab(new DepartmentTab("Biological Sciences", array("BIOL")));
-        if(self::checkRole($person) || $person->isSubRole("QA_CMPUT") || $department == "Computing Science")
-            $tabbedPage->addTab(new DepartmentTab("Computing Science", array("CMPUT")));
-        if(self::checkRole($person) || $person->isSubRole("QA_MATH")  || $department == "Mathematical And Statistical Sciences")
-            $tabbedPage->addTab(new DepartmentTab("Mathematical And Statistical Sciences", array("MATH", "STAT")));
-        if(self::checkRole($person) || $person->isSubRole("QA_EAS")   || $department == "Earth And Atmospheric Sciences")
-            $tabbedPage->addTab(new DepartmentTab("Earth And Atmospheric Sciences", array("EAS")));
-        if(self::checkRole($person) || $person->isSubRole("QA_PSYCH") || $department == "Psychology")
-            $tabbedPage->addTab(new DepartmentTab("Psychology", array("PSYCH")));
+        foreach($facultyMapSimple[getFaculty()] as $key => $dept){
+            if(self::checkRole($person) || $department == $dept)
+            $tabbedPage->addTab(new DepartmentTab($dept, array($key)));
+        }
         
         $tabbedPage->showPage();
         
@@ -55,15 +45,7 @@ class QASummary extends SpecialPage{
     
     function userCanExecute($user){
         $me = Person::newFromUser($user);
-        return (self::checkRole($me) || 
-                $me->isRole(CHAIR) ||
-                $me->isSubRole('QA_PHYS') ||
-                $me->isSubRole('QA_CHEM') ||
-                $me->isSubRole('QA_BIOL') ||
-                $me->isSubRole('QA_CMPUT') ||
-                $me->isSubRole('QA_MATH') ||
-                $me->isSubRole('QA_EAS') ||
-                $me->isSubRole('QA_PSYCH'));
+        return (self::checkRole($me) || $me->isRole(CHAIR));
     }
     
     static function createSubTabs(&$tabs){
